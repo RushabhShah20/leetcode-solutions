00d3e05b8c7b77252789c36ef1ba9b32e1f76da8
Rushabh <20rush09abh2001@gmail.com>
2025-04-01 15:18:54 +0530

    Added C++ solution to 'Find the Sum of Encrypted Integers'

---
+++ easy/Find the Sum of Encrypted Integers/C++/main.cpp
@@ -0,0 +1,47 @@
+// Problem: Find the Sum of Encrypted Integers
+// Link to the problem: https://leetcode.com/problems/find-the-sum-of-encrypted-integers/
+class Solution
+{
+public:
+    int maxDigitOfNum(int num)
+    {
+        int maxDigit = 0;
+        while (num > 0)
+        {
+            maxDigit = max(maxDigit, num % 10);
+            num /= 10;
+        }
+        return maxDigit;
+    }
+    int numofDigits(int num)
+    {
+        int count = 0;
+        while (num > 0)
+        {
+            count++;
+            num /= 10;
+        }
+        return count;
+    }
+    int encrypt(int num)
+    {
+        int maxDigit = maxDigitOfNum(num);
+        int numDigits = numofDigits(num);
+        int ans = 0;
+        for (int i = 1; i <= numDigits; i++)
+        {
+            ans += maxDigit * pow(10, i - 1);
+        }
+        return ans;
+    }
+    int sumOfEncryptedInt(vector<int> &nums)
+    {
+        int ans = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            nums[i] = encrypt(nums[i]);
+            ans += nums[i];
+        }
+        return ans;
+    }
+};
