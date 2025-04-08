ae54c06887fcfe4f204db4fd704a959e495fd26b
Rushabh <20rush09abh2001@gmail.com>
2025-04-08 13:39:32 +0530

    Added C# solution to 'Find the Maximum Achievable Number'

---
+++ easy/Find the Maximum Achievable Number/C#/main.cs
@@ -0,0 +1,9 @@
+// Problem: Find the Maximum Achievable Number
+// Link to the problem: https://leetcode.com/problems/find-the-maximum-achievable-number/
+public class Solution
+{
+    public int TheMaximumAchievableX(int num, int t)
+    {
+        return num + (2 * t);
+    }
+}
