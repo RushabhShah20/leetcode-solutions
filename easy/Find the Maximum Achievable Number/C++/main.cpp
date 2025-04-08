8ddfc2715c946f13acadcdfb4f5980ef53a6dedd
Rushabh <20rush09abh2001@gmail.com>
2025-04-08 13:27:44 +0530

    Find C++ solution to 'Find the Maximum Achievable Number'

---
+++ easy/Find the Maximum Achievable Number/C++/main.cpp
@@ -0,0 +1,10 @@
+// Problem: Find the Maximum Achievable Number
+// Link to the problem: https://leetcode.com/problems/find-the-maximum-achievable-number/
+class Solution
+{
+public:
+    int theMaximumAchievableX(int num, int t)
+    {
+        return num + (t * 2);
+    }
+};
