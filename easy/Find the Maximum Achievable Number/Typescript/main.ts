7ad734b526a9737517d5f1675c2aa7fea7bfdca9
Rushabh <20rush09abh2001@gmail.com>
2025-04-08 13:40:47 +0530

    Added Typescript solution to 'Find the Maximum Achievable Number'

---
+++ easy/Find the Maximum Achievable Number/Typescript/main.ts
@@ -0,0 +1,5 @@
+// Problem: Find the Maximum Achievable Number
+// Link to the problem: https://leetcode.com/problems/find-the-maximum-achievable-number/
+function theMaximumAchievableX(num: number, t: number): number {
+    return num + (2 * t);
+};
