e3544e266549023a4cf3af2f831d597fc743a863
Rushabh <20rush09abh2001@gmail.com>
2025-04-08 13:40:15 +0530

    Added JavaScript solution to 'Find the Maximum Achievable Number'

---
+++ easy/Find the Maximum Achievable Number/JavaScript/main.js
@@ -0,0 +1,10 @@
+// Problem: Find the Maximum Achievable Number
+// Link to the problem: https://leetcode.com/problems/find-the-maximum-achievable-number/
+/**
+ * @param {number} num
+ * @param {number} t
+ * @return {number}
+ */
+var theMaximumAchievableX = function (num, t) {
+  return num + 2 * t;
+};
