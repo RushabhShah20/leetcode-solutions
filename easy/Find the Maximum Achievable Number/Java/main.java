418711ef2a5af7115ce3700442c8ed8776489d9f
Rushabh <20rush09abh2001@gmail.com>
2025-04-08 13:28:02 +0530

    Added Java solution to 'Find the Maximum Achievable Number'

---
+++ easy/Find the Maximum Achievable Number/Java/main.java
@@ -0,0 +1,7 @@
+// Problem: Find the Maximum Achievable Number
+// Link to the problem: https://leetcode.com/problems/find-the-maximum-achievable-number/
+class Solution {
+    public int theMaximumAchievableX(int num, int t) {
+        return num + (2 * t);
+    }
+}
