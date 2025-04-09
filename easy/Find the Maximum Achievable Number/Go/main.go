2683df370eee5a4722678569da9aadc080666f23
Rushabh <20rush09abh2001@gmail.com>
2025-04-09 10:54:41 +0530

    Added Go solution to 'Find the Maximum Achiveable Number'

---
+++ easy/Find the Maximum Achievable Number/Go/main.go
@@ -0,0 +1,5 @@
+// Problem: Find the Maximum Achievable Number
+// Link to the problem: https://leetcode.com/problems/find-the-maximum-achievable-number/
+func theMaximumAchievableX(num int, t int) int {
+	return num + (2 * t)
+}
