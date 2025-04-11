1c37bf688a15b8593765d82106b2cad44ba03d8c
Rushabh <20rush09abh2001@gmail.com>
2025-04-11 15:47:18 +0530

    Added Swift solution to 'Maximum Achievable Number'

---
+++ easy/Find the Maximum Achievable Number/Swift/main.swift
@@ -0,0 +1,7 @@
+// Problem: Find the Maximum Achievable Number
+// Link to the problem: https://leetcode.com/problems/find-the-maximum-achievable-number/
+class Solution {
+    func theMaximumAchievableX(_ num: Int, _ t: Int) -> Int {
+        return num + (2 * t);
+    }
+}
