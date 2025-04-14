9c675454384685d76c4964a72b1064931b5f5530
Rushabh <20rush09abh2001@gmail.com>
2025-04-14 20:06:17 +0530

    Added Typescript solution to 'Find Missing and Repeated Values'

---
+++ easy/Find Missing and Repeated Values/Typescript/main.ts
@@ -0,0 +1,20 @@
+// Problem: Find Missing and Repeated Values
+// Link to the problem: https://leetcode.com/problems/find-missing-and-repeated-values/
+function findMissingAndRepeatedValues(grid: number[][]): number[] {
+    let freq: number[] = new Array(grid.length * grid[0].length).fill(0);
+    let ans: number[] = new Array(2).fill(0);
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[0].length; j++) {
+            freq[grid[i][j] - 1]++;
+        }
+    }
+    for (let i = 0; i < freq.length; i++) {
+        if (freq[i] == 0) {
+            ans[1] = i + 1;
+        }
+        if (freq[i] == 2) {
+            ans[0] = i + 1;
+        }
+    }
+    return ans;
+};
