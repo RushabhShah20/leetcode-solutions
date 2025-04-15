ed8c9631ad7be30e8b006fb80267669d04226f9f
Rushabh <20rush09abh2001@gmail.com>
2025-04-15 15:43:57 +0530

    Added C++ solution to 'Kth Smallest Element in a Sorted Matrix'

---
+++ medium/Kth Smallest Element in a Sorted Matrix/C++/main.cpp
@@ -0,0 +1,21 @@
+// Problem: Kth Smallest Element in a Sorted Matrix
+// Link to the problem: https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int kthSmallest(vector<vector<int>> &matrix, int k)
+    {
+        vector<int> ans;
+        for (size_t i = 0; i < matrix.size(); i++)
+        {
+            for (size_t j = 0; j < matrix[0].size(); j++)
+            {
+                ans.push_back(matrix[i][j]);
+            }
+        }
+        sort(ans.begin(), ans.end());
+        return ans[k - 1];
+    }
+};
