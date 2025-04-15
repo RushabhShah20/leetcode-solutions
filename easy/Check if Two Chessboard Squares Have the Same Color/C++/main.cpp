2eb95f50f65813dd02b2a9b51b01134883b17b52
Rushabh <20rush09abh2001@gmail.com>
2025-04-15 12:06:47 +0530

    Added C++ solution to 'Check if Two Chessboard Squares Have the Same Color'

---
+++ easy/Check if Two Chessboard Squares Have the Same Color/C++/main.cpp
@@ -0,0 +1,45 @@
+// Problem: Check if Two Chessboard Squares Have the Same Color
+// Link to the problem: https://leetcode.com/problems/check-if-two-chessboard-squares-have-the-same-color/
+class Solution
+{
+public:
+    bool checkTwoChessboards(string coordinate1, string coordinate2)
+    {
+        bool ans = false;
+        char col_cord1 = coordinate1[0], col_cord2 = coordinate2[0];
+        int row_cord1 = (int)coordinate1[1], row_cord2 = (int)coordinate2[1];
+        if ((col_cord1 == 'a' || col_cord1 == 'c' || col_cord1 == 'e' || col_cord1 == 'g') && (col_cord2 == 'a' || col_cord2 == 'c' || col_cord2 == 'e' || col_cord2 == 'g') && (row_cord1 % 2 != 0) && (row_cord2 % 2 != 0))
+        {
+            ans = true;
+        }
+        else if ((col_cord1 == 'a' || col_cord1 == 'c' || col_cord1 == 'e' || col_cord1 == 'g') && (col_cord2 == 'b' || col_cord2 == 'd' || col_cord2 == 'f' || col_cord2 == 'h') && (row_cord1 % 2 != 0) && (row_cord2 % 2 == 0))
+        {
+            ans = true;
+        }
+        else if ((col_cord1 == 'b' || col_cord1 == 'd' || col_cord1 == 'f' || col_cord1 == 'h') && (col_cord2 == 'a' || col_cord2 == 'c' || col_cord2 == 'e' || col_cord2 == 'g') && (row_cord1 % 2 == 0) && (row_cord2 % 2 != 0))
+        {
+            ans = true;
+        }
+        else if ((col_cord1 == 'b' || col_cord1 == 'd' || col_cord1 == 'f' || col_cord1 == 'h') && (col_cord2 == 'b' || col_cord2 == 'd' || col_cord2 == 'f' || col_cord2 == 'h') && (row_cord1 % 2 == 0) && (row_cord2 % 2 == 0))
+        {
+            ans = true;
+        }
+        else if ((col_cord1 == 'a' || col_cord1 == 'c' || col_cord1 == 'e' || col_cord1 == 'g') && (col_cord2 == 'a' || col_cord2 == 'c' || col_cord2 == 'e' || col_cord2 == 'g') && (row_cord1 % 2 == 0) && (row_cord2 % 2 == 0))
+        {
+            ans = true;
+        }
+        else if ((col_cord1 == 'a' || col_cord1 == 'c' || col_cord1 == 'e' || col_cord1 == 'g') && (col_cord2 == 'b' || col_cord2 == 'd' || col_cord2 == 'f' || col_cord2 == 'h') && (row_cord1 % 2 == 0) && (row_cord2 % 2 != 0))
+        {
+            ans = true;
+        }
+        else if ((col_cord1 == 'b' || col_cord1 == 'd' || col_cord1 == 'f' || col_cord1 == 'h') && (col_cord2 == 'a' || col_cord2 == 'c' || col_cord2 == 'e' || col_cord2 == 'g') && (row_cord1 % 2 != 0) && (row_cord2 % 2 == 0))
+        {
+            ans = true;
+        }
+        else if ((col_cord1 == 'b' || col_cord1 == 'd' || col_cord1 == 'f' || col_cord1 == 'h') && (col_cord2 == 'b' || col_cord2 == 'd' || col_cord2 == 'f' || col_cord2 == 'h') && (row_cord1 % 2 != 0) && (row_cord2 % 2 != 0))
+        {
+            ans = true;
+        }
+        return ans;
+    }
+};
