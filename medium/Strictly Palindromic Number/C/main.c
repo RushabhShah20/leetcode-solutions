daf3828192c7339b68242016f649fb760d38121e
Rushabh <20rush09abh2001@gmail.com>
2025-04-22 13:04:18 +0530

    Added C solution to 'Strictly Palindromic Number'

---
+++ medium/Strictly Palindromic Number/C/main.c
@@ -0,0 +1,45 @@
+// Problem: Strictly Palindromic Number
+// Link to the problem: https://leetcode.com/problems/strictly-palindromic-number/
+long long int nInary(long long int n, long long int base)
+{
+    long long int result = 0;
+    long long int multiplier = 1;
+    while (n > 0)
+    {
+        result += (n % base) * multiplier;
+        n /= base;
+        multiplier *= 10;
+    }
+    return result;
+}
+
+bool isPalindrome(long long int n)
+{
+    long long int reversed = 0;
+    long long int original = n;
+    while (n > 0)
+    {
+        reversed = reversed * 10 + n % 10;
+        n /= 10;
+    }
+    return original == reversed;
+}
+
+bool isStrictlyPalindromic(int n)
+{
+    bool ans = false;
+    for (int i = 2; i <= n - 2; i++)
+    {
+        int num = nInary(n, i);
+        if (isPalindrome(num))
+        {
+            ans = true;
+        }
+        else
+        {
+            ans = false;
+            break;
+        }
+    }
+    return ans;
+}
