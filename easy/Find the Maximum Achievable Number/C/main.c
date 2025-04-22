35796c328a640f00e8d73dd030dbf77b7533a4f0
Rushabh <20rush09abh2001@gmail.com>
2025-04-22 12:18:07 +0530

    Added C solution to 'Find the Maximum Achievable Number'

---
+++ easy/Find the Maximum Achievable Number/C/main.c
@@ -0,0 +1,6 @@
+// Problem: Find the Maximum Achievable Number
+// Link to the problem: https://leetcode.com/problems/find-the-maximum-achievable-number/
+int theMaximumAchievableX(int num, int t)
+{
+    return num + (t * 2);
+}
