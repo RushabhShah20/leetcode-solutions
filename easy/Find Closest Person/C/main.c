9419f61e84e75771a2b0d7f178864056dd2854f5
Rushabh <20rush09abh2001@gmail.com>
2025-04-22 11:59:41 +0530

    Added C solution to 'Find Closest Person'

---
+++ easy/Find Closest Person/C/main.c
@@ -0,0 +1,17 @@
+// Problem: Find Closest Person
+// Link to the problem: https://leetcode.com/problems/find-closest-person/
+int findClosest(int x, int y, int z)
+{
+    if (abs(x - z) < abs(y - z))
+    {
+        return 1;
+    }
+    else if (abs(x - z) > abs(y - z))
+    {
+        return 2;
+    }
+    else
+    {
+        return 0;
+    }
+}
