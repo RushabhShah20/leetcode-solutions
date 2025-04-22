e1490d6a59f0f641220b2ef7ef1eca9ef15f6eea
Rushabh <20rush09abh2001@gmail.com>
2025-04-22 12:18:20 +0530

    Added Rust solution to 'Find the Maximum Achievable Number'

---
+++ easy/Find the Maximum Achievable Number/Rust/main.rs
@@ -0,0 +1,7 @@
+// Problem: Find the Maximum Achievable Number
+// Link to the problem: https://leetcode.com/problems/find-the-maximum-achievable-number/
+impl Solution {
+    pub fn the_maximum_achievable_x(num: i32, t: i32) -> i32 {
+        return num + (t * 2);
+    }
+}
