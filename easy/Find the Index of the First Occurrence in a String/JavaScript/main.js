f8e663527dd50065e25c7a75827612a3404dcb61
Rushabh <20rush09abh2001@gmail.com>
2025-04-23 15:56:14 +0530

    Added JavaScript solution to 'Find the Index of the First Occurrence in a String'

---
+++ easy/Find the Index of the First Occurrence in a String/JavaScript/main.js
@@ -0,0 +1,10 @@
+// Problem: Find the Index of the First Occurrence in a String
+// Link to the problem: https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function (haystack, needle) {
+    return haystack.indexOf(needle);
+};
