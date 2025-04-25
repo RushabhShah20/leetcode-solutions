25b69af2bb3e69c8614488c3a8456245d7939df9
Rushabh <20rush09abh2001@gmail.com>
2025-04-25 11:40:49 +0530

    Added Go solution to 'Find the Index of the First Occurrence in a String'

---
+++ easy/Find the Index of the First Occurrence in a String/Go/main.go
@@ -0,0 +1,5 @@
+// Problem: Find the Index of the First Occurrence in a String
+// Link to the problem: https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
+func strStr(haystack string, needle string) int {
+	return strings.Index(haystack, needle)
+}
