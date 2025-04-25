f2c3913280e823bf1e03c25128fca1b6ea6056fb
Rushabh <20rush09abh2001@gmail.com>
2025-04-25 11:43:19 +0530

    Added C# solution to 'Find the Index of the First Occurrence in a String'

---
+++ easy/Find the Index of the First Occurrence in a String/C#/main.cs
@@ -0,0 +1,9 @@
+// Problem: Find the Index of the First Occurrence in a String
+// Link to the problem: https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
+public class Solution
+{
+    public int StrStr(string haystack, string needle)
+    {
+        return haystack.IndexOf(needle);
+    }
+}
