156e3bf0175f98879df202ea9c54896ad81d69e2
Rushabh <20rush09abh2001@gmail.com>
2025-04-27 12:33:03 +0530

    Added JavaScript solution to 'Replace Elements with Greatest Element on Right Side'

---
+++ easy/Replace Elements with Greatest Element on Right Side/JavaScript/main.js
@@ -0,0 +1,24 @@
+// Problem: Replace Elements with Greatest Element on Right Side
+// Link to the problem: https://leetcode.com/problems/replace-elements-with-greatest-element-on-right-side/
+/**
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+var replaceElements = function (arr) {
+    let ans = new Array();
+    ans.push(-1);
+    let max = -1;
+    if (arr.length == 1) {
+        return ans;
+    }
+    else {
+        for (let i = arr.length - 1; i >= 1; i--) {
+            if (max < arr[i]) {
+                max = arr[i];
+            }
+            ans.push(max);
+        }
+        ans.reverse();
+        return ans;
+    }
+};
