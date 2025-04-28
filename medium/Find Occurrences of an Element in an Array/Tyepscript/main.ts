4fc309ddaf5638beb2fde544d37a46aa271504b0
Rushabh <20rush09abh2001@gmail.com>
2025-04-28 15:55:36 +0530

    Added Typescript solution to 'Find Occurrences of an Element in an Array'

---
+++ medium/Find Occurrences of an Element in an Array/Tyepscript/main.ts
@@ -0,0 +1,17 @@
+// Problem: Find Occurrences of an Element in an Array
+// Link to the problem: https://leetcode.com/problems/find-occurrences-of-an-element-in-an-array/
+function occurrencesOfElement(nums: number[], queries: number[], x: number): number[] {
+    let ans = new Array(queries.length).fill(-1);
+    let freq = [];
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] == x) {
+            freq.push(i);
+        }
+    }
+    for (let i = 0; i < ans.length; i++) {
+        if (queries[i] <= freq.length) {
+            ans[i] = freq[queries[i] - 1];
+        }
+    }
+    return ans;
+};
