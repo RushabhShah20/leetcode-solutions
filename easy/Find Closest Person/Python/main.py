8878e4c2f962af080973be03fdd608858c2c2968
Rushabh <20rush09abh2001@gmail.com>
2025-04-29 11:54:03 +0530

    Added Python solution to 'Find Closest Person'

---
+++ easy/Find Closest Person/Python/main.py
@@ -0,0 +1,10 @@
+# Problem: Find Closest Person
+# Link to the problem: https://leetcode.com/problems/find-closest-person/
+class Solution:
+    def findClosest(self, x: int, y: int, z: int) -> int:
+        if abs(x - z) < abs(y - z):
+            return 1
+        elif abs(x - z) > abs(y - z):
+            return 2
+        else:
+            return 0
