634c1f40abd021de9368d3db7187b6dd64f66948
Rushabh <20rush09abh2001@gmail.com>
2025-05-02 11:48:15 +0530

    Added C++ solution to 'Max Consecutive Ones'

---
+++ easy/Max Consecutive Ones/C++/main.cpp
@@ -0,0 +1,23 @@
+// Problem: Max Consecutive Ones
+// Link to the problem: https://leetcode.com/problems/max-consecutive-ones/
+class Solution
+{
+public:
+    int findMaxConsecutiveOnes(vector<int> &nums)
+    {
+        int ans = 0, count = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] == 1)
+            {
+                count++;
+                ans = max(ans, count);
+            }
+            else
+            {
+                count = 0;
+            }
+        }
+        return ans;
+    }
+};
