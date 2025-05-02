b353de4a0b3c4cd30861c2392cf745039e6c5dab
Rushabh <20rush09abh2001@gmail.com>
2025-05-02 12:20:05 +0530

    Added C++ solution to 'Majority Element'

---
+++ easy/Majority Element/C++/main.cpp
@@ -0,0 +1,17 @@
+// Problem: Majority Element
+// Link to the problem: https://leetcode.com/problems/majority-element/
+class Solution
+{
+public:
+    int majorityElement(vector<int> &nums)
+    {
+        int element = -1, count = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (count == 0)
+                element = nums[i];
+            count += (element == nums[i]) ? 1 : -1;
+        }
+        return element;
+    }
+};
