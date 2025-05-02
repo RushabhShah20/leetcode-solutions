ed0f423840cbb44f1bb169243e653961925603c2
Rushabh <20rush09abh2001@gmail.com>
2025-05-02 12:35:42 +0530

    Added Dart solution to 'Majority Element'

---
+++ easy/Majority Element/Dart/main.dart
@@ -0,0 +1,12 @@
+// Problem: Majority Element
+// Link to the problem: https://leetcode.com/problems/majority-element/
+class Solution {
+  int majorityElement(List<int> nums) {
+    int element = -1, count = 0;
+    for (int i = 0; i < nums.length; i++) {
+      if (count == 0) element = nums[i];
+      count += (element == nums[i]) ? 1 : -1;
+    }
+    return element;
+  }
+}
