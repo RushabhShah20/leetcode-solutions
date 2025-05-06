520b301bcfb2156cb00fd8868852e95c4da0044b
Rushabh <20rush09abh2001@gmail.com>
2025-05-06 10:52:49 +0530

    Added C solution to 'Ugly Number'

---
+++ easy/Ugly Number/C/main.c
@@ -0,0 +1,25 @@
+// Problem: Ugly Number
+// Link to the problem: https://leetcode.com/problems/ugly-number/
+bool isUgly(int n)
+{
+    if (n <= 0)
+    {
+        return false;
+    }
+    else
+    {
+        while (n % 2 == 0)
+        {
+            n /= 2;
+        }
+        while (n % 3 == 0)
+        {
+            n /= 3;
+        }
+        while (n % 5 == 0)
+        {
+            n /= 5;
+        }
+        return n == 1;
+    }
+}
