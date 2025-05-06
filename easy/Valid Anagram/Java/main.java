5cd422d2533f6493bc71f691f77a8cfa44a7e3cc
Rushabh <20rush09abh2001@gmail.com>
2025-05-06 11:42:20 +0530

    Added Java solution to 'Valid Anagram'

---
+++ easy/Valid Anagram/Java/main.java
@@ -0,0 +1,15 @@
+// Problem: Valid Anagram
+// Link to the problem: https://leetcode.com/problems/valid-anagram/
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character, Integer> m1 = new HashMap<>();
+        HashMap<Character, Integer> m2 = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            m1.put(s.charAt(i), m1.getOrDefault(s.charAt(i), 0) + 1);
+        }
+        for (int i = 0; i < t.length(); i++) {
+            m2.put(t.charAt(i), m2.getOrDefault(t.charAt(i), 0) + 1);
+        }
+        return m1.equals(m2);
+    }
+}
