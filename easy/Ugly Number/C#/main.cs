8745ea3b37e46214d4f24d108402a999d67d10c2
Rushabh <20rush09abh2001@gmail.com>
2025-05-06 10:53:20 +0530

    Added C# solution to 'Ugly Number'

---
+++ easy/Ugly Number/C#/main.cs
@@ -0,0 +1,28 @@
+// Problem: Ugly Number
+// Link to the problem: https://leetcode.com/problems/ugly-number/
+public class Solution
+{
+    public bool IsUgly(int n)
+    {
+        if (n <= 0)
+        {
+            return false;
+        }
+        else
+        {
+            while (n % 2 == 0)
+            {
+                n /= 2;
+            }
+            while (n % 3 == 0)
+            {
+                n /= 3;
+            }
+            while (n % 5 == 0)
+            {
+                n /= 5;
+            }
+            return n == 1;
+        }
+    }
+}
