9637a667f6f55d20062e9416392aa7c94a9dc1c1
Rushabh <20rush09abh2001@gmail.com>
2025-05-07 18:16:55 +0530

    Added C# solution to 'Number of 1 Bits'

---
+++ easy/Number of 1 Bits/C#/main.cs
@@ -0,0 +1,15 @@
+// Problem: Number of 1 Bits
+// Link to the problem: https://leetcode.com/problems/number-of-1-bits/
+public class Solution
+{
+    public int HammingWeight(int n)
+    {
+        int ans = 0;
+        while (n != 0)
+        {
+            n &= (n - 1);
+            ans++;
+        }
+        return ans;
+    }
+}
