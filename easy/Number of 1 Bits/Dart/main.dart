61545ad73894a2df2d6bbb8b7b9c0743ab5ceacf
Rushabh <20rush09abh2001@gmail.com>
2025-05-07 18:18:12 +0530

    Added Dart solution to 'Number of 1 Bits'

---
+++ easy/Number of 1 Bits/Dart/main.dart
@@ -0,0 +1,12 @@
+// Problem: Number of 1 Bits
+// Link to the problem: https://leetcode.com/problems/number-of-1-bits/
+class Solution {
+  int hammingWeight(int n) {
+    int ans = 0;
+    while (n != 0) {
+      n &= (n - 1);
+      ans++;
+    }
+    return ans;
+  }
+}
