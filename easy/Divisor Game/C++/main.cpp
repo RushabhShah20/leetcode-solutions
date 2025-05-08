4b4dec4b804a256547aa5566bafd648cfeb2a82a
Rushabh <20rush09abh2001@gmail.com>
2025-05-08 19:22:25 +0530

    Added C++ solution to 'Divisor Game'

---
+++ easy/Divisor Game/C++/main.cpp
@@ -0,0 +1,10 @@
+// Problem: Divisor Game
+// Link to the problem: https://leetcode.com/problems/divisor-game/
+class Solution
+{
+public:
+    bool divisorGame(int n)
+    {
+        return n % 2 == 0;
+    }
+};
