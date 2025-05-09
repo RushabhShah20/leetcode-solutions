51d6abf11603ce95a1668f925726d1fec9a78c87
Rushabh <20rush09abh2001@gmail.com>
2025-05-09 12:03:30 +0530

    Added Rust solution to 'Divisor Game'

---
+++ easy/Divisor Game/Rust/main.rs
@@ -0,0 +1,7 @@
+// Problem: Divisor Game
+// Link to the problem: https://leetcode.com/problems/divisor-game/
+impl Solution {
+    pub fn divisor_game(n: i32) -> bool {
+        return n % 2 == 0;
+    }
+}
