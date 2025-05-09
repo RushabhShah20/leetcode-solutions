29c12f75c5a7dc0d2c64bcc2a49162e11616c8af
Rushabh <20rush09abh2001@gmail.com>
2025-05-09 12:04:33 +0530

    Added Scala solution to 'Divisor Game'

---
+++ easy/Divisor Game/Scala/main.scala
@@ -0,0 +1,7 @@
+// Problem: Divisor Game
+// Link to the problem: https://leetcode.com/problems/divisor-game/
+object Solution {
+    def divisorGame(n: Int): Boolean = {
+        return n%2==0;
+    }
+}
