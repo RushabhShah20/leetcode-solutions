142c52086fd1f800a11f31924a5e5642bafbcfe7
Rushabh <20rush09abh2001@gmail.com>
2025-05-09 12:01:37 +0530

    Added JavaScript solution to 'Divisor Game'

---
+++ easy/Divisor Game/JavaScript/main.js
@@ -0,0 +1,9 @@
+// Problem: Divisor Game
+// Link to the problem: https://leetcode.com/problems/divisor-game/
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var divisorGame = function(n) {
+    return n % 2 === 0;
+};
