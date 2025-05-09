6758295e08df8f4c5138e5f2bf1a20455d5930df
Rushabh <20rush09abh2001@gmail.com>
2025-05-09 12:00:22 +0530

    Added Java solution to 'Divisor Game'

---
+++ easy/Divisor Game/Java/main.java
@@ -0,0 +1,7 @@
+// Problem: Divisor Game
+// Link to the problem: https://leetcode.com/problems/divisor-game/
+class Solution {
+    public boolean divisorGame(int n) {
+        return n % 2 == 0;
+    }
+}
