2328634d73f415676e7a6dc0b5ac57251efb11e9
Rushabh <20rush09abh2001@gmail.com>
2025-05-09 12:04:49 +0530

    Added PHP solution to 'Divisor Game'

---
+++ easy/Divisor Game/PHP/main.php
@@ -0,0 +1,12 @@
+// Problem: Divisor Game
+// Link to the problem: https://leetcode.com/problems/divisor-game/
+class Solution {
+
+/**
+ * @param Integer $n
+ * @return Boolean
+ */
+function divisorGame($n) {
+    return $n%2==0;
+}
+}
