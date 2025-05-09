2202c02e993e1e9a85d11fd20121fb92effc9ddd
Rushabh <20rush09abh2001@gmail.com>
2025-05-09 12:01:10 +0530

    Added C# solution to 'Divisor Game'

---
+++ easy/Divisor Game/C#/main.cs
@@ -0,0 +1,9 @@
+// Problem: Divisor Game
+// Link to the problem: https://leetcode.com/problems/divisor-game/
+public class Solution
+{
+    public bool DivisorGame(int n)
+    {
+        return n % 2 == 0;
+    }
+}
