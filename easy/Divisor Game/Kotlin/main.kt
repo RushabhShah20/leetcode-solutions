19e30308422a3f193eb76e302208f2046a62ec35
Rushabh <20rush09abh2001@gmail.com>
2025-05-09 12:03:46 +0530

    Added Kotlin solution to 'Divisor Game'

---
+++ easy/Divisor Game/Kotlin/main.kt
@@ -0,0 +1,7 @@
+// Problem: Divisor Game
+// Link to the problem: https://leetcode.com/problems/divisor-game/
+class Solution {
+    fun divisorGame(n: Int): Boolean {
+        return n % 2 == 0; 
+    }
+}
