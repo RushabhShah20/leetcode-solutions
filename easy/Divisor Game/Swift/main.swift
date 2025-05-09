2d87aa03f0ba0e4b5ae65dc862ac57ae80dfccf1
Rushabh <20rush09abh2001@gmail.com>
2025-05-09 12:04:01 +0530

    Added Swift solution to 'Divisor Game'

---
+++ easy/Divisor Game/Swift/main.swift
@@ -0,0 +1,7 @@
+// Problem: Divisor Game
+// Link to the problem: https://leetcode.com/problems/divisor-game/
+class Solution {
+    func divisorGame(_ n: Int) -> Bool {
+        return n % 2 == 0;
+    }
+}
