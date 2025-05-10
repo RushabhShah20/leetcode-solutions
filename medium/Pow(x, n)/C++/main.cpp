69733c511707a66b10c4a640b30f159606eb4b60
Rushabh <20rush09abh2001@gmail.com>
2025-05-10 15:59:19 +0530

    Added C++ solution to 'Pow(x, n)'

---
+++ medium/Pow(x, n)/C++/main.cpp
@@ -0,0 +1,27 @@
+// Problem: Pow(x, n)
+// Link to the problem: https://leetcode.com/problems/powx-n/
+class Solution
+{
+public:
+    double myPow(double x, int n)
+    {
+        long t = n;
+        if (n == 0)
+        {
+            return 1;
+        }
+        if (t < 0)
+        {
+            t = -1 * t;
+            x = 1 / x;
+        }
+        if (n % 2 == 0)
+        {
+            return myPow(x * x, (int)(t / 2));
+        }
+        else
+        {
+            return x * myPow(x, (int)(t - 1));
+        }
+    }
+};
