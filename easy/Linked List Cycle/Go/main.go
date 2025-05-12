702da131b1cc1730143e83d36ee82964e2dae185
Rushabh <20rush09abh2001@gmail.com>
2025-05-12 12:27:57 +0530

    Added Go solution to 'Linked List Cycle'

---
+++ easy/Linked List Cycle/Go/main.go
@@ -0,0 +1,21 @@
+// Problem: Linked List Cycle
+// Link to the problem: https://leetcode.com/problems/linked-list-cycle/
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func hasCycle(head *ListNode) bool {
+	var fast *ListNode = head
+	var slow *ListNode = head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if slow == fast {
+			return true
+		}
+	}
+	return false
+}
