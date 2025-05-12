36c19de2a1c88e6deac4c11c890a0ccd5d43e026
Rushabh <20rush09abh2001@gmail.com>
2025-05-12 15:51:02 +0530

    Added C++ solution to 'Remove Linked List Elements'

---
+++ easy/Remove Linked List Elements/C++/main.cpp
@@ -0,0 +1,37 @@
+// Problem: Remove Linked List Elements
+// Link to the problem: https://leetcode.com/problems/remove-linked-list-elements/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *removeElements(ListNode *head, int val)
+    {
+        if (head == NULL)
+        {
+            return head;
+        }
+        ListNode *dummy = new ListNode(0, head);
+        ListNode *cur = dummy;
+        while (cur->next != NULL)
+        {
+            if (cur->next->val == val)
+            {
+                cur->next = cur->next->next;
+            }
+            else
+            {
+                cur = cur->next;
+            }
+        }
+        return dummy->next;
+    }
+};
