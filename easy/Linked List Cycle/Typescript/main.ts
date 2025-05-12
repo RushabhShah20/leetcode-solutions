677bf2b03871f1494d1a7e252098e775366181e9
Rushabh <20rush09abh2001@gmail.com>
2025-05-12 12:27:39 +0530

    Added Typescript solution to 'Linked List Cycle'

---
+++ easy/Linked List Cycle/Typescript/main.ts
@@ -0,0 +1,26 @@
+// Problem: Linked List Cycle
+// Link to the problem: https://leetcode.com/problems/linked-list-cycle/
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function hasCycle(head: ListNode | null): boolean {
+    let fast: ListNode | null = head;
+    let slow: ListNode | null = head;
+    while (fast !== null && fast.next !== null) {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (slow === fast) {
+            return true;
+        }
+    }
+    return false;
+};
