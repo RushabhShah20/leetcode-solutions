c6c7a152a47dd3a246e1e7c88ede84b16d293b40
Rushabh <20rush09abh2001@gmail.com>
2025-05-12 12:27:02 +0530

    Added C# solution to 'Linked List Cycle'

---
+++ easy/Linked List Cycle/C#/main.cs
@@ -0,0 +1,31 @@
+// Problem: Linked List Cycle
+// Link to the problem: https://leetcode.com/problems/linked-list-cycle/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public bool HasCycle(ListNode head)
+    {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
