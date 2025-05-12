58fe15919b81409ef5dc5455400a72b4ec417398
Rushabh <20rush09abh2001@gmail.com>
2025-05-12 12:01:44 +0530

    Added Java solution to 'Find Most Frequent Vowel And Consonent'

---
+++ easy/Find Most Frequent Vowel and Consonant/Java/main.java
@@ -0,0 +1,28 @@
+// Problem: Find Most Frequent Vowel And Consonent
+// Link to the problem: https://leetcode.com/problems/find-most-frequent-vowel-and-consonent
+class Solution {
+    public int maxFreqSum(String s) {
+        HashMap<Character, Integer> vowels = new HashMap<>();
+        HashMap<Character, Integer> consonents = new HashMap<>();
+        int max_vowels = 0, max_consonents = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o'
+                    || s.charAt(i) == 'u') {
+                vowels.put(s.charAt(i), vowels.getOrDefault(s.charAt(i), 0) + 1);
+            } else {
+                consonents.put(s.charAt(i), consonents.getOrDefault(s.charAt(i), 0) + 1);
+            }
+        }
+        for (var i : vowels.keySet()) {
+            if (vowels.get(i) > max_vowels) {
+                max_vowels = vowels.get(i);
+            }
+        }
+        for (var i : consonents.keySet()) {
+            if (consonents.get(i) > max_consonents) {
+                max_consonents = consonents.get(i);
+            }
+        }
+        return max_vowels + max_consonents;
+    }
+}
