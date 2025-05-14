63aaf8fa0a8bade8f9f7265793686b2eb7aad398
Rushabh <20rush09abh2001@gmail.com>
2025-05-14 12:31:39 +0530

    Added Dart solution to 'Same Tree'

---
+++ easy/Same Tree/Dart/main.dart
@@ -0,0 +1,25 @@
+// Problem: Same Tree
+// Link to the problem: https://leetcode.com/problems/same-tree/
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *   int val;
+ *   TreeNode? left;
+ *   TreeNode? right;
+ *   TreeNode([this.val = 0, this.left, this.right]);
+ * }
+ */
+class Solution {
+  bool isSameTree(TreeNode? p, TreeNode? q) {
+    if (p == null && q == null) {
+      return true;
+    }
+    if (p == null || q == null) {
+      return false;
+    }
+    if (p.val == q.val) {
+      return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+    return false;
+  }
+}
