3e2b573d91f5ba1e65bc504da70b417b961ccba9
Rushabh <20rush09abh2001@gmail.com>
2025-05-15 15:03:57 +0530

    Added JavaScript solution to 'Binary Search'

---
+++ easy/Binary Search/JavaScript/main.js
@@ -0,0 +1,22 @@
+// Problem: Binary Search
+// Link to the problem: https://leetcode.com/problems/binary-search/
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+    let start = 0;
+    let end = nums.length - 1;
+    while (start <= end) {
+        let mid = (start + (end - start) / 2) >> 0;
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
+    }
+    return -1;
+};
