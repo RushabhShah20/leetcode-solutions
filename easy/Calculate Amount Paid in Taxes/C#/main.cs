16bf3d8c4b898608fad952992b4b764dc8d15da0
Rushabh <20rush09abh2001@gmail.com>
2025-05-16 12:10:50 +0530

    Added C# solution to 'Calculate Amount Paid in Taxes'

---
+++ easy/Calculate Amount Paid in Taxes/C#/main.cs
@@ -0,0 +1,31 @@
+// Problem: Calculate Amount Paid in Taxes
+// Link to the problem: https://leetcode.com/problems/calculate-amount-paid-in-taxes/
+public class Solution
+{
+    public double CalculateTax(int[][] brackets, int income)
+    {
+        double ans = 0;
+        if (income >= brackets[0][0])
+        {
+            ans = ((brackets[0][0] * brackets[0][1]) / 100.0);
+        }
+        else
+        {
+            return ((income * brackets[0][1]) / 100.0);
+        }
+        for (int i = 1; i < brackets.Length; i++)
+        {
+            if (income >= (brackets[i][0]))
+            {
+                ans += (((brackets[i][0] - brackets[i - 1][0]) * brackets[i][1]) / 100.0);
+            }
+            else
+            {
+                ans += (((income - brackets[i - 1][0]) * brackets[i][1]) / 100.0);
+                break;
+            }
+        }
+        return ans;
+
+    }
+}
