9c32878847487f54a532b6b299682bb1c4c09899
Rushabh <20rush09abh2001@gmail.com>
2025-05-16 12:10:37 +0530

    Added Java solution to 'Calculate Amount Paid in Taxes'

--- easy/Calculate Amount Paid in Taxes/Java/main.java
+++ easy/Calculate Amount Paid in Taxes/Java/main.java
@@ -1,31 +1,21 @@
 // Problem: Calculate Amount Paid in Taxes
 // Link to the problem: https://leetcode.com/problems/calculate-amount-paid-in-taxes/
-class Solution
-{
-public:
-    double calculateTax(vector<vector<int>> &brackets, int income)
-    {
+class Solution {
+    public double calculateTax(int[][] brackets, int income) {
         double ans = 0;
-        if (income >= brackets[0][0])
-        {
+        if (income >= brackets[0][0]) {
             ans = ((brackets[0][0] * brackets[0][1]) / 100.0);
-        }
-        else
-        {
+        } else {
             return ((income * brackets[0][1]) / 100.0);
         }
-        for (int i = 1; i < brackets.size(); i++)
-        {
-            if (income >= (brackets[i][0]))
-            {
+        for (int i = 1; i < brackets.length; i++) {
+            if (income >= (brackets[i][0])) {
                 ans += (((brackets[i][0] - brackets[i - 1][0]) * brackets[i][1]) / 100.0);
-            }
-            else
-            {
+            } else {
                 ans += (((income - brackets[i - 1][0]) * brackets[i][1]) / 100.0);
                 break;
             }
         }
         return ans;
     }
-};
+}
