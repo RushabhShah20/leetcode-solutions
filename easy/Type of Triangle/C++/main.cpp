1201de93d375c8f5683eded9470aa8aa6c267326
Rushabh <20rush09abh2001@gmail.com>
2025-05-19 18:24:09 +0530

    Added C++ solution to 'Type of Triangle'

---
+++ easy/Type of Triangle/C++/main.cpp
@@ -0,0 +1,26 @@
+// Problem: Type of Triangle
+// Link to the problem: https://leetcode.com/problems/type-of-triangle/
+class Solution
+{
+public:
+    string triangleType(vector<int> &nums)
+    {
+        int a = nums[0], b = nums[1], c = nums[2];
+        if (a + b > c && b + c > a && c + a > b)
+        {
+            if (a == b && b == c && c == a)
+            {
+                return "equilateral";
+            }
+            else if (a == b || b == c || c == a)
+            {
+                return "isosceles";
+            }
+            else
+            {
+                return "scalene";
+            }
+        }
+        return "none";
+    }
+};
