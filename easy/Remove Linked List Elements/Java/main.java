7787a2e27c5c3bf2307edfd8d730bc858faf2fbf
Rushabh <20rush09abh2001@gmail.com>
2025-05-24 12:19:07 +0530

    Added Java solution to 'Remove Linked List Elements'

---
+++ easy/Remove Linked List Elements/Java/main.java
@@ -0,0 +1,29 @@
+// Problem: Remove Linked List Elements
+// Link to the problem: https://leetcode.com/problems/remove-linked-list-elements/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        if (head == null) {
+            return head;
+        }
+        ListNode dummy = new ListNode(0, head);
+        ListNode cur = dummy;
+        while (cur.next != null) {
+            if (cur.next.val == val) {
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;
+            }
+        }
+        return dummy.next;
+    }
+}
