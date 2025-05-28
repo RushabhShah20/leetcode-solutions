edddcd6007d2b7fbd32c8be1a068ad58d12e3be6
Rushabh <20rush09abh2001@gmail.com>
2025-05-28 18:41:42 +0530

    Added Java solution to 'Divisible and Non-divisible Sums Difference'

---
+++ easy/Divisible and Non-divisible Sums Difference/Java/main.java
@@ -0,0 +1,15 @@
+// Problem: Divisible and Non-divisible Sums Difference
+// Link to the problem: https://leetcode.com/problems/divisible-and-non-divisible-sums-difference/
+class Solution {
+    public int differenceOfSums(int n, int m) {
+        int suma = 0, sumb = 0;
+        for (int i = 1; i <= n; i++) {
+            if (i % m == 0) {
+                sumb += i;
+            } else {
+                suma += i;
+            }
+        }
+        return suma - sumb;
+    }
+}
