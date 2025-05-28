7b66967efb73e3bfeb3bdb50b74e37002310a94c
Rushabh <20rush09abh2001@gmail.com>
2025-05-28 12:13:08 +0530

    Added Dart solution to 'Angle Between Hands of a Clock'

---
+++ medium/Angle Between Hands of a Clock/Dart/main.dart
@@ -0,0 +1,11 @@
+// Problem: Angle Between Hands of a Clock
+// Link to the problem: https://leetcode.com/problems/angle-between-hands-of-a-clock/
+class Solution {
+  double angleClock(int hour, int minutes) {
+    double hour_angle = 0.5 * (hour * 60 + minutes);
+    double minute_angle = 6.0 * minutes;
+    double angle = (hour_angle - minute_angle).abs();
+    angle = min(360 - angle, angle);
+    return angle;
+  }
+}
