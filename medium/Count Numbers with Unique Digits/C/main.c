993250c0eba766ec44266c1f3b47e5030f7b5d7a
Rushabh <20rush09abh2001@gmail.com>
2025-05-30 13:45:00 +0530

    Added C solution to 'Count Numbers with Unique Digits'

---
+++ medium/Count Numbers with Unique Digits/C/main.c
@@ -0,0 +1,21 @@
+// Problem: Count Numbers with Unique Digits
+// Link to the problem: https://leetcode.com/problems/count-numbers-with-unique-digits/
+int countNumbersWithUniqueDigits(int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    if (n == 1)
+    {
+        return 10;
+    }
+    int total = 10;
+    int current = 9;
+    for (int i = 2; i <= n; ++i)
+    {
+        current *= (10 - (i - 1));
+        total += current;
+    }
+    return total;
+}
