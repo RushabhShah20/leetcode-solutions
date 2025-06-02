abf2dd4338f6e28450249121749a174ac7b549d5
Rushabh <20rush09abh2001@gmail.com>
2025-06-02 12:21:13 +0530

    Added C solution to 'N-th Tribonacci Number'

---
+++ easy/N-th Tribonacci Number/C/main.c
@@ -0,0 +1,18 @@
+// Problem: N-th Tribonacci Number
+// Link to the problem: https://leetcode.com/problems/n-th-tribonacci-number/
+int tribonacci(int n)
+{
+    if (n < 2)
+    {
+        return n;
+    }
+    int a = 0, b = 1, c = 1, d;
+    while (n-- > 2)
+    {
+        d = a + b + c;
+        a = b;
+        b = c;
+        c = d;
+    }
+    return c;
+}
