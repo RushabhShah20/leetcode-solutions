bc5baa4bc3b56b5d0268c8df3dfa4dd3bd81dc27
Rushabh <20rush09abh2001@gmail.com>
2025-06-02 12:55:16 +0530

    Added C solution to 'Type of Triangle'

---
+++ easy/Type of Triangle/C/main.c
@@ -0,0 +1,22 @@
+// Problem: Type of Triangle
+// Link to the problem: https://leetcode.com/problems/type-of-triangle/
+char *triangleType(int *nums, int numsSize)
+{
+    int a = nums[0], b = nums[1], c = nums[2];
+    if (a + b > c && b + c > a && c + a > b)
+    {
+        if (a == b && b == c && c == a)
+        {
+            return "equilateral";
+        }
+        else if (a == b || b == c || c == a)
+        {
+            return "isosceles";
+        }
+        else
+        {
+            return "scalene";
+        }
+    }
+    return "none";
+}
