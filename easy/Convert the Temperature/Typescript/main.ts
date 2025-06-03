0514b1addd0547d7f0c43a4e3c28762386274e54
Rushabh <20rush09abh2001@gmail.com>
2025-06-03 14:08:27 +0530

    Added Typescript solution to 'Convert the Temperature'

---
+++ easy/Convert the Temperature/Typescript/main.ts
@@ -0,0 +1,8 @@
+// Problem: Convert the Temperature
+// Link to the problem: https://leetcode.com/problems/convert-the-temperature/
+function convertTemperature(celsius: number): number[] {
+    let ans: number[] = new Array(2).fill(0);
+    ans[0] = celsius + 273.15;
+    ans[1] = celsius * 1.8 + 32;
+    return ans;
+};
