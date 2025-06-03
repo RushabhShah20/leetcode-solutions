c3904f420e94c2004dae51c63fc1f720f204755b
Rushabh <20rush09abh2001@gmail.com>
2025-06-03 11:20:57 +0530

    Added C++ solution to 'Valid Perfect Square'

---
+++ easy/Valid Perfect Square/C++/main.cpp
@@ -0,0 +1,27 @@
+// Problem: Valid Perfect Square
+// Link to the problem: https://leetcode.com/problems/valid-perfect-square/
+class Solution
+{
+public:
+    bool isPerfectSquare(int num)
+    {
+        long long s = 1, e = num;
+        while (s <= e)
+        {
+            long long mid = s + (e - s) / 2;
+            if (mid * mid == num)
+            {
+                return true;
+            }
+            else if (mid * mid > num)
+            {
+                e = mid - 1;
+            }
+            else
+            {
+                s = mid + 1;
+            }
+        }
+        return false;
+    }
+};
