90f32c803bccc161dd79bc89d031e65e7cf3cb9c
Rushabh <20rush09abh2001@gmail.com>
2025-06-03 11:21:52 +0530

    Added C# solution to 'Valid Perfect Square'

---
+++ easy/Valid Perfect Square/C#/main.cs
@@ -0,0 +1,24 @@
+// Problem: Valid Perfect Square
+// Link to the problem: https://leetcode.com/problems/valid-perfect-square/
+public class Solution {
+    public bool IsPerfectSquare(int num) {
+    long s = 1, e = num;
+    while (s <= e)
+    {
+        long mid = s + (e - s) / 2;
+        if (mid * mid == num)
+        {
+            return true;
+        }
+        else if (mid * mid > num)
+        {
+            e = mid - 1;
+        }
+        else
+        {
+            s = mid + 1;
+        }
+    }
+    return false;
+    }
+}
