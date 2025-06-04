1bc06bc9ed178335cf069209495f98e5c52a6381
Rushabh <20rush09abh2001@gmail.com>
2025-06-04 12:42:23 +0530

    Added Java solution to 'Missing Number'

---
+++ easy/Missing Number/Java/main.java
@@ -0,0 +1,11 @@
+// Problem: Missing Number
+// Link to the problem: https://leetcode.com/problems/missing-number/
+class Solution {
+    public int missingNumber(int[] nums) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+        return ((nums.length * (nums.length + 1)) / 2) - sum;
+    }
+}
