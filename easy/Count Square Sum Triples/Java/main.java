b5090c42c41eb0ba889d51d9bc14b53e16c33821
Rushabh <20rush09abh2001@gmail.com>
2025-06-04 12:31:19 +0530

    Added Java solution to 'Count Square Sum Triples'

---
+++ easy/Count Square Sum Triples/Java/main.java
@@ -0,0 +1,17 @@
+// Problem: Count Square Sum Triples
+// Link to the problem: https://leetcode.com/problems/count-square-sum-triples/
+class Solution {
+    public int countTriples(int n) {
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j <= n; j++) {
+                for (int k = i; k <= n; k++) {
+                    if (i * i + j * j == k * k) {
+                        count = count + 2;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}
