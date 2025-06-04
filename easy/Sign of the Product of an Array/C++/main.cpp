e59b38f5f5f278f9b381218f710782e524712667
Rushabh <20rush09abh2001@gmail.com>
2025-06-04 12:19:25 +0530

    Added C++ solution to 'Sign of the Product of an Array'

---
+++ easy/Sign of the Product of an Array/C++/main.cpp
@@ -0,0 +1,18 @@
+// Problem: Sign of the Product of an Array
+// Link to the problem: https://leetcode.com/problems/sign-of-the-product-of-an-array/
+class Solution
+{
+public:
+    int arraySign(vector<int> &nums)
+    {
+        int sign = 1;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] == 0)
+                return 0;
+            else if (nums[i] < 0)
+                sign *= -1;
+        }
+        return sign;
+    }
+};
