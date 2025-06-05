921385d7851a8187b94d92cb0fa12d9b5470035f
Rushabh <20rush09abh2001@gmail.com>
2025-06-05 12:48:12 +0530

    Added C# solution to 'Sum of Digits in Base K'

---
+++ easy/Sum of Digits in Base K/C#/main.cs
@@ -0,0 +1,15 @@
+// Problem: Sum of Digits in Base K
+// Link to the problem: https://leetcode.com/problems/sum-of-digits-in-base-k/
+public class Solution
+{
+    public int SumBase(int n, int k)
+    {
+        int sum = 0;
+        while (n > 0)
+        {
+            sum += n % k;
+            n /= k;
+        }
+        return sum;
+    }
+}
