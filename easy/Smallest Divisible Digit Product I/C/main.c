df5ca7e52b8da39f0c9433cb9aa44ddae62df5ed
Rushabh <20rush09abh2001@gmail.com>
2025-06-06 12:45:36 +0530

    Added C solution to 'Smallest Divisible Digit Product I'

---
+++ easy/Smallest Divisible Digit Product I/C/main.c
@@ -0,0 +1,25 @@
+// Problem: Smallest Divisible Digit Product I
+// Link to the problem: https://leetcode.com/problems/smallest-divisible-digit-product-i/
+int getProduct(int n)
+{
+    int product = 1;
+    while (n != 0)
+    {
+        product = product * (n % 10);
+        n = n / 10;
+    }
+    return product;
+}
+int smallestNumber(int n, int t)
+{
+    int ans = n;
+    for (int i = n; i <= n + (10 - (n % 10)); i++)
+    {
+        if (getProduct(i) % t == 0)
+        {
+            ans = i;
+            break;
+        }
+    }
+    return ans;
+}
