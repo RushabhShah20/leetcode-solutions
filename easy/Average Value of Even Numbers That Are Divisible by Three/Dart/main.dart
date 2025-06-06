de3afc9ea3000ffc0da4884e1720a67bd5a75380
Rushabh <20rush09abh2001@gmail.com>
2025-06-06 13:02:02 +0530

    Added Dart solution to 'Average Value of Even Numbers That Are Divisible by Three'

---
+++ easy/Average Value of Even Numbers That Are Divisible by Three/Dart/main.dart
@@ -0,0 +1,18 @@
+// Problem: Average Value of Even Numbers That Are Divisible by Three
+// Link to the problem: https://leetcode.com/problems/average-value-of-even-numbers-that-are-divisible-by-three/
+class Solution {
+  int averageValue(List<int> nums) {
+    int i;
+    int sum = 0, s = 0;
+    for (i = 0; i < nums.length; i++) {
+      if (nums[i] % 6 == 0) {
+        sum = sum + nums[i];
+        s++;
+      }
+    }
+    if (s == 0)
+      return 0;
+    else
+      return sum ~/ s;
+  }
+}
