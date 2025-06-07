a910b9b57f8f448c5ed41a15cae1907c61326bdf
Rushabh <20rush09abh2001@gmail.com>
2025-06-07 19:00:24 +0530

    Added Java solution to 'Complement of Base 10 Integer'

---
+++ easy/Complement of Base 10 Integer/Java/main.java
@@ -0,0 +1,38 @@
+// Problem: Complement of Base 10 Integer
+// Link to the problem: https://leetcode.com/problems/complement-of-base-10-integer/
+class Solution {
+    List<Integer> toBinary(int n) {
+        List<Integer> ans = new ArrayList<>();
+        while (n > 0) {
+            int rem = n % 2;
+            ans.add(rem);
+            n /= 2;
+        }
+        return ans;
+    }
+
+    List<Integer> complement(List<Integer> res) {
+        for (int i = 0; i < res.size(); i++) {
+            if (res.get(i) == 1) {
+                res.set(i, 0);
+            } else if (res.get(i) == 0) {
+                res.set(i, 1);
+            }
+        }
+        return res;
+    }
+
+    public int bitwiseComplement(int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            int ans = 0;
+            List<Integer> binary = toBinary(n);
+            List<Integer> com = complement(binary);
+            for (int i = 0; i < com.size(); i++) {
+                ans += Math.pow(2, i) * com.get(i);
+            }
+            return ans;
+        }
+    }
+}
