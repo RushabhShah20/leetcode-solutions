7d0f3c8697063cf6d6ed50edbd4d3ae40d4dd680
Rushabh <20rush09abh2001@gmail.com>
2025-06-10 10:04:57 +0530

    Added Java solution to 'Reverse Integer'

---
+++ medium/Reverse Integer/Java/main.java
@@ -0,0 +1,15 @@
+// Problem: Reverse Integer
+// Link to the problem: https://leetcode.com/problems/reverse-integer/
+class Solution {
+    public int reverse(int x) {
+        int ans = 0;
+        while (x != 0) {
+            if ((ans > Integer.MAX_VALUE / 10) || (ans < Integer.MIN_VALUE / 10)) {
+                return 0;
+            }
+            ans = ans * 10 + x % 10;
+            x /= 10;
+        }
+        return ans;
+    }
+}
