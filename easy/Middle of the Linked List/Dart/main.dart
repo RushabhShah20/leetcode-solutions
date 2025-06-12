ca9d36d75634a3e53a68c8e128328f6382346c70
Rushabh <20rush09abh2001@gmail.com>
2025-06-12 11:18:16 +0530

    Added Dart solution to 'Middle of the Linked List'

---
+++ easy/Middle of the Linked List/Dart/main.dart
@@ -0,0 +1,24 @@
+// Problem: Middle of the Linked List
+// Link to the problem: https://leetcode.com/problems/middle-of-the-linked-list/
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *   int val;
+ *   ListNode? next;
+ *   ListNode([this.val = 0, this.next]);
+ * }
+ */
+class Solution {
+  ListNode? middleNode(ListNode? head) {
+    if (head == null || head.next == null) {
+      return head;
+    }
+    ListNode? slow = head;
+    ListNode? fast = head;
+    while (fast != null && fast.next != null) {
+      slow = slow!.next;
+      fast = fast.next!.next;
+    }
+    return slow;
+  }
+}
