9d243a3d72b4c91c07440dbff03211edf1da9e2d
Rushabh <20rush09abh2001@gmail.com>
2025-06-12 11:18:59 +0530

    Added Rust solution to 'Middle of the Linked List'

---
+++ easy/Middle of the Linked List/Rust/main.rs
@@ -0,0 +1,29 @@
+// Problem: Middle of the Linked List
+// Link to the problem: https://leetcode.com/problems/middle-of-the-linked-list/
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+//
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+impl Solution {
+    pub fn middle_node(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut slow: &Option<Box<ListNode>> = &head;
+        let mut fast: &Option<Box<ListNode>> = &head;
+        while fast.is_some() && fast.as_ref().unwrap().next.is_some() {
+            slow = &slow.as_ref().unwrap().next;
+            fast = &fast.as_ref().unwrap().next.as_ref().unwrap().next;
+        }
+        return slow.clone();
+    }
+}
