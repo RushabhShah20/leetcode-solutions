13207163d74eb1661714e81b08e8fb4cd43e1b0a
Rushabh <20rush09abh2001@gmail.com>
2025-06-13 11:05:38 +0530

    Added Dart solution to 'Maximum Containers on a Ship'

---
+++ easy/Maximum Containers on a Ship/Dart/main.dart
@@ -0,0 +1,9 @@
+// Problem: Maximum Containers on a Ship
+// Link to the problem: https://leetcode.com/problems/maximum-containers-on-a-ship/
+class Solution {
+  int maxContainers(int n, int w, int maxWeight) {
+    return ((pow(n, 2).toInt() * w) <= maxWeight
+        ? (pow(n, 2).toInt())
+        : (maxWeight ~/ w));
+  }
+}
