16956ba6170aae24ba70f60e0c66a0c1a72e0da4
Rushabh <20rush09abh2001@gmail.com>
2025-06-13 11:04:23 +0530

    Added C solution to 'Maximum Containers on a Ship'

---
+++ easy/Maximum Containers on a Ship/C/main.c
@@ -0,0 +1,6 @@
+// Problem: Maximum Containers on a Ship
+// Link to the problem: https://leetcode.com/problems/maximum-containers-on-a-ship/
+int maxContainers(int n, int w, int maxWeight)
+{
+    return ((pow(n, 2) * w) <= maxWeight ? (pow(n, 2)) : (maxWeight / w));
+}
