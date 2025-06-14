96fdbb5ab81aba00ada6cd4be4a88f511128d080
Rushabh <20rush09abh2001@gmail.com>
2025-06-14 15:08:08 +0530

    Added C++ solution to 'Check if The Number is Fascinating'

---
+++ easy/Check if The Number is Fascinating/C++/main.cpp
@@ -0,0 +1,10 @@
+// Problem: Check if The Number is Fascinating
+// Link to the problem: https://leetcode.com/problems/check-if-the-number-is-fascinating/
+class Solution
+{
+public:
+    bool isFascinating(int n)
+    {
+        return n == 192 || n == 219 || n == 273 || n == 327; 
+    }
+};
