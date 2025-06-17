f9300d2bdd409a2fb37d2e12751fc8c00082ba6b
Rushabh <20rush09abh2001@gmail.com>
2025-06-17 14:10:55 +0530

    Added C++ solution to 'Find the Peaks'

---
+++ easy/Find the Peaks/C++/main.cpp
@@ -0,0 +1,18 @@
+// Problem: Find the Peaks
+// Link to the problem: https://leetcode.com/problems/find-the-peaks/
+class Solution
+{
+public:
+    vector<int> findPeaks(vector<int> &mountain)
+    {
+        vector<int> ans;
+        for (int i = 1; i < mountain.size() - 1; i++)
+        {
+            if (mountain[i] > mountain[i + 1] && mountain[i] > mountain[i - 1])
+            {
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
