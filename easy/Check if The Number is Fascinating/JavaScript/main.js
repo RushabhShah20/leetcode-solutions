0d36c922ddde97e163d8a640ee1fc4cca6c97f95
Rushabh <20rush09abh2001@gmail.com>
2025-06-18 14:02:14 +0530

    Added JavaScript solution to 'Check if The Number is Fascinating'

---
+++ easy/Check if The Number is Fascinating/JavaScript/main.js
@@ -0,0 +1,9 @@
+// Problem: Check if The Number is Fascinating
+// Link to the problem: https://leetcode.com/problems/check-if-the-number-is-fascinating/
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isFascinating = function (n) {
+    return n == 192 || n == 219 || n == 273 || n == 327;
+};
