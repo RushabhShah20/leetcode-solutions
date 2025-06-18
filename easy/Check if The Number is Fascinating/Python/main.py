301754f9b2b5368524882fbad5ecc0d6ecf8f1d3
Rushabh <20rush09abh2001@gmail.com>
2025-06-18 14:01:57 +0530

    Added Python solution to 'Check if The Number is Fascinating'

---
+++ easy/Check if The Number is Fascinating/Python/main.py
@@ -0,0 +1,5 @@
+# Problem: Check if The Number is Fascinating
+# Link to the problem: https://leetcode.com/problems/check-if-the-number-is-fascinating/
+class Solution:
+    def isFascinating(self, n: int) -> bool:
+        return n == 192 or n == 219 or n == 273 or n == 327
