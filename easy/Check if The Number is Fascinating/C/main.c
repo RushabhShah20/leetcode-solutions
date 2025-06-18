0d104f3aee458d227331cdb1c2abb6c4eac73126
Rushabh <20rush09abh2001@gmail.com>
2025-06-18 14:01:28 +0530

    Added C solution to 'Check if The Number is Fascinating'

---
+++ easy/Check if The Number is Fascinating/C/main.c
@@ -0,0 +1,6 @@
+// Problem: Check if The Number is Fascinating
+// Link to the problem: https://leetcode.com/problems/check-if-the-number-is-fascinating/
+bool isFascinating(int n)
+{
+    return n == 192 || n == 219 || n == 273 || n == 327;
+}
