f6c6fa222ad16ec335116901bfb6bfb851f493db
Rushabh <20rush09abh2001@gmail.com>
2025-06-18 14:02:53 +0530

    Added Typescript solution to 'Check if The Number is Fascinating'

---
+++ easy/Check if The Number is Fascinating/Typescript/main.ts
@@ -0,0 +1,5 @@
+// Problem: Check if The Number is Fascinating
+// Link to the problem: https://leetcode.com/problems/check-if-the-number-is-fascinating/
+function isFascinating(n: number): boolean {
+    return n == 192 || n == 219 || n == 273 || n == 327;
+};
