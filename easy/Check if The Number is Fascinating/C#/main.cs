2ae6fe1e054a296aa156b02dbe85ce07686c31c4
Rushabh <20rush09abh2001@gmail.com>
2025-06-18 14:01:40 +0530

    Added C# solution to 'Check if The Number is Fascinating'

---
+++ easy/Check if The Number is Fascinating/C#/main.cs
@@ -0,0 +1,9 @@
+// Problem: Check if The Number is Fascinating
+// Link to the problem: https://leetcode.com/problems/check-if-the-number-is-fascinating/
+public class Solution
+{
+    public bool IsFascinating(int n)
+    {
+        return n == 192 || n == 219 || n == 273 || n == 327;
+    }
+}
