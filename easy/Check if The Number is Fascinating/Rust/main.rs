4401b0e5fa56c9b0b8a7efc38ec99a589b75210e
Rushabh <20rush09abh2001@gmail.com>
2025-06-18 14:03:41 +0530

    Added Rust solution to 'Check if The Number is Fascinating'

---
+++ easy/Check if The Number is Fascinating/Rust/main.rs
@@ -0,0 +1,7 @@
+// Problem: Check if The Number is Fascinating
+// Link to the problem: https://leetcode.com/problems/check-if-the-number-is-fascinating/
+impl Solution {
+    pub fn is_fascinating(n: i32) -> bool {
+        return n == 192 || n == 219 || n == 273 || n == 327;
+    }
+}
