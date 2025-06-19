d98aa88e828c947a3ee1288dc117735bd98c011d
Rushabh <20rush09abh2001@gmail.com>
2025-06-19 14:09:45 +0530

    Added C++ solution to 'Circular Sentence'

---
+++ easy/Circular Sentence/C++/main.cpp
@@ -0,0 +1,21 @@
+// Problem: Circular Sentence
+// Link to the problem: https://leetcode.com/problems/circular-sentence/
+class Solution
+{
+public:
+    bool isCircularSentence(string sentence)
+    {
+        if (sentence[0] != sentence[sentence.size() - 1])
+        {
+            return false;
+        }
+        for (int i = 0; i < sentence.size(); i++)
+        {
+            if (sentence[i] == ' ' && sentence[i - 1] != sentence[i + 1])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
