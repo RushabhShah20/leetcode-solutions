62a23e49e4d075303d2dcd88b418d2c3cbe969d5
Rushabh <20rush09abh2001@gmail.com>
2025-06-21 12:49:16 +0530

    Added C++ solution to 'Bulb Switcher'

---
+++ medium/Bulb Switcher/C++/main.cpp
@@ -0,0 +1,10 @@
+// Problem: Bulb Switcher
+// Link to the problem: https://leetcode.com/problems/bulb-switcher/
+class Solution
+{
+public:
+    int bulbSwitch(int n)
+    {
+        return sqrt(n);
+    }
+};
