9ba1a4b29dde365bab8f5cb95603bdbf7b39bffe
Rushabh <20rush09abh2001@gmail.com>
2025-06-24 14:51:55 +0530

    Added C++ solution to 'Calculate Money in Leetcode Bank'

---
+++ easy/Calculate Money in Leetcode Bank/C++/main.cpp
@@ -0,0 +1,15 @@
+// Problem: Calculate Money in Leetcode Bank
+// Link to the problem: https://leetcode.com/problems/calculate-money-in-leetcode-bank/
+class Solution
+{
+public:
+    int totalMoney(int n)
+    {
+        int q = n / 7;
+        int r = n % 7;
+        int ans = ((q * (q - 1)) / 2) * 7;
+        ans += q * 28;
+        ans += ((r * (r + 1)) / 2) + (q * r);
+        return ans;
+    }
+};
