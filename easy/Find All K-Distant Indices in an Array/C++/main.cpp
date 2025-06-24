2335cf059724d39f5507a18de8c45bf67136132e
Rushabh <20rush09abh2001@gmail.com>
2025-06-24 10:22:00 +0530

    Added C++ solution to 'Find All K-Distant Indices in an Array'

---
+++ easy/Find All K-Distant Indices in an Array/C++/main.cpp
@@ -0,0 +1,25 @@
+// Problem: Find All K-Distant Indices in an Array
+// Link to the problem: https://leetcode.com/problems/find-all-k-distant-indices-in-an-array/
+class Solution
+{
+public:
+    vector<int> findKDistantIndices(vector<int> &nums, int key, int k)
+    {
+        vector<int> ans;
+        int r = 0;
+        int n = nums.size();
+        for (int j = 0; j < n; j++)
+        {
+            if (nums[j] == key)
+            {
+                int l = max(r, j - k);
+                r = min(n - 1, j + k) + 1;
+                for (int i = l; i < r; ++i)
+                {
+                    ans.push_back(i);
+                }
+            }
+        }
+        return ans;
+    }
+};
