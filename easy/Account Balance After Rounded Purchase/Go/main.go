e395febc3329b66feab3acbbf1035203840813aa
Rushabh <20rush09abh2001@gmail.com>
2025-06-25 11:50:45 +0530

    Added Go solution to 'Account Balance After Rounded Purchase'

---
+++ easy/Account Balance After Rounded Purchase/Go/main.go
@@ -0,0 +1,11 @@
+// Problem: Account Balance After Rounded Purchase
+// Link to the problem: https://leetcode.com/problems/account-balance-after-rounded-purchase/
+func accountBalanceAfterPurchase(purchaseAmount int) int {
+	if purchaseAmount%10 == 0 {
+		return 100 - purchaseAmount
+	} else if purchaseAmount%10 < 5 {
+		return 100 - ((purchaseAmount) - (purchaseAmount % 10))
+	} else {
+		return (100 - (purchaseAmount + (10 - (purchaseAmount % 10))))
+	}
+}
