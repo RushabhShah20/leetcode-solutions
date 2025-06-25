9bc9fad0fd65d1b25ec94f9f89970cbb604e1917
Rushabh <20rush09abh2001@gmail.com>
2025-06-25 11:49:25 +0530

    Added C# solution to 'Account Balance After Rounded Purchase'

---
+++ easy/Account Balance After Rounded Purchase/C#/main.cs
@@ -0,0 +1,20 @@
+// Problem: Account Balance After Rounded Purchase
+// Link to the problem: https://leetcode.com/problems/account-balance-after-rounded-purchase/
+public class Solution
+{
+    public int AccountBalanceAfterPurchase(int purchaseAmount)
+    {
+        if (purchaseAmount % 10 == 0)
+        {
+            return 100 - purchaseAmount;
+        }
+        else if (purchaseAmount % 10 < 5)
+        {
+            return 100 - ((purchaseAmount) - (purchaseAmount % 10));
+        }
+        else
+        {
+            return (100 - (purchaseAmount + (10 - (purchaseAmount % 10))));
+        }
+    }
+}
