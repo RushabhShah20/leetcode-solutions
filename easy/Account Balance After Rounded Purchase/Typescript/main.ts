3e2a629ef1fc308a433faab406553a0e5d8300fa
Rushabh <20rush09abh2001@gmail.com>
2025-06-25 11:50:22 +0530

    Added Typescript solution to 'Account Balance After Rounded Purchase'

---
+++ easy/Account Balance After Rounded Purchase/Typescript/main.ts
@@ -0,0 +1,13 @@
+// Problem: Account Balance After Rounded Purchase
+// Link to the problem: https://leetcode.com/problems/account-balance-after-rounded-purchase/
+function accountBalanceAfterPurchase(purchaseAmount: number): number {
+    if (purchaseAmount % 10 == 0) {
+        return 100 - purchaseAmount;
+    }
+    else if (purchaseAmount % 10 < 5) {
+        return 100 - ((purchaseAmount) - (purchaseAmount % 10));
+    }
+    else {
+        return (100 - (purchaseAmount + (10 - (purchaseAmount % 10))));
+    }
+};
