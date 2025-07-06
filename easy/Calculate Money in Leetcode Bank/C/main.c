48e7e54863b51d16b4f092cca5745e53ab884946
Rushabh <20rush09abh2001@gmail.com>
2025-07-06 20:30:18 +0530

    Added C solution to 'Calculate Money in Leetcode Bank'

---
+++ easy/Calculate Money in Leetcode Bank/C/main.c
@@ -0,0 +1,11 @@
+// Problem: Calculate Money in Leetcode Bank
+// Link to the problem: https://leetcode.com/problems/calculate-money-in-leetcode-bank/
+int totalMoney(int n)
+{
+    int q = n / 7;
+    int r = n % 7;
+    int ans = ((q * (q - 1)) / 2) * 7;
+    ans += q * 28;
+    ans += ((r * (r + 1)) / 2) + (q * r);
+    return ans;
+}
