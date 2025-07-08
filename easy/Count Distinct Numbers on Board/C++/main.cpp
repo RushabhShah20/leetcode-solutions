c33444736f7e44d0b23f64cb9d86aced99b70edd
Rushabh <20rush09abh2001@gmail.com>
2025-07-08 15:58:03 +0530

    Added C++ solution to 'Count Distinct Numbers on Board'

---
+++ easy/Count Distinct Numbers on Board/C++/main.cpp
@@ -0,0 +1,10 @@
+// Problem: Count Distinct Numbers on Board
+// Link to the problem: https://leetcode.com/problems/count-distinct-numbers-on-board/
+class Solution
+{
+public:
+    int distinctIntegers(int n)
+    {
+        return ((n == 1) ? (1) : (n - 1));
+    }
+};
