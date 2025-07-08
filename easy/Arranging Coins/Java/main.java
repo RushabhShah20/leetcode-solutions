9b41fa4a0770ee6a231675f5746c2112c9479e63
Rushabh <20rush09abh2001@gmail.com>
2025-07-08 12:51:05 +0530

    Added Java solution to 'Arranging Coins'

---
+++ easy/Arranging Coins/Java/main.java
@@ -0,0 +1,21 @@
+// Problem: Arranging Coins
+// Link to the problem: https://leetcode.com/problems/arranging-coins/
+class Solution {
+    public int arrangeCoins(int n) {
+        int low = 0, high = n;
+        int ans = 0;
+        while (low <= high) {
+            long mid = low + (high - low) / 2;
+            long sum = mid * (mid + 1) / 2;
+            if (sum == n) {
+                return (int) mid;
+            } else if (sum < n) {
+                ans = (int) mid;
+                low = (int) (mid + 1);
+            } else {
+                high = (int) (mid - 1);
+            }
+        }
+        return ans;
+    }
+}
