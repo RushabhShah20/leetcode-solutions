ded25a80db8074f186278c40a2ce0011268ebee8
Rushabh <20rush09abh2001@gmail.com>
2025-07-30 12:14:24 +0530

    Added C++ solution to 'Find Pivot Index'

---
+++ easy/Find Pivot Index/C++/main.cpp
@@ -0,0 +1,29 @@
+// Problem: Find Pivot Index
+// Link to the problem: https://leetcode.com/problems/find-pivot-index/
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int pivotIndex(vector<int> &nums)
+    {
+        for (int i = 0; i < nums.size(); i++)
+        {
+            vector<int> prefix(nums.begin(), nums.begin() + i), suffix(nums.begin() + i + 1, nums.end());
+            int prefixSum = 0, suffixSum = 0;
+            for (int j = 0; j < prefix.size(); j++)
+            {
+                prefixSum += prefix[j];
+            }
+            for (int j = 0; j < suffix.size(); j++)
+            {
+                suffixSum += suffix[j];
+            }
+            if (prefixSum == suffixSum)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
