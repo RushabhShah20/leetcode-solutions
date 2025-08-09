074c410c9c957a26ea7e2e01357591c6b1663b27
Rushabh <20rush09abh2001@gmail.com>
2025-08-09 11:15:03 +0530

    Added Java solution to 'Power of Three'

---
+++ easy/Power of Three/Java/main.java
@@ -0,0 +1,16 @@
+// Problem: Power of Three
+// Link to the problem: https://leetcode.com/problems/power-of-three/
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        while (n > 1) {
+            if (n % 3 != 0) {
+                return false;
+            }
+            n /= 3;
+        }
+        return true;
+    }
+}
