ea0ee1045ee60d2f90c887f769454a91bfba13fe
Rushabh <20rush09abh2001@gmail.com>
2025-08-09 08:30:29 +0530

    Added Java solution to 'Power of Two'

---
+++ easy/Power of Two/Java/main.java
@@ -0,0 +1,16 @@
+// Problem: Power of Two
+// Link to the problem: https://leetcode.com/problems/power-of-two/
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        while (n > 1) {
+            if (n % 2 != 0) {
+                return false;
+            }
+            n /= 2;
+        }
+        return true;
+    }
+}
