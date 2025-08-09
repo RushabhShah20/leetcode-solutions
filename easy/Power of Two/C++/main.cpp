8b2c1efb7e390dfd08565e7ba84e5e622ddaddd8
Rushabh <20rush09abh2001@gmail.com>
2025-08-09 08:30:11 +0530

    Added C++ solution to 'Power of Two'

---
+++ easy/Power of Two/C++/main.cpp
@@ -0,0 +1,22 @@
+// Problem: Power of Two
+// Link to the problem: https://leetcode.com/problems/power-of-two/
+class Solution
+{
+public:
+    bool isPowerOfTwo(int n)
+    {
+        if (n <= 0)
+        {
+            return false;
+        }
+        while (n > 1)
+        {
+            if (n % 2 != 0)
+            {
+                return false;
+            }
+            n /= 2;
+        }
+        return true;
+    }
+};
