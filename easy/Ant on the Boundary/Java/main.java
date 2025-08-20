d2bd762e8cd2c2b44c93fe8af61fcdce3c862cfd
Rushabh <20rush09abh2001@gmail.com>
2025-08-20 14:36:25 +0530

    Added Java solution to 'Ant on the Boundary'

---
+++ easy/Ant on the Boundary/Java/main.java
@@ -0,0 +1,14 @@
+// Problem: Ant on the Boundary
+// Link to the problem: https://leetcode.com/problems/ant-on-the-boundary/
+class Solution {
+    public int returnToBoundaryCount(int[] nums) {
+        int ans = 0, count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            count += nums[i];
+            if (count == 0) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
