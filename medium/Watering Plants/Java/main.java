0ff6591a32d48f5a817e77ce838e986771c513fd
Rushabh <20rush09abh2001@gmail.com>
2025-08-21 12:58:10 +0530

    Added Java solution to 'Watering Plants'

---
+++ medium/Watering Plants/Java/main.java
@@ -0,0 +1,18 @@
+// Problem: Watering Plants
+// Link to the problem: https://leetcode.com/problems/watering-plants/
+class Solution {
+    public int wateringPlants(int[] plants, int capacity) {
+        int ans = 0, x = capacity;
+        for (int i = 0; i < plants.length; i++) {
+            if (plants[i] <= capacity) {
+                ans++;
+                capacity -= plants[i];
+            } else {
+                ans += (2 * (i)) + 1;
+                capacity = x;
+                capacity -= plants[i];
+            }
+        }
+        return ans;
+    }
+}
