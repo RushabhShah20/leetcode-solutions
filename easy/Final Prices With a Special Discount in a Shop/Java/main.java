f57e1c473339004f8e310a59723d71c62bbb78f3
Rushabh <20rush09abh2001@gmail.com>
2025-08-21 17:06:54 +0530

    Added Java solution to 'Final Prices With a Special Discount in a Shop'

---
+++ easy/Final Prices With a Special Discount in a Shop/Java/main.java
@@ -0,0 +1,17 @@
+// Problem: Final Prices With a Special Discount in a Shop
+// Link to the problem: https://leetcode.com/problems/final-prices-with-a-special-discount-in-a-shop/
+class Solution {
+    public int[] finalPrices(int[] prices) {
+        int[] ans = new int[prices.length];
+        for (int i = 0; i < prices.length; i++) {
+            ans[i] = prices[i];
+            for (int j = i + 1; j < prices.length; j++) {
+                if (prices[j] <= prices[i]) {
+                    ans[i] = prices[i] - prices[j];
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+}
