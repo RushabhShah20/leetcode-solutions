107ebb76b6556d24d02351efcb35de976a201f06
Rushabh <20rush09abh2001@gmail.com>
2025-08-23 15:01:04 +0530

    Added Dart solution to 'Number of Days Between Two Dates'

---
+++ easy/Number of Days Between Two Dates/Dart/main.dart
@@ -0,0 +1,35 @@
+// Problem: Number of Days Between Two Dates
+// Link to the problem: https://leetcode.com/problems/number-of-days-between-two-dates/
+class Solution {
+  int dayOfYear(String date) {
+    int day = int.parse(date.substring(8, 10)),
+        month = int.parse(date.substring(5, 7)),
+        year = int.parse(date.substring(0, 4));
+    int ans = 0;
+    for (int i = 1971; i < year; i++) {
+      if (i % 4 == 0 && (i % 100 != 0 || i % 400 == 0)) {
+        ans += 366;
+      } else {
+        ans += 365;
+      }
+    }
+    if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+      List<int> months = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+      for (int i = 0; i < month - 1; i++) {
+        ans += months[i];
+      }
+    } else {
+      List<int> months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+      for (int i = 0; i < month - 1; i++) {
+        ans += months[i];
+      }
+    }
+    ans += day;
+    return ans;
+  }
+
+  int daysBetweenDates(String date1, String date2) {
+    int x = dayOfYear(date1), y = dayOfYear(date2);
+    return (x - y).abs();
+  }
+}
