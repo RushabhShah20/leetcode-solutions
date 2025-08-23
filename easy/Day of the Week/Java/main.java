e8a722919e59b57fb2604e1f5f72415645adffa2
Rushabh <20rush09abh2001@gmail.com>
2025-08-23 14:15:33 +0530

    Added Java solution to 'Day of the Week'

---
+++ easy/Day of the Week/Java/main.java
@@ -0,0 +1,42 @@
+// Problem: Day of the Week
+// Link to the problem: https://leetcode.com/problems/day-of-the-week/
+class Solution {
+    public String dayOfTheWeek(int day, int month, int year) {
+        int ans = 0;
+        for (int i = 1971; i < year; i++) {
+            if (i % 4 == 0 && (i % 100 != 0 || i % 400 == 0)) {
+                ans += 366;
+            } else {
+                ans += 365;
+            }
+        }
+        if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+            int[] months = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            for (int i = 0; i < month - 1; i++) {
+                ans += months[i];
+            }
+        } else {
+            int[] months = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            for (int i = 0; i < month - 1; i++) {
+                ans += months[i];
+            }
+        }
+        ans += day;
+        ans %= 7;
+        if (ans == 1) {
+            return "Friday";
+        } else if (ans == 2) {
+            return "Saturday";
+        } else if (ans == 3) {
+            return "Sunday";
+        } else if (ans == 4) {
+            return "Monday";
+        } else if (ans == 5) {
+            return "Tuesday";
+        } else if (ans == 6) {
+            return "Wednesday";
+        } else {
+            return "Thursday";
+        }
+    }
+}
