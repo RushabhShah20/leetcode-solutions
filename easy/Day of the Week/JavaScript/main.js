718f4e2d900869ef13ae8b865ae9f944bc18c2ed
Rushabh <20rush09abh2001@gmail.com>
2025-08-23 14:16:14 +0530

    Added JavaScript solution to 'Day of the Week'

---
+++ easy/Day of the Week/JavaScript/main.js
@@ -0,0 +1,54 @@
+// Problem: Day of the Week
+// Link to the problem: https://leetcode.com/problems/day-of-the-week/
+/**
+ * @param {number} day
+ * @param {number} month
+ * @param {number} year
+ * @return {string}
+ */
+var dayOfTheWeek = function (day, month, year) {
+    let ans = 0;
+    for (let i = 1971; i < year; i++) {
+        if (i % 4 === 0 && (i % 100 != 0 || i % 400 === 0)) {
+            ans += 366;
+        }
+        else {
+            ans += 365;
+        }
+    }
+    if (year % 4 === 0 && (year % 100 != 0 || year % 400 === 0)) {
+        let months = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+        for (let i = 0; i < month - 1; i++) {
+            ans += months[i];
+        }
+    }
+    else {
+        let months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+        for (let i = 0; i < month - 1; i++) {
+            ans += months[i];
+        }
+    }
+    ans += day;
+    ans %= 7;
+    if (ans === 1) {
+        return "Friday";
+    }
+    else if (ans === 2) {
+        return "Saturday";
+    }
+    else if (ans === 3) {
+        return "Sunday";
+    }
+    else if (ans === 4) {
+        return "Monday";
+    }
+    else if (ans === 5) {
+        return "Tuesday";
+    }
+    else if (ans === 6) {
+        return "Wednesday";
+    }
+    else {
+        return "Thursday";
+    }
+};
