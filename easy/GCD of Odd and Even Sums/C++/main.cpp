ca8b22e9161be0713495c1f188ff5932c1fcad30
Rushabh <20rush09abh2001@gmail.com>
2025-08-24 11:30:42 +0530

    Added C++ solution to 'GCD of Odd and Even Sums'

---
+++ easy/GCD of Odd and Even Sums/C++/main.cpp
@@ -0,0 +1,14 @@
+// Problem: GCD of Odd and Even Sums
+// Link to the problem: https://leetcode.com/problems/gcd-of-odd-and-even-sums/
+class Solution
+{
+public:
+    int gcd(int a, int b)
+    {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+    int gcdOfOddEvenSums(int n)
+    {
+        return gcd((n) * (n + 1), pow(n, 2));
+    }
+};
