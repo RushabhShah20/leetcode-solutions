17c309e5b5e470acef6ab55f11511cdcabd63a0a
Rushabh <20rush09abh2001@gmail.com>
2025-08-25 14:36:29 +0530

    Added Dart solution to 'Sum of Even Numbers After Queries'

---
+++ medium/Sum of Even Numbers After Queries/Dart/main.dart
@@ -0,0 +1,33 @@
+// Problem: Sum of Even Numbers After Queries
+// Link to the problem: https://leetcode.com/problems/sum-of-even-numbers-after-queries/
+class Solution {
+  List<int> sumEvenAfterQueries(List<int> nums, List<List<int>> queries) {
+    int sum = 0;
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] % 2 == 0) {
+        sum += nums[i];
+      }
+    }
+    List<int> ans = new List.filled(queries.length, 0);
+    for (int i = 0; i < queries.length; i++) {
+      if (nums[queries[i][1]] % 2 == 0) {
+        if ((nums[queries[i][1]] + queries[i][0]) % 2 == 0) {
+          sum += queries[i][0];
+          nums[queries[i][1]] += queries[i][0];
+        } else {
+          sum -= nums[queries[i][1]];
+          nums[queries[i][1]] += queries[i][0];
+        }
+      } else {
+        if ((nums[queries[i][1]] + queries[i][0]) % 2 == 0) {
+          sum += (nums[queries[i][1]] + queries[i][0]);
+          nums[queries[i][1]] += queries[i][0];
+        } else {
+          nums[queries[i][1]] += queries[i][0];
+        }
+      }
+      ans[i] = sum;
+    }
+    return ans;
+  }
+}
