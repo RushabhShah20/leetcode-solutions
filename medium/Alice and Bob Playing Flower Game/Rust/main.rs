8ce5c4f6321d3d3890a622c7967b06494090bf72
Rushabh <20rush09abh2001@gmail.com>
2025-08-29 11:40:13 +0530

    Added Rust solution to 'Alice and Bob Playing Flower Game'

---
+++ medium/Alice and Bob Playing Flower Game/Rust/main.rs
@@ -0,0 +1,7 @@
+// Problem: Alice and Bob Playing Flower Game
+// Link to the problem: https://leetcode.com/problems/alice-and-bob-playing-flower-game/
+impl Solution {
+    pub fn flower_game(n: i32, m: i32) -> i64 {
+        return ((n as i64) * (m as i64)) / 2;
+    }
+}
