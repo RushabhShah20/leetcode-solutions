020909f21fa26275ee8b9165a03371eb460f1691
Rushabh <20rush09abh2001@gmail.com>
2025-08-29 14:17:22 +0530

    Added Dart solution to 'Alternating Groups I'

---
+++ easy/Alternating Groups I/Dart/main.dart
@@ -0,0 +1,20 @@
+// Problem: Alternating Groups I
+// Link to the problem: https://leetcode.com/problems/alternating-groups-i/
+class Solution {
+  int numberOfAlternatingGroups(List<int> colors) {
+    int ans = 0;
+    for (int i = 1; i < colors.length - 1; i++) {
+      if (colors[i] != colors[i - 1] && colors[i] != colors[i + 1]) {
+        ans++;
+      }
+    }
+    if (colors[0] != colors[1] && colors[0] != colors[colors.length - 1]) {
+      ans++;
+    }
+    if (colors[colors.length - 1] != colors[colors.length - 2] &&
+        colors[colors.length - 1] != colors[0]) {
+      ans++;
+    }
+    return ans;
+  }
+}
