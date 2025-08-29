c5ef7ca8b71f1bbd5abd17ef10c575fab796df5e
Rushabh <20rush09abh2001@gmail.com>
2025-08-29 11:38:58 +0530

    Added Python solution to 'Alice and Bob Playing Flower Game'

---
+++ medium/Alice and Bob Playing Flower Game/Python/main.py
@@ -0,0 +1,5 @@
+# Problem: Alice and Bob Playing Flower Game
+# Link to the problem: https://leetcode.com/problems/alice-and-bob-playing-flower-game/
+class Solution:
+    def flowerGame(self, n: int, m: int) -> int:
+        return ((n) * (m)) // 2
