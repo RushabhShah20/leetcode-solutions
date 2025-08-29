bcb3ed1a1a8acfaa8e8ed95fdd7f45a2e70fe8b2
Rushabh <20rush09abh2001@gmail.com>
2025-08-29 14:02:42 +0530

    Added Dart solution to 'Airplane Seat Assignment Probability'

---
+++ medium/Airplane Seat Assignment Probability/Dart/main.dart
@@ -0,0 +1,11 @@
+// Problem: Airplane Seat Assignment Probability
+// Link to the problem: https://leetcode.com/problems/airplane-seat-assignment-probability/
+class Solution {
+  double nthPersonGetsNthSeat(int n) {
+    if (n == 1) {
+      return 1;
+    } else {
+      return 0.5;
+    }
+  }
+}
