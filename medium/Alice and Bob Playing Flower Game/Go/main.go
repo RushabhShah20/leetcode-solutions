d42dd96b1541874f8d269eccb7567a4ef063dc3e
Rushabh <20rush09abh2001@gmail.com>
2025-08-29 11:39:59 +0530

    Added Go solution to 'Alice and Bob Playing Flower Game'

---
+++ medium/Alice and Bob Playing Flower Game/Go/main.go
@@ -0,0 +1,5 @@
+// Problem: Alice and Bob Playing Flower Game
+// Link to the problem: https://leetcode.com/problems/alice-and-bob-playing-flower-game/
+func flowerGame(n int, m int) int64 {
+	return ((int64)(n) * (int64)(m)) / 2
+}
