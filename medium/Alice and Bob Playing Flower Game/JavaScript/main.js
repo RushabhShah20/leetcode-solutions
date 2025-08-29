39a2b91b36b09956d6e41dfdcc98e542209a54d4
Rushabh <20rush09abh2001@gmail.com>
2025-08-29 11:39:12 +0530

    Added JavaScript solution to 'Alice and Bob Playing Flower Game'

---
+++ medium/Alice and Bob Playing Flower Game/JavaScript/main.js
@@ -0,0 +1,10 @@
+// Problem: Alice and Bob Playing Flower Game
+// Link to the problem: https://leetcode.com/problems/alice-and-bob-playing-flower-game/
+/**
+ * @param {number} n
+ * @param {number} m
+ * @return {number}
+ */
+var flowerGame = function (n, m) {
+    return Math.floor(((n) * (m)) / 2);
+};
