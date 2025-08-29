066fe1f83e939251485ae5a2bde68e8e9fbfb43c
Rushabh <20rush09abh2001@gmail.com>
2025-08-29 11:38:22 +0530

    Added Java solution to 'Alice and Bob Playing Flower Game'

---
+++ medium/Alice and Bob Playing Flower Game/Java/main.java
@@ -0,0 +1,7 @@
+// Problem: Alice and Bob Playing Flower Game
+// Link to the problem: https://leetcode.com/problems/alice-and-bob-playing-flower-game/
+class Solution {
+    public long flowerGame(int n, int m) {
+        return ((long) (n) * (long) (m)) / 2;
+    }
+}
