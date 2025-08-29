f5d3a10b6ed1f8bbe4df48944e541ed770e7c0b5
Rushabh <20rush09abh2001@gmail.com>
2025-08-29 11:39:40 +0530

    Added Dart solution to 'Alice and Bob Playing Flower Game'

---
+++ medium/Alice and Bob Playing Flower Game/Dart/main.dart
@@ -0,0 +1,7 @@
+// Problem: Alice and Bob Playing Flower Game
+// Link to the problem: https://leetcode.com/problems/alice-and-bob-playing-flower-game/
+class Solution {
+  int flowerGame(int n, int m) {
+    return ((n) * (m)) ~/ 2;
+  }
+}
