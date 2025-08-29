a6ce611846cb3132fd32f98bef671bcc5de89046
Rushabh <20rush09abh2001@gmail.com>
2025-08-29 14:02:00 +0530

    Added Python solution to 'Airplane Seat Assignment Probability'

---
+++ medium/Airplane Seat Assignment Probability/Python/main.py
@@ -0,0 +1,8 @@
+# Problem: Airplane Seat Assignment Probability
+# Link to the problem: https://leetcode.com/problems/airplane-seat-assignment-probability/
+class Solution:
+    def nthPersonGetsNthSeat(self, n: int) -> float:
+        if n == 1:
+            return 1
+        else:
+            return 0.5
