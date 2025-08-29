9f6c70d9b510c69547254473f12d8580c07b8748
Rushabh <20rush09abh2001@gmail.com>
2025-08-29 11:39:27 +0530

    Added Typescript solution to 'Alice and Bob Playing Flower Game'

---
+++ medium/Alice and Bob Playing Flower Game/Typescript/main.ts
@@ -0,0 +1,5 @@
+// Problem: Alice and Bob Playing Flower Game
+// Link to the problem: https://leetcode.com/problems/alice-and-bob-playing-flower-game/
+function flowerGame(n: number, m: number): number {
+    return Math.floor(((n) * (m)) / 2);
+};
