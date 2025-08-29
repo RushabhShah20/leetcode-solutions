157ae3e6fa23951360dd888ac5f2090c827a1d08
Rushabh <20rush09abh2001@gmail.com>
2025-08-29 11:38:33 +0530

    Added C solution to 'Alice and Bob Playing Flower Game'

---
+++ medium/Alice and Bob Playing Flower Game/C/main.c
@@ -0,0 +1,6 @@
+// Problem: Alice and Bob Playing Flower Game
+// Link to the problem: https://leetcode.com/problems/alice-and-bob-playing-flower-game/
+long long flowerGame(int n, int m)
+{
+    return ((long long)(n) * (long long)(m)) / 2;
+}
