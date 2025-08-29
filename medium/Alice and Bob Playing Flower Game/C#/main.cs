018794b66f8cd1479b3a299c1d210fcc57a0b195
Rushabh <20rush09abh2001@gmail.com>
2025-08-29 11:38:45 +0530

    Added C# solution to 'Alice and Bob Playing Flower Game'

---
+++ medium/Alice and Bob Playing Flower Game/C#/main.cs
@@ -0,0 +1,9 @@
+// Problem: Alice and Bob Playing Flower Game
+// Link to the problem: https://leetcode.com/problems/alice-and-bob-playing-flower-game/
+public class Solution
+{
+    public long FlowerGame(int n, int m)
+    {
+        return ((long)(n) * (long)(m)) / 2;
+    }
+}
