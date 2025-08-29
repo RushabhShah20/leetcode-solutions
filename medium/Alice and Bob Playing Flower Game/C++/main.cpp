2baf12db93b9c08994a5069d1a4644bf5660e4cb
Rushabh <20rush09abh2001@gmail.com>
2025-08-29 11:28:53 +0530

    Added C++ solution to 'Alice and Bob Playing Flower Game'

---
+++ medium/Alice and Bob Playing Flower Game/C++/main.cpp
@@ -0,0 +1,10 @@
+// Problem: Alice and Bob Playing Flower Game
+// Link to the problem: https://leetcode.com/problems/alice-and-bob-playing-flower-game/
+class Solution
+{
+public:
+    long long flowerGame(int n, int m)
+    {
+        return ((long long)(n) * (long long)(m)) / 2;
+    }
+};
