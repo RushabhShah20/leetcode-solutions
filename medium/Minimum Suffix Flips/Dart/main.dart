9f4adfdb1a9d6ea5f265b939bcbde9d6d16e8f54
Rushabh <20rush09abh2001@gmail.com>
2025-09-02 15:35:47 +0530

    Added Dart solution to 'Minimum Suffix Flips'

---
+++ medium/Minimum Suffix Flips/Dart/main.dart
@@ -0,0 +1,26 @@
+// Problem: Minimum Suffix Flips
+// Link to the problem: https://leetcode.com/problems/minimum-suffix-flips/
+class Solution {
+  int minFlips(String target) {
+    int ans = 0;
+    bool x = false;
+    if (target[0] == '1') {
+      ans++;
+      x = true;
+    }
+    for (int i = 1; i < target.length; i++) {
+      if (target[i] == '1') {
+        if (x == false) {
+          ans++;
+          x = true;
+        }
+      } else {
+        if (x == true) {
+          ans++;
+          x = false;
+        }
+      }
+    }
+    return ans;
+  }
+}
