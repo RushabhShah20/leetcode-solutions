71e2a96f8961c1dd151c20598fd76fca2cddf9e4
Rushabh <20rush09abh2001@gmail.com>
2025-09-02 13:51:06 +0530

    Added C++ solution to 'Find the Number of Ways to Place People I'

---
+++ medium/Find the Number of Ways to Place People I/C++/main.cpp
@@ -0,0 +1,41 @@
+// Problem: Find the Number of Ways to Place People I
+// Link to the problem: https://leetcode.com/problems/find-the-number-of-ways-to-place-people-i/
+class Solution
+{
+public:
+    int numberOfPairs(vector<vector<int>> &points)
+    {
+        int ans = 0;
+        for (int i = 0; i < points.size(); i++)
+        {
+            for (int j = 0; j < points.size(); j++)
+            {
+                if (i != j)
+                {
+                    int x1 = points[i][0], y1 = points[i][1], x2 = points[j][0], y2 = points[j][1];
+                    if ((x1 <= x2) && (y1 >= y2))
+                    {
+                        bool z = true;
+                        for (int k = 0; k < points.size(); k++)
+                        {
+                            int x = points[k][0], y = points[k][1];
+                            if (i != k && j != k)
+                            {
+                                if (((x1 <= x) && (x2 >= x)) && ((y1 >= y) && (y2 <= y)))
+                                {
+                                    z = false;
+                                    break;
+                                }
+                            }
+                        }
+                        if (z == true)
+                        {
+                            ans++;
+                        }
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
