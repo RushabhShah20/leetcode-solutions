5f882dc8587d968bdb6ecb302d4c23fd961fa30e
Rushabh <20rush09abh2001@gmail.com>
2025-09-03 13:51:08 +0530

    Added Typescript solution to 'Odd String Difference'

---
+++ easy/Odd String Difference/Typescript/main.ts
@@ -0,0 +1,40 @@
+// Problem: Odd String Difference
+// Link to the problem: https://leetcode.com/problems/odd-string-difference/
+function arraysEqual(arr1: number[], arr2: number[]) {
+    if (arr1.length !== arr2.length) return false;
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) return false;
+    }
+    return true;
+}
+function oddString(words: string[]): string {
+    let ans: string = "";
+    let diff: number[][] = new Array();
+    for (let i = 0; i < words.length; i++) {
+        diff[i] = new Array(words[0].length - 1);
+    }
+    for (let i = 0; i < words.length; i++) {
+        for (let j = 0; j < words[i].length - 1; j++) {
+            diff[i][j] = words[i].charCodeAt(j + 1) - words[i].charCodeAt(j);
+        }
+    }
+    if (arraysEqual(diff[1], diff[2])) {
+        if (!arraysEqual(diff[0], diff[1]) && !arraysEqual(diff[0], diff[2])) {
+            return words[0];
+        }
+    }
+    for (let i = 1; i < diff.length - 1; i++) {
+        if (arraysEqual(diff[i - 1], diff[i + 1])) {
+            if (!arraysEqual(diff[i], diff[i - 1]) && !arraysEqual(diff[i], diff[i + 1])) {
+                return words[i];
+            }
+        }
+    }
+    if (arraysEqual(diff[words.length - 3], diff[words.length - 2])) {
+        if (!arraysEqual(diff[words.length - 1], diff[words.length - 2]) &&
+            !arraysEqual(diff[words.length - 1], diff[words.length - 3])) {
+            return words[words.length - 1];
+        }
+    }
+    return ans;
+};
