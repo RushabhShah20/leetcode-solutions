58f33c307bcbb4e7463870b453bfd85cba13e8b0
Rushabh <20rush09abh2001@gmail.com>
2025-09-05 16:13:14 +0530

    Added Typescript solution to 'Vowels Game in a String'

---
+++ medium/Vowels Game in a String/Typescript/main.ts
@@ -0,0 +1,10 @@
+// Problem: Vowels Game in a String
+// Link to the problem: https://leetcode.com/problems/vowels-game-in-a-string/
+function doesAliceWin(s: string): boolean {
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === 'a' || s[i] === 'e' || s[i] === 'i' || s[i] === 'o' || s[i] === 'u') {
+            return true;
+        }
+    }
+    return false;
+};
