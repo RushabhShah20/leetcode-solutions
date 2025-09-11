2355b7e83f2387f925edcdc47503e9e665086348
Rushabh <20rush09abh2001@gmail.com>
2025-09-11 11:38:03 +0530

    Added Typescript solution to 'Sort Vowels in a String'

---
+++ medium/Sort Vowels in a String/Typescript/main.ts
@@ -0,0 +1,18 @@
+// Problem: Sort Vowels in a String
+// Link to the problem: https://leetcode.com/problems/sort-vowels-in-a-string/
+function sortVowels(s: string): string {
+    let indices: number[] = new Array();
+    let alphabets: string[] = new Array();
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === 'a' || s[i] === 'e' || s[i] === 'i' || s[i] === 'o' || s[i] === 'u' || s[i] === 'A' || s[i] === 'E' || s[i] === 'I' || s[i] === 'O' || s[i] === 'U') {
+            indices.push(i);
+            alphabets.push(s[i]);
+        }
+    }
+    alphabets.sort();
+    let t: string[] = s.split('');
+    for (let i = 0; i < indices.length; i++) {
+        t[indices[i]] = alphabets[i];
+    }
+    return t.join('');
+};
