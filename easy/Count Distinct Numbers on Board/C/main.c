199e35e0496d743bd7e11488f715eda94f5184a1
Rushabh <20rush09abh2001@gmail.com>
2025-09-12 12:15:21 +0530

    Added C solution to 'Count Distinct Numbers on Board'

---
+++ easy/Count Distinct Numbers on Board/C/main.c
@@ -0,0 +1,6 @@
+// Problem: Count Distinct Numbers on Board
+// Link to the problem: https://leetcode.com/problems/count-distinct-numbers-on-board/
+int distinctIntegers(int n)
+{
+    return ((n == 1) ? (1) : (n - 1));
+}
