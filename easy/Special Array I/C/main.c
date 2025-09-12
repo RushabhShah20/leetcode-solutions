a77a72d76d2957bbefe8a6e644481dd20a5e9b6b
Rushabh <20rush09abh2001@gmail.com>
2025-09-12 11:51:22 +0530

    Added C solution to 'Special Array I'

---
+++ easy/Special Array I/C/main.c
@@ -0,0 +1,17 @@
+// Problem: Special Array I
+// Link to the problem: https://leetcode.com/problems/special-array-i/
+bool isArraySpecial(int *nums, int numsSize)
+{
+    for (int i = 0; i < numsSize - 1; i++)
+    {
+        if (nums[i] % 2 == 0 && nums[i + 1] % 2 == 0)
+        {
+            return false;
+        }
+        if (nums[i] % 2 != 0 && nums[i + 1] % 2 != 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
