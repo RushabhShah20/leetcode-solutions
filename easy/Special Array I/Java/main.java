09f812020a03f86c50f12565a7c23bb914849d25
Rushabh <20rush09abh2001@gmail.com>
2025-09-12 11:51:09 +0530

    Added Java solution to 'Special Array I'

---
+++ easy/Special Array I/Java/main.java
@@ -0,0 +1,15 @@
+// Problem: Special Array I
+// Link to the problem: https://leetcode.com/problems/special-array-i/
+class Solution {
+    public boolean isArraySpecial(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] % 2 == 0 && nums[i + 1] % 2 == 0) {
+                return false;
+            }
+            if (nums[i] % 2 != 0 && nums[i + 1] % 2 != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
