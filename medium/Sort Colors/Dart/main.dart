2401a81d619b38549adb8d2af0a98b76d99833e7
Rushabh <20rush09abh2001@gmail.com>
2025-09-12 11:24:05 +0530

    Added Dart solution to 'Sort Colors'

---
+++ medium/Sort Colors/Dart/main.dart
@@ -0,0 +1,28 @@
+// Problem: Sort Colors
+// Link to the problem: https://leetcode.com/problems/sort-colors/
+class Solution {
+  void sortColors(List<int> nums) {
+    int zeroes = 0, ones = 0, twos = 0;
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] == 0) {
+        zeroes++;
+      } else if (nums[i] == 1) {
+        ones++;
+      } else {
+        twos++;
+      }
+    }
+    for (int i = 0; i < nums.length; i++) {
+      if (zeroes > 0) {
+        nums[i] = 0;
+        zeroes--;
+      } else if (ones > 0) {
+        nums[i] = 1;
+        ones--;
+      } else {
+        nums[i] = 2;
+        twos--;
+      }
+    }
+  }
+}
