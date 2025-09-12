59f15e9758fe398ff2f36010f51f3a705c2e7127
Rushabh <20rush09abh2001@gmail.com>
2025-09-12 12:16:19 +0530

    Added JavaScript solution to 'Count Distinct Numbers on Board'

---
+++ easy/Count Distinct Numbers on Board/JavaScript/main.js
@@ -0,0 +1,9 @@
+// Problem: Count Distinct Numbers on Board
+// Link to the problem: https://leetcode.com/problems/count-distinct-numbers-on-board/
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var distinctIntegers = function (n) {
+    return ((n === 1) ? (1) : (n - 1));
+};
