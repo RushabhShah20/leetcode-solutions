1a30cd0e63768c93e95aa906ba76cbda0b3dd74e
Rushabh <20rush09abh2001@gmail.com>
2025-09-12 12:15:49 +0530

    Added C# solution to 'Count Distinct Numbers on Board'

---
+++ easy/Count Distinct Numbers on Board/C#/main.cs
@@ -0,0 +1,9 @@
+// Problem: Count Distinct Numbers on Board
+// Link to the problem: https://leetcode.com/problems/count-distinct-numbers-on-board/
+public class Solution
+{
+    public int DistinctIntegers(int n)
+    {
+        return ((n == 1) ? (1) : (n - 1));
+    }
+}
