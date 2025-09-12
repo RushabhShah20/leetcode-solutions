f7ff3426dfe4d84896bf54ef33c6ad5d7bb89fc6
Rushabh <20rush09abh2001@gmail.com>
2025-09-12 12:16:34 +0530

    Added Typescript solution to 'Count Distinct Numbers on Board'

---
+++ easy/Count Distinct Numbers on Board/Typescript/main.ts
@@ -0,0 +1,5 @@
+// Problem: Count Distinct Numbers on Board
+// Link to the problem: https://leetcode.com/problems/count-distinct-numbers-on-board/
+function distinctIntegers(n: number): number {
+    return ((n === 1) ? (1) : (n - 1));
+};
