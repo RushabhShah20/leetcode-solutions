eaadfa70e775cfb170c2cedfbbcff4a091d8e4d8
Rushabh <20rush09abh2001@gmail.com>
2025-09-21 10:22:21 +0530

    Added JavaScript solution to 'Bitwise OR of Even Numbers in an Array'

---
+++ easy/Bitwise OR of Even Numbers in an Array/JavaScript/main.js
@@ -0,0 +1,15 @@
+// Problem: Bitwise OR of Even Numbers in an Array
+// Link to the problem: https://leetcode.com/problems/bitwise-or-of-even-numbers-in-an-array/
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var evenNumberBitwiseORs = function (nums) {
+    let ans = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] % 2 === 0) {
+            ans |= nums[i];
+        }
+    }
+    return ans;
+};
