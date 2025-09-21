7980256cc7455b64522ed3ffc6de5fb244c58aad
Rushabh <20rush09abh2001@gmail.com>
2025-09-21 10:44:55 +0530

    Added Java solution to 'Maximum Total Subarray Value I'

---
+++ medium/Maximum Total Subarray Value I/Java/main.java
@@ -0,0 +1,18 @@
+// Problem: Maximum Total Subarray Value I
+// Link to the problem: https://leetcode.com/problems/maximum-total-subarray-value-i/
+class Solution {
+    public long maxTotalValue(int[] nums, int k) {
+        long ans = 0;
+        int maxELement = nums[0], minElement = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (maxELement < nums[i]) {
+                maxELement = nums[i];
+            }
+            if (minElement > nums[i]) {
+                minElement = nums[i];
+            }
+        }
+        ans = ((long) (maxELement - minElement) * (long) (k));
+        return ans;
+    }
+}
