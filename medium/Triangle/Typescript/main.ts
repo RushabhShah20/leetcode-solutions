2267b3e749ce30378b961723832705c7a8e7ba5f
Rushabh <20rush09abh2001@gmail.com>
2025-09-25 15:49:16 +0530

    Added Typescript solution to 'Triangle'

---
+++ medium/Triangle/Typescript/main.ts
@@ -0,0 +1,15 @@
+// Problem: Triangle
+// Link to the problem: https://leetcode.com/problems/triangle/
+function minimumTotal(triangle: number[][]): number {
+    let n: number = triangle.length;
+    let dp: number[] = new Array(n);
+    for (let i = 0; i < n; i++) {
+        dp[i] = triangle[n - 1][i];
+    }
+    for (let i = n - 2; i >= 0; i--) {
+        for (let j = 0; j < triangle[i].length; j++) {
+            dp[j] = triangle[i][j] + Math.min(dp[j], dp[j + 1]);
+        }
+    }
+    return dp[0];
+};
