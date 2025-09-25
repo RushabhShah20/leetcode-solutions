a792a4ebadc1a8bc7608da2f6ad56bb99c24fdd4
Rushabh <20rush09abh2001@gmail.com>
2025-09-25 17:31:25 +0530

    Added Java solution to 'Maximum Ascending Subarray Sum'

---
+++ easy/Maximum Ascending Subarray Sum/Java/main.java
@@ -0,0 +1,17 @@
+// Problem: Maximum Ascending Subarray Sum
+// Link to the problem: https://leetcode.com/problems/maximum-ascending-subarray-sum/
+class Solution {
+    public int maxAscendingSum(int[] nums) {
+        int ans = 0, sum = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) {
+                sum += nums[i];
+            } else {
+                ans = Math.max(ans, sum);
+                sum = nums[i];
+            }
+        }
+        ans = Math.max(ans, sum);
+        return ans;
+    }
+}
