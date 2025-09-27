25f4195a0985bbbe7b3b15d11909e8fd7d75e4f5
Rushabh <20rush09abh2001@gmail.com>
2025-09-27 10:48:07 +0530

    Added Dart solution to 'Largest Triangle Area'

---
+++ easy/Largest Triangle Area/Dart/main.dart
@@ -0,0 +1,24 @@
+// Problem: Largest Triangle Area
+// Link to the problem: https://leetcode.com/problems/largest-triangle-area/
+class Solution {
+  double largestTriangleArea(List<List<int>> points) {
+    double ans = 0;
+    int n = points.length;
+    for (int i = 0; i < n; i++) {
+      for (int j = i + 1; j < n; j++) {
+        for (int k = j + 1; k < n; k++) {
+          int x1 = points[i][0],
+              x2 = points[j][0],
+              x3 = points[k][0],
+              y1 = points[i][1],
+              y2 = points[j][1],
+              y3 = points[k][1];
+          double area =
+              0.5 * (x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)).abs();
+          ans = max(ans, area);
+        }
+      }
+    }
+    return ans;
+  }
+}
