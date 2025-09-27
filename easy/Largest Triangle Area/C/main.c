5b4d5d336782278c9c50afdcaa81b39ef74fb053
Rushabh <20rush09abh2001@gmail.com>
2025-09-27 10:46:55 +0530

    Added C solution to 'Largest Triangle Area'

---
+++ easy/Largest Triangle Area/C/main.c
@@ -0,0 +1,20 @@
+// Problem: Largest Triangle Area
+// Link to the problem: https://leetcode.com/problems/largest-triangle-area/
+double largestTriangleArea(int **points, int pointsSize, int *pointsColSize)
+{
+    double ans = 0;
+    int n = pointsSize;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            for (int k = j + 1; k < n; k++)
+            {
+                int x1 = points[i][0], x2 = points[j][0], x3 = points[k][0], y1 = points[i][1], y2 = points[j][1], y3 = points[k][1];
+                double area = 0.5 * abs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2));
+                ans = fmax(ans, area);
+            }
+        }
+    }
+    return ans;
+}
