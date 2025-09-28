287811548f94e3a94ad083ca720f2dfdf41197e9
Rushabh <20rush09abh2001@gmail.com>
2025-09-28 09:21:09 +0530

    Added JavaScript solution to 'Largest Perimeter Triangle'

---
+++ easy/Largest Perimeter Triangle/JavaScript/main.js
@@ -0,0 +1,15 @@
+// Problem: Largest Perimeter Triangle
+// Link to the problem: https://leetcode.com/problems/largest-perimeter-triangle/
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var largestPerimeter = function (nums) {
+    nums.sort((a, b) => a - b);
+    for (let i = nums.length - 1; i > 1; --i) {
+        if (nums[i] < nums[i - 1] + nums[i - 2]) {
+            return nums[i] + nums[i - 1] + nums[i - 2];
+        }
+    }
+    return 0;
+};
