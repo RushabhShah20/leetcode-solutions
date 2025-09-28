7c89a0b9f205529eca7e623d7d11e14dc7e63d6e
Rushabh <20rush09abh2001@gmail.com>
2025-09-28 09:20:30 +0530

    Added C# solution to 'Largest Perimeter Triangle'

---
+++ easy/Largest Perimeter Triangle/C#/main.cs
@@ -0,0 +1,17 @@
+// Problem: Largest Perimeter Triangle
+// Link to the problem: https://leetcode.com/problems/largest-perimeter-triangle/
+public class Solution
+{
+    public int LargestPerimeter(int[] nums)
+    {
+        Array.Sort(nums);
+        for (int i = nums.Length - 1; i > 1; --i)
+        {
+            if (nums[i] < nums[i - 1] + nums[i - 2])
+            {
+                return nums[i] + nums[i - 1] + nums[i - 2];
+            }
+        }
+        return 0;
+    }
+}
