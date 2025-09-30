624ab65c5a7369015e1451b81444f06470e1245a
Rushabh <20rush09abh2001@gmail.com>
2025-09-30 09:44:55 +0530

    Added C solution to 'Find Triangular Sum of an Array'

---
+++ medium/Find Triangular Sum of an Array/C/main.c
@@ -0,0 +1,13 @@
+// Problem: Find Triangular Sum of an Array
+// Link to the problem: https://leetcode.com/problems/find-triangular-sum-of-an-array/
+int triangularSum(int *nums, int numsSize)
+{
+    for (int i = numsSize - 1; i >= 0; i--)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            nums[j] = ((nums[j] + nums[j + 1]) % 10);
+        }
+    }
+    return nums[0];
+}
