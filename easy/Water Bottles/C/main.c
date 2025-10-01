4f04af79e810b98b222256e691e3ff8c86fe4099
Rushabh <20rush09abh2001@gmail.com>
2025-10-01 10:27:24 +0530

    Added C solution to 'Water Bottles'

---
+++ easy/Water Bottles/C/main.c
@@ -0,0 +1,14 @@
+// Problem: Water Bottles
+// Link to the problem: https://leetcode.com/problems/water-bottles/
+int numWaterBottles(int numBottles, int numExchange)
+{
+    int ans = numBottles;
+    while (numBottles >= numExchange)
+    {
+        int x = numBottles / numExchange;
+        int y = numBottles % numExchange;
+        numBottles = x + y;
+        ans += x;
+    }
+    return ans;
+}
