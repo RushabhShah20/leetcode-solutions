15c2072b445700ac51875505a49c34babcd2f6cd
Rushabh <20rush09abh2001@gmail.com>
2025-10-02 16:49:39 +0530

    Added C++ solution to 'Apple Redistribution into Boxes'

---
+++ easy/Apple Redistribution into Boxes/C++/main.cpp
@@ -0,0 +1,33 @@
+// Problem: Apple Redistribution into Boxes
+// Link to the problem: https://leetcode.com/problems/apple-redistribution-into-boxes/
+class Solution
+{
+public:
+    int minimumBoxes(vector<int> &apple, vector<int> &capacity)
+    {
+        int ans = 0;
+        sort(capacity.begin(), capacity.end(), greater<int>());
+        int total = 0;
+        for (int a : apple)
+        {
+            total += a;
+        }
+        for (int c : capacity)
+        {
+            if (total >= c)
+            {
+                total -= c;
+                ans++;
+            }
+            else
+            {
+                if (total != 0)
+                {
+                    ans++;
+                }
+                break;
+            }
+        }
+        return ans;
+    }
+};
