6eca62b019f397024e9d2b3b147bb683925a9d08
Rushabh <20rush09abh2001@gmail.com>
2025-10-04 11:06:53 +0530

    Added C++ solution to 'Container With Most Water'

---
+++ medium/Container With Most Water/C++/main.cpp
@@ -0,0 +1,25 @@
+// Problem: Container With Most Water
+// Link to the problem: https://leetcode.com/problems/container-with-most-water/
+class Solution
+{
+public:
+    int maxArea(vector<int> &height)
+    {
+        int ans = 0, n = height.size();
+        int left = 0, right = n - 1;
+        while (left <= right)
+        {
+            int vol = (right - left) * min(height[left], height[right]);
+            ans = max(ans, vol);
+            if (height[left] <= height[right])
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+        return ans;
+    }
+};
