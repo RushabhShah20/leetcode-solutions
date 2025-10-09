3dc14059b3abea4476cb57999c97e3edb44d3f55
Rushabh <20rush09abh2001@gmail.com>
2025-10-09 14:32:37 +0530

    Added C solution to 'First Bad Version'

---
+++ easy/First Bad Version/C/main.c
@@ -0,0 +1,22 @@
+// Problem: First Bad Version
+// Link to the problem: https://leetcode.com/problems/first-bad-version/
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+int firstBadVersion(int n)
+{
+    int l = 1, r = n;
+    while (l < r)
+    {
+        int m = l + (r - l) / 2;
+        if (isBadVersion(m))
+        {
+            r = m;
+        }
+        else
+        {
+            l = m + 1;
+        }
+    }
+    return l;
+}
