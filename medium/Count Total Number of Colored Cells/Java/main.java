eb9baaa3c3f3c6064890f2231920c91973a47cbc
Rushabh <20rush09abh2001@gmail.com>
2025-10-11 16:10:33 +0530

    Added Java solution to 'Count Total Number of Colored Cells'

---
+++ medium/Count Total Number of Colored Cells/Java/main.java
@@ -0,0 +1,12 @@
+// Problem: Count Total Number of Colored Cells
+// Link to the problem: https://leetcode.com/problems/count-total-number-of-colored-cells/
+class Solution {
+    public long coloredCells(int n) {
+        long ans = 0;
+        for (long i = 1; i < (2 * n) - 1; i += 2) {
+            ans += (2 * i);
+        }
+        ans += ((2 * n) - 1);
+        return ans;
+    }
+}
