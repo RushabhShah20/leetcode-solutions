349908066979a224ea769b862f34d6309c8f60fa
Rushabh <20rush09abh2001@gmail.com>
2025-10-13 16:33:44 +0530

    Added C++ solution to 'Find Largest Value in Each Tree Row'

---
+++ medium/Find Largest Value in Each Tree Row/C++/main.cpp
@@ -0,0 +1,42 @@
+// Problem: Find Largest Value in Each Tree Row
+// Link to the problem: https://leetcode.com/problems/find-largest-value-in-each-tree-row/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    void levelOrder(TreeNode *root, int level, vector<vector<int>> &res)
+    {
+        if (root == nullptr)
+        {
+            return;
+        }
+        if (res.size() <= level)
+        {
+            res.push_back({});
+        }
+        res[level].push_back(root->val);
+        levelOrder(root->left, level + 1, res);
+        levelOrder(root->right, level + 1, res);
+    }
+    vector<int> largestValues(TreeNode *root)
+    {
+        vector<vector<int>> a;
+        levelOrder(root, 0, a);
+        vector<int> ans;
+        for (vector<int> i : a)
+        {
+            ans.push_back(*max_element(i.begin(), i.end()));
+        }
+        return ans;
+    }
+};
