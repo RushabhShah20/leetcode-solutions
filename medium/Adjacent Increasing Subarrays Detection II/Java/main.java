8c8d2e25f696b43ad0b2e5290f2ff5421cd6368f
Rushabh <20rush09abh2001@gmail.com>
2025-10-15 13:37:33 +0530

    Added Java solution to 'Adjacent Increasing Subarrays Detection II'

---
+++ medium/Adjacent Increasing Subarrays Detection II/Java/main.java
@@ -0,0 +1,21 @@
+// Problem: Adjacent Increasing Subarrays Detection II
+// Link to the problem: https://leetcode.com/problems/adjacent-increasing-subarrays-detection-ii/
+class Solution {
+    public int maxIncreasingSubarrays(List<Integer> nums) {
+        int n = nums.size();
+        int prevK = 1;
+        int k = 1;
+        int res = 1;
+        for (int i = 0; i < n - 1; i++) {
+            if (nums.get(i + 1) > nums.get(i))
+                k++;
+            else {
+                prevK = k;
+                k = 1;
+            }
+            res = Math.max(res, Math.min(prevK, k));
+            res = Math.max(res, k / 2);
+        }
+        return res;
+    }
+}
