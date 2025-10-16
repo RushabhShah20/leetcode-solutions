3a8120fca6a0715793e4a31679bcc97b39d32f63
Rushabh <20rush09abh2001@gmail.com>
2025-10-16 19:45:18 +0530

    Added Dart solution to 'Maximum Sum of an Hourglass'

---
+++ medium/Maximum Sum of an Hourglass/Dart/main.dart
@@ -0,0 +1,21 @@
+// Problem: Maximum Sum of an Hourglass
+// Link to the problem: https://leetcode.com/problems/maximum-sum-of-an-hourglass/
+class Solution {
+  int maxSum(List<List<int>> grid) {
+    int m = grid.length, n = grid[0].length, ans = 0;
+    for (int i = 1; i < m - 1; i++) {
+      for (int j = 1; j < n - 1; j++) {
+        int x =
+            grid[i - 1][j - 1] +
+            grid[i - 1][j] +
+            grid[i - 1][j + 1] +
+            grid[i][j] +
+            grid[i + 1][j - 1] +
+            grid[i + 1][j] +
+            grid[i + 1][j + 1];
+        ans = max(ans, x);
+      }
+    }
+    return ans;
+  }
+}
