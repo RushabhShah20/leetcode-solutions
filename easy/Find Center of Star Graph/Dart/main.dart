0bd7866d5f36188ea784ca868e8b73ed6cd657c5
Rushabh <20rush09abh2001@gmail.com>
2025-10-17 15:23:06 +0530

    Added Dart solution to 'Find Center of Star Graph'

---
+++ easy/Find Center of Star Graph/Dart/main.dart
@@ -0,0 +1,13 @@
+// Problem: Find Center of Star Graph
+// Link to the problem: https://leetcode.com/problems/find-center-of-star-graph/
+class Solution {
+  int findCenter(List<List<int>> edges) {
+    int a = edges[0][1];
+    int b = edges[0][0];
+    if (edges[1][1] == a || edges[1][0] == a) {
+      return a;
+    } else {
+      return b;
+    }
+  }
+}
