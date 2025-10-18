cc147f798dcf1cfaa839d8e6b9d4cecabbcd005b
Rushabh <20rush09abh2001@gmail.com>
2025-10-18 15:46:35 +0530

    Added Typescript solution to 'Insert into a Binary Search Tree'

---
+++ medium/Insert into a Binary Search Tree/Typescript/main.ts
@@ -0,0 +1,28 @@
+// Problem: Insert into a Binary Search Tree
+// Link to the problem: https://leetcode.com/problems/insert-into-a-binary-search-tree/
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function insertIntoBST(root: TreeNode | null, val: number): TreeNode | null {
+    if (root === null) {
+        return new TreeNode(val);
+    }
+    if (val > root.val) {
+        root.right = insertIntoBST(root.right, val);
+    }
+    else {
+        root.left = insertIntoBST(root.left, val);
+    }
+    return root;
+};
