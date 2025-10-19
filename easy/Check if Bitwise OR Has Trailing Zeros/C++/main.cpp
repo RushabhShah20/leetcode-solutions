270b26a6d63389bc7059f150184772a3a18d86c0
Rushabh <20rush09abh2001@gmail.com>
2025-10-19 09:18:36 +0530

    Added C++ solution to 'Check if Bitwise OR Has Trailing Zeros'

---
+++ easy/Check if Bitwise OR Has Trailing Zeros/C++/main.cpp
@@ -0,0 +1,22 @@
+// Problem: Check if Bitwise OR Has Trailing Zeros
+// Link to the problem: https://leetcode.com/problems/check-if-bitwise-or-has-trailing-zeros/
+class Solution
+{
+public:
+    bool hasTrailingZeros(vector<int> &nums)
+    {
+        int count = 0;
+        for (int num : nums)
+        {
+            if (num % 2 == 0)
+            {
+                count++;
+            }
+            if (count == 2)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
