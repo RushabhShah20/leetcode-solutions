b9ebe977bdcdf6f3d13eb3855aad40db7c6159b7
Rushabh <20rush09abh2001@gmail.com>
2025-10-21 16:27:22 +0530

    Added C solution to 'Construct the Longest New String'

---
+++ medium/Construct the Longest New String/C/main.c
@@ -0,0 +1,17 @@
+// Problem: Construct the Longest New String
+// Link to the problem: https://leetcode.com/problems/construct-the-longest-new-string/
+int longestString(int x, int y, int z)
+{
+    if (x > y)
+    {
+        return ((2 * ((y) + (y + 1))) + (2 * z));
+    }
+    else if (x < y)
+    {
+        return ((2 * ((x) + (x + 1))) + (2 * z));
+    }
+    else
+    {
+        return ((2 * (x + y)) + (2 * z));
+    }
+}
