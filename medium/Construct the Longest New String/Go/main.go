3fb4117704c39297f1eafa71672e297b11ff6d7e
Rushabh <20rush09abh2001@gmail.com>
2025-10-21 16:28:37 +0530

    Added Go solution to 'Construct the Longest New String'

---
+++ medium/Construct the Longest New String/Go/main.go
@@ -0,0 +1,11 @@
+// Problem: Construct the Longest New String
+// Link to the problem: https://leetcode.com/problems/construct-the-longest-new-string/
+func longestString(x int, y int, z int) int {
+	if x > y {
+		return ((2 * ((y) + (y + 1))) + (2 * z))
+	} else if x < y {
+		return ((2 * ((x) + (x + 1))) + (2 * z))
+	} else {
+		return ((2 * (x + y)) + (2 * z))
+	}
+}
