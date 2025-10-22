e45f982821caec7d381daabe47417e0c53d1a4cf
Rushabh <20rush09abh2001@gmail.com>
2025-10-22 16:16:45 +0530

    Added C++ solution to 'Maximum Score After Splitting a String'

---
+++ easy/Maximum Score After Splitting a String/C++/main.cpp
@@ -0,0 +1,38 @@
+// Problem: Maximum Score After Splitting a String
+// Link to the problem: https://leetcode.com/problems/maximum-score-after-splitting-a-string/
+class Solution
+{
+public:
+    int maxScore(string s)
+    {
+        int ans = 0;
+        vector<int> pref(s.size(), 0);
+        if (s[0] == '1')
+        {
+            pref[0] = 0;
+        }
+        else
+        {
+            pref[0] = 1;
+        }
+        for (int i = 1; i < s.size(); i++)
+        {
+            {
+                if (s[i] == '1')
+                {
+                    pref[i] = pref[i - 1];
+                }
+                else
+                {
+                    pref[i] = pref[i - 1] + 1;
+                }
+            }
+        }
+        for (int i = 0; i < s.size() - 1; i++)
+        {
+            int score = (pref[i] + ((s.size() - i - 1) - (pref[s.size() - 1] - pref[i])));
+            ans = max(ans, score);
+        }
+        return ans;
+    }
+};
