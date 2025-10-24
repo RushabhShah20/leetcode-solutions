f61451c4f002cf14a2d4eb4ea00f87d4034628e1
Rushabh <20rush09abh2001@gmail.com>
2025-10-24 19:27:24 +0530

    Added C++ solution to 'Construct the Minimum Bitwise Array I'

---
+++ easy/Construct the Minimum Bitwise Array I/C++/main.cpp
@@ -0,0 +1,23 @@
+// Problem: Construct the Minimum Bitwise Array I
+// Link to the problem: https://leetcode.com/problems/construct-the-minimum-bitwise-array-i/
+class Solution
+{
+public:
+    vector<int> minBitwiseArray(vector<int> &nums)
+    {
+        int n = nums.size();
+        vector<int> ans(n);
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] % 2 == 0)
+            {
+                ans[i] = -1;
+            }
+            else
+            {
+                ans[i] = nums[i] - (((nums[i] + 1) & (-nums[i] - 1)) / 2);
+            }
+        }
+        return ans;
+    }
+};
