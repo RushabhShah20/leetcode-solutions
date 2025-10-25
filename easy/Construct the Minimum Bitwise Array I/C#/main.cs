2173cc76bbb68296a1408d0489f8c7086e992f07
Rushabh <20rush09abh2001@gmail.com>
2025-10-25 06:18:57 +0530

    Added C# solution to 'Construct the Minimum Bitwise Array I'

---
+++ easy/Construct the Minimum Bitwise Array I/C#/main.cs
@@ -0,0 +1,22 @@
+// Problem: Construct the Minimum Bitwise Array I
+// Link to the problem: https://leetcode.com/problems/construct-the-minimum-bitwise-array-i/
+public class Solution
+{
+    public int[] MinBitwiseArray(IList<int> nums)
+    {
+        int n = nums.Count;
+        int[] ans = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] % 2 == 0)
+            {
+                ans[i] = -1;
+            }
+            else
+            {
+                ans[i] = nums[i] - (((nums[i] + 1) & (-nums[i] - 1)) / 2);
+            }
+        }
+        return ans;
+    }
+}
