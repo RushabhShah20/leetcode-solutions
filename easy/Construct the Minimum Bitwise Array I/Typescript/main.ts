fcce5ef5af6b0f387fc2cd6d745f4cd329213751
Rushabh <20rush09abh2001@gmail.com>
2025-10-25 06:19:46 +0530

    Added Typescript solution to 'Construct the Minimum Bitwise Array I'

---
+++ easy/Construct the Minimum Bitwise Array I/Typescript/main.ts
@@ -0,0 +1,15 @@
+// Problem: Construct the Minimum Bitwise Array I
+// Link to the problem: https://leetcode.com/problems/construct-the-minimum-bitwise-array-i/
+function minBitwiseArray(nums: number[]): number[] {
+    let n: number = nums.length;
+    let ans: number[] = new Array(n);
+    for (let i = 0; i < n; i++) {
+        if (nums[i] % 2 == 0) {
+            ans[i] = -1;
+        }
+        else {
+            ans[i] = nums[i] - Math.floor(((nums[i] + 1) & (-nums[i] - 1)) / 2);
+        }
+    }
+    return ans;
+};
