90864a656e925cfd83ddeee0a6804a26c387f258
Rushabh <20rush09abh2001@gmail.com>
2025-10-27 11:54:11 +0530

    Added C solution to 'Find the Winning Player in Coin Game'

---
+++ easy/Find the Winning Player in Coin Game/C/main.c
@@ -0,0 +1,7 @@
+// Problem: Find the Winning Player in Coin Game
+// Link to the problem: https://leetcode.com/problems/find-the-winning-player-in-coin-game/
+char *winningPlayer(int x, int y)
+{
+    int ans = fmin(x, y / 4);
+    return (ans % 2 == 0) ? ("Bob") : ("Alice");
+}
