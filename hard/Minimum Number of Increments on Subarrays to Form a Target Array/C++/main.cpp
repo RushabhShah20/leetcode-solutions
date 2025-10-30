42c230773789485205f51a5802bbbd501c40bac6
Rushabh <20rush09abh2001@gmail.com>
2025-10-30 14:38:44 +0530

    Added C++ solution to 'Minimum Number of Increments on Subarrays to Form a Target Array'

---
+++ hard/Minimum Number of Increments on Subarrays to Form a Target Array/C++/main.cpp
@@ -0,0 +1,18 @@
+// Problem: Minimum Number of Increments on Subarrays to Form a Target Array
+// Link to the problem: https://leetcode.com/problems/minimum-number-of-increments-on-subarrays-to-form-a-target-array/
+class Solution
+{
+public:
+    int minNumberOperations(vector<int> &target)
+    {
+        int n = target.size(), ans = target[0];
+        for (int i = 1; i < n; i++)
+        {
+            if (target[i] > target[i - 1])
+            {
+                ans += (target[i] - target[i - 1]);
+            }
+        }
+        return ans;
+    }
+};
