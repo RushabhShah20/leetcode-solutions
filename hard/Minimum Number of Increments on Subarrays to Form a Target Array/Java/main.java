f70a6ead34640d8e40085da5dd4d54f996f552c3
Rushabh <20rush09abh2001@gmail.com>
2025-10-30 14:51:04 +0530

    Added Java solution to 'Minimum Number of Increments on Subarrays to Form a Target Array'

---
+++ hard/Minimum Number of Increments on Subarrays to Form a Target Array/Java/main.java
@@ -0,0 +1,13 @@
+// Problem: Minimum Number of Increments on Subarrays to Form a Target Array
+// Link to the problem: https://leetcode.com/problems/minimum-number-of-increments-on-subarrays-to-form-a-target-array/
+class Solution {
+    public int minNumberOperations(int[] target) {
+        int n = target.length, ans = target[0];
+        for (int i = 1; i < n; i++) {
+            if (target[i] > target[i - 1]) {
+                ans += (target[i] - target[i - 1]);
+            }
+        }
+        return ans;
+    }
+}
