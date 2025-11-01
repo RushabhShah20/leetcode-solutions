2805fc1ea7c37793979caf7723747879782ea23c
Rushabh <20rush09abh2001@gmail.com>
2025-11-01 11:52:47 +0530

    Added JavaScript solution to 'Remove Linked List Elements'

---
+++ easy/Remove Linked List Elements/JavaScript/main.js
@@ -0,0 +1,29 @@
+// Problem: Remove Linked List Elements
+// Link to the problem: https://leetcode.com/problems/remove-linked-list-elements/
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function (head, val) {
+    if (head === null) {
+        return head;
+    }
+    let dummy = new ListNode(0, head);
+    let cur = dummy;
+    while (cur.next !== null) {
+        if (cur.next.val === val) {
+            cur.next = cur.next.next;
+        } else {
+            cur = cur.next;
+        }
+    }
+    return dummy.next;
+};
