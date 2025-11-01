671169213890ee9c23e5fe7874d8c2faa8ddc6a8
Rushabh <20rush09abh2001@gmail.com>
2025-11-01 11:53:30 +0530

    Added Go solution to 'Remove Linked List Elements'

---
+++ easy/Remove Linked List Elements/Go/main.go
@@ -0,0 +1,24 @@
+// Problem: Remove Linked List Elements
+// Link to the problem: https://leetcode.com/problems/remove-linked-list-elements/
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func removeElements(head *ListNode, val int) *ListNode {
+	if head == nil {
+		return head
+	}
+	var dummy *ListNode = &ListNode{0, head}
+	var cur *ListNode = dummy
+	for cur.Next != nil {
+		if cur.Next.Val == val {
+			cur.Next = cur.Next.Next
+		} else {
+			cur = cur.Next
+		}
+	}
+	return dummy.Next
+}
