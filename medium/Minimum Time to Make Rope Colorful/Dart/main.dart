7194afdc9140e5709f543135263901e469aa3cb5
Rushabh <20rush09abh2001@gmail.com>
2025-11-03 16:56:44 +0530

    Added Dart solution to 'Minimum Time to Make Rope Colorful'

---
+++ medium/Minimum Time to Make Rope Colorful/Dart/main.dart
@@ -0,0 +1,14 @@
+// Problem: Minimum Time to Make Rope Colorful
+// Link to the problem: https://leetcode.com/problems/minimum-time-to-make-rope-colorful/
+class Solution {
+  int minCost(String colors, List<int> neededTime) {
+    int n = colors.length, ans = 0;
+    for (int i = 1; i < n; i++) {
+      if (colors[i] == colors[i - 1]) {
+        ans += (min(neededTime[i], neededTime[i - 1]));
+        neededTime[i] = max(neededTime[i], neededTime[i - 1]);
+      }
+    }
+    return ans;
+  }
+}
