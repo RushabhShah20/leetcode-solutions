400de4f35cfe8f6037070a75cc488c23728e9540
Rushabh <20rush09abh2001@gmail.com>
2025-11-03 19:22:47 +0530

    Added Typescript solution to 'Distribute Money to Maximum Children'

---
+++ easy/Distribute Money to Maximum Children/Typescript/main.ts
@@ -0,0 +1,39 @@
+// Problem: Distribute Money to Maximum Children
+// Link to the problem: https://leetcode.com/problems/distribute-money-to-maximum-children/
+function distMoney(money: number, children: number): number {
+    if (money < children) {
+        return -1;
+    } else {
+        let ans: number = 0;
+        let a: number[] = new Array(children).fill(1);
+        money -= children;
+        for (let i = 0; i < children; i++) {
+            if (money < 7) {
+                if (money === 3) {
+                    if (i === children - 1) {
+                        a[i - 1]++;
+                    } else {
+                        a[i + 1]++;
+                    }
+                    a[i] += 2;
+                } else {
+                    a[i] += money;
+                }
+                money = 0;
+                break;
+            } else {
+                a[i] += 7;
+                money -= 7;
+            }
+        }
+        if (money != 0) {
+            a[children - 1] += money;
+        }
+        for (const i of a) {
+            if (i === 8) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
