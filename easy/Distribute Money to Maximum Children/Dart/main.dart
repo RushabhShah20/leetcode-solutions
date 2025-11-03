6004d18b7f1897954dc9d4759fa74e48621aeb61
Rushabh <20rush09abh2001@gmail.com>
2025-11-03 19:22:59 +0530

    Added Dart solution to 'Distribute Money to Maximum Children'

---
+++ easy/Distribute Money to Maximum Children/Dart/main.dart
@@ -0,0 +1,41 @@
+// Problem: Distribute Money to Maximum Children
+// Link to the problem: https://leetcode.com/problems/distribute-money-to-maximum-children/
+class Solution {
+  int distMoney(int money, int children) {
+    if (money < children) {
+      return -1;
+    } else {
+      int ans = 0;
+      List<int> a = new List.filled(children, 1);
+      money -= children;
+      for (int i = 0; i < children; i++) {
+        if (money < 7) {
+          if (money == 3) {
+            if (i == children - 1) {
+              a[i - 1]++;
+            } else {
+              a[i + 1]++;
+            }
+            a[i] += 2;
+          } else {
+            a[i] += money;
+          }
+          money = 0;
+          break;
+        } else {
+          a[i] += 7;
+          money -= 7;
+        }
+      }
+      if (money != 0) {
+        a[children - 1] += money;
+      }
+      for (int i in a) {
+        if (i == 8) {
+          ans++;
+        }
+      }
+      return ans;
+    }
+  }
+}
