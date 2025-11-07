db3fc9ad9e70a49ccd81137387cb135c74c2463a
Rushabh <20rush09abh2001@gmail.com>
2025-11-07 19:26:15 +0530

    Added C++ solution to 'Minimum Time Visiting All Points'

---
+++ easy/Minimum Time Visiting All Points/C++/main.cpp
@@ -0,0 +1,15 @@
+// Problem: Minimum Time Visiting All Points
+// Link to the problem: https://leetcode.com/problems/minimum-time-visiting-all-points/
+class Solution
+{
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>> &points)
+    {
+        int n = points.size(), ans = 0;
+        for (int i = 1; i < n; i++)
+        {
+            ans += max(abs(points[i - 1][1] - points[i][1]), abs(points[i - 1][0] - points[i][0]));
+        }
+        return ans;
+    }
+};
