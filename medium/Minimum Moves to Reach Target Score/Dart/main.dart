8562131d11373f708c14391323f36bd19fe55533
Rushabh <20rush09abh2001@gmail.com>
2025-11-07 18:34:43 +0530

    Added Dart solution to 'Minimum Moves to Reach Target Score'

---
+++ medium/Minimum Moves to Reach Target Score/Dart/main.dart
@@ -0,0 +1,26 @@
+// Problem: Minimum Moves to Reach Target Score
+// Link to the problem: https://leetcode.com/problems/minimum-moves-to-reach-target-score/
+class Solution {
+  int minMoves(int target, int maxDoubles) {
+    if (target == 1) {
+      return 0;
+    }
+    int ans = 0;
+    while (maxDoubles > 0) {
+      if (target % 2 == 0) {
+        target ~/= 2;
+        maxDoubles--;
+      } else {
+        target--;
+      }
+      ans++;
+      if (target == 1) {
+        break;
+      }
+    }
+    if (target != 1) {
+      ans += (target - 1);
+    }
+    return ans;
+  }
+}
