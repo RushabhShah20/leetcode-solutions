750f2ac70b70f94d4c003845e2697cc8cb4887d4
Rushabh <20rush09abh2001@gmail.com>
2025-11-07 19:41:13 +0530

    Added JavaScript solution to 'Minimum Time Visiting All Points'

---
+++ easy/Minimum Time Visiting All Points/JavaScript/main.js
@@ -0,0 +1,13 @@
+// Problem: Minimum Time Visiting All Points
+// Link to the problem: https://leetcode.com/problems/minimum-time-visiting-all-points/
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+var minTimeToVisitAllPoints = function (points) {
+    let n = points.length, ans = 0;
+    for (let i = 1; i < n; i++) {
+        ans += Math.max(Math.abs(points[i - 1][1] - points[i][1]), Math.abs(points[i - 1][0] - points[i][0]));
+    }
+    return ans;
+};
