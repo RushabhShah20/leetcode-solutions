1486c960a06bf4aa7fdd5f6a0a3664b05ba11aab
Rushabh <20rush09abh2001@gmail.com>
2025-11-07 17:06:46 +0530

    Added Typescript solution to 'Minimum Number of Pushes to Type Word I'

---
+++ easy/Minimum Number of Pushes to Type Word I/Typescript/main.ts
@@ -0,0 +1,20 @@
+// Problem: Minimum Number of Pushes to Type Word I
+// Link to the problem: https://leetcode.com/problems/minimum-number-of-pushes-to-type-word-i/
+function minimumPushes(word: string): number {
+    let ans: number = 0, n: number = word.length;
+    for (let i = 0; i < n; i++) {
+        if (i < 8) {
+            ans += 1;
+        }
+        else if (i >= 8 && i < 16) {
+            ans += 2;
+        }
+        else if (i >= 16 && i < 24) {
+            ans += 3;
+        }
+        else {
+            ans += 4;
+        }
+    }
+    return ans;
+};
