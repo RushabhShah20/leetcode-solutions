1beb186e92ef8437e0e85d63cf18ca14512c9bbc
Rushabh <20rush09abh2001@gmail.com>
2025-11-07 19:41:27 +0530

    Added Typescript solution to 'Minimum Time Visiting All Points'

---
+++ easy/Minimum Time Visiting All Points/Typescript/main.ts
@@ -0,0 +1,9 @@
+// Problem: Minimum Time Visiting All Points
+// Link to the problem: https://leetcode.com/problems/minimum-time-visiting-all-points/
+function minTimeToVisitAllPoints(points: number[][]): number {
+    let n: number = points.length, ans: number = 0;
+    for (let i = 1; i < n; i++) {
+        ans += Math.max(Math.abs(points[i - 1][1] - points[i][1]), Math.abs(points[i - 1][0] - points[i][0]));
+    }
+    return ans;
+};
