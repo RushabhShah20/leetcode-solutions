51f5d72da823e406649bf310c5321f5c4da107bf
Rushabh <20rush09abh2001@gmail.com>
2025-11-07 17:06:31 +0530

    Added JavaScript solution to 'Minimum Number of Pushes to Type Word I'

---
+++ easy/Minimum Number of Pushes to Type Word I/JavaScript/main.js
@@ -0,0 +1,24 @@
+// Problem: Minimum Number of Pushes to Type Word I
+// Link to the problem: https://leetcode.com/problems/minimum-number-of-pushes-to-type-word-i/
+/**
+ * @param {string} word
+ * @return {number}
+ */
+var minimumPushes = function (word) {
+    let ans = 0, n = word.length;
+    for (let i = 0; i < n; i++) {
+        if (i < 8) {
+            ans += 1;
+        }
+        else if (i >= 8 && i < 16) {
+            ans += 2;
+        }
+        else if (i >= 16 && i < 24) {
+            ans += 3;
+        }
+        else {
+            ans += 4;
+        }
+    }
+    return ans;
+};
