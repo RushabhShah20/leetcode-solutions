f6cb6d292145c604ff5f31f9fcd1f629be44b4d5
Rushabh <20rush09abh2001@gmail.com>
2025-11-07 19:42:26 +0530

    Added Dart solution to 'Minimum Time Visiting All Points'

---
+++ easy/Minimum Time Visiting All Points/Dart/main.dart
@@ -0,0 +1,14 @@
+// Problem: Minimum Time Visiting All Points
+// Link to the problem: https://leetcode.com/problems/minimum-time-visiting-all-points/
+class Solution {
+  int minTimeToVisitAllPoints(List<List<int>> points) {
+    int n = points.length, ans = 0;
+    for (int i = 1; i < n; i++) {
+      ans += max(
+        (points[i - 1][1] - points[i][1]).abs(),
+        (points[i - 1][0] - points[i][0]).abs(),
+      );
+    }
+    return ans;
+  }
+}
