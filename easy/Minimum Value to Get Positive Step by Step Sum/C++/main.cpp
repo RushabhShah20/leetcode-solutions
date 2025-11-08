2a396b1563b7e14048689409a51cea1d70d59af2
Rushabh <20rush09abh2001@gmail.com>
2025-11-08 16:02:13 +0530

    Added C++ solution to 'Minimum Value to Get Positive Step by Step Sum'

---
+++ easy/Minimum Value to Get Positive Step by Step Sum/C++/main.cpp
@@ -0,0 +1,20 @@
+// Problem: Minimum Value to Get Positive Step by Step Sum
+// Link to the problem: https://leetcode.com/problems/minimum-value-to-get-positive-step-by-step-sum/
+class Solution
+{
+public:
+    int minStartValue(vector<int> &nums)
+    {
+        int ans = 0, sum = 0;
+        for (int num : nums)
+        {
+            sum += num;
+            if (sum <= 0)
+            {
+                ans += (1 - sum);
+                sum += (1 - sum);
+            }
+        }
+        return ((ans == 0) ? (1) : (ans));
+    }
+};
