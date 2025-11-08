a1cf19591f7a34eacb470599d29c305b8793533b
Rushabh <20rush09abh2001@gmail.com>
2025-11-08 15:30:10 +0530

    Added C++ solution to 'Minimum Cost to Move Chips to The Same Position'

---
+++ easy/Minimum Cost to Move Chips to The Same Position/C++/main.cpp
@@ -0,0 +1,23 @@
+// Problem: Minimum Cost to Move Chips to The Same Position
+// Link to the problem: https://leetcode.com/problems/minimum-cost-to-move-chips-to-the-same-position/
+class Solution
+{
+public:
+    int minCostToMoveChips(vector<int> &position)
+    {
+        int even = 0, odd = 0;
+        for (int i : position)
+        {
+            if (i % 2 == 0)
+            {
+                even++;
+            }
+            else
+            {
+                odd++;
+            }
+        }
+        int ans = min(even, odd);
+        return ans;
+    }
+};
