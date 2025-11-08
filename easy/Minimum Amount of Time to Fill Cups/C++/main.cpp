c33b83e725b57c892ecc418c305964a05cb3cf75
Rushabh <20rush09abh2001@gmail.com>
2025-11-08 14:00:17 +0530

    Added C++ solution to 'Minimum Amount of Time to Fill Cups'

---
+++ easy/Minimum Amount of Time to Fill Cups/C++/main.cpp
@@ -0,0 +1,56 @@
+// Problem: Minimum Amount of Time to Fill Cups
+// Link to the problem: https://leetcode.com/problems/minimum-amount-of-time-to-fill-cups/
+class Solution
+{
+public:
+    int smallest(int a, int b, int c)
+    {
+        if (a < b)
+        {
+            if (a < c)
+            {
+                return a;
+            }
+            else
+            {
+                return c;
+            }
+        }
+        else
+        {
+            if (b < c)
+            {
+                return b;
+            }
+            else
+            {
+                return c;
+            }
+        }
+    }
+    int fillCups(vector<int> &amount)
+    {
+        int ans = 0;
+        int a = amount[0], b = amount[1], c = amount[2];
+        while (a > 0 || b > 0 || c > 0)
+        {
+            if (smallest(a, b, c) == a)
+            {
+                b--;
+                c--;
+            }
+            else if (smallest(a, b, c) == b)
+            {
+                a--;
+                c--;
+            }
+            else
+            {
+                a--;
+                b--;
+            }
+            ans++;
+        }
+        return ans;
+    }
+};
