fc2f0718a3b12d96e96fa34b562266021752a5a4
Rushabh <20rush09abh2001@gmail.com>
2025-11-09 09:01:49 +0530

    Added Java solution to 'Count Operations to Obtain Zero'

---
+++ easy/Count Operations to Obtain Zero/Java/main.java
@@ -0,0 +1,15 @@
+// Problem: Count Operations to Obtain Zero
+// Link to the problem: https://leetcode.com/problems/count-operations-to-obtain-zero/
+class Solution {
+    public int countOperations(int num1, int num2) {
+        int ans = 0, a = Math.max(num1, num2), b = Math.min(num1, num2);
+        while (a > 0 && b > 0) {
+            ans += (a / b);
+            a %= b;
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+        return ans;
+    }
+}
