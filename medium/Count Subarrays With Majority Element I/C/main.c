2dad71ef12da89e6a57c7a9a315fe766437a9078
Rushabh <20rush09abh2001@gmail.com>
2025-11-09 13:38:58 +0530

    Added C solution to 'Count Subarrays With Majority Element I'

---
+++ medium/Count Subarrays With Majority Element I/C/main.c
@@ -0,0 +1,39 @@
+// Problem: Count Subarrays With Majority Element I
+// Link to the problem: https://leetcode.com/problems/count-subarrays-with-majority-element-i/
+int countMajoritySubarrays(int *nums, int numsSize, int target)
+{
+    int ans = 0, n = numsSize;
+    for (int k = 1; k <= n; k++)
+    {
+        int count = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (i >= k)
+            {
+                if (nums[i] == target)
+                {
+                    count++;
+                }
+                if (nums[i - k] == target)
+                {
+                    count--;
+                }
+            }
+            else
+            {
+                if (nums[i] == target)
+                {
+                    count++;
+                }
+            }
+            if (i >= k - 1)
+            {
+                if (2 * count > k)
+                {
+                    ans++;
+                }
+            }
+        }
+    }
+    return ans;
+}
