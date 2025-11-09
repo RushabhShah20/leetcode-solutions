6705a77d94392067fed720e2e6b7dd6b5268d2b8
Rushabh <20rush09abh2001@gmail.com>
2025-11-09 09:02:25 +0530

    Added C solution to 'Count Operations to Obtain Zero'

---
+++ easy/Count Operations to Obtain Zero/C/main.c
@@ -0,0 +1,15 @@
+// Problem: Count Operations to Obtain Zero
+// Link to the problem: https://leetcode.com/problems/count-operations-to-obtain-zero/
+int countOperations(int num1, int num2)
+{
+    int ans = 0, a = fmax(num1, num2), b = fmin(num1, num2);
+    while (a > 0 && b > 0)
+    {
+        ans += (a / b);
+        a %= b;
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+    return ans;
+}
