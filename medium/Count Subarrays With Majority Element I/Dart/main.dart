28e314a7c4aecb401922ad7b53b0b9893650b488
Rushabh <20rush09abh2001@gmail.com>
2025-11-09 13:40:12 +0530

    Added Dart solution to 'Count Subarrays With Majority Element I'

---
+++ medium/Count Subarrays With Majority Element I/Dart/main.dart
@@ -0,0 +1,30 @@
+// Problem: Count Subarrays With Majority Element I
+// Link to the problem: https://leetcode.com/problems/count-subarrays-with-majority-element-i/
+class Solution {
+  int countMajoritySubarrays(List<int> nums, int target) {
+    int ans = 0, n = nums.length;
+    for (int k = 1; k <= n; k++) {
+      int count = 0;
+      for (int i = 0; i < n; i++) {
+        if (i >= k) {
+          if (nums[i] == target) {
+            count++;
+          }
+          if (nums[i - k] == target) {
+            count--;
+          }
+        } else {
+          if (nums[i] == target) {
+            count++;
+          }
+        }
+        if (i >= k - 1) {
+          if (2 * count > k) {
+            ans++;
+          }
+        }
+      }
+    }
+    return ans;
+  }
+}
