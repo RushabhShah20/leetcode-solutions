9b9c3acd9e3420490d15015f0420627febe80e54
Rushabh <20rush09abh2001@gmail.com>
2025-11-10 21:00:33 +0530

    Added C++ solution to 'Daily Temperatures'

---
+++ medium/Daily Temperatures/C++/main.cpp
@@ -0,0 +1,23 @@
+// Problem: Daily Temperatures
+// Link to the problem: https://leetcode.com/problems/daily-temperatures/
+class Solution
+{
+public:
+    vector<int> dailyTemperatures(vector<int> &temperatures)
+    {
+        int n = temperatures.size();
+        vector<int> ans(n, 0);
+        stack<int> s;
+        for (int i = 0; i < n; i++)
+        {
+            while (!s.empty() && temperatures[s.top()] < temperatures[i])
+            {
+                int j = s.top();
+                s.pop();
+                ans[j] = i - j;
+            }
+            s.push(i);
+        }
+        return ans;
+    }
+};
