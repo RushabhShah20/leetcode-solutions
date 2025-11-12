65d89c11693c30d21a4a36918231fcfe9174e94f
Rushabh <20rush09abh2001@gmail.com>
2025-11-12 11:29:00 +0530

    Added C++ solution to 'Calculate Delayed Arrival Time'

---
+++ easy/Calculate Delayed Arrival Time/C++/main.cpp
@@ -0,0 +1,10 @@
+// Problem: Calculate Delayed Arrival Time
+// Link to the problem: https://leetcode.com/problems/calculate-delayed-arrival-time/
+class Solution
+{
+public:
+    int findDelayedArrivalTime(int arrivalTime, int delayedTime)
+    {
+        return (arrivalTime + delayedTime) % 24;
+    }
+};
