80c219801eee3a52ddd2026718f4fa42a522ba8e
Rushabh <20rush09abh2001@gmail.com>
2025-11-12 11:29:52 +0530

    Added C solution to 'Calculate Delayed Arrival Time'

---
+++ easy/Calculate Delayed Arrival Time/C/main.c
@@ -0,0 +1,6 @@
+// Problem: Calculate Delayed Arrival Time
+// Link to the problem: https://leetcode.com/problems/calculate-delayed-arrival-time/
+int findDelayedArrivalTime(int arrivalTime, int delayedTime)
+{
+    return (arrivalTime + delayedTime) % 24;
+}
