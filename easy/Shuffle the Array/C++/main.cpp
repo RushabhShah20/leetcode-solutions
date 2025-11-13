73d024f40e7d9de6ecd284f7c3258fbcafbc766c
Rushabh <20rush09abh2001@gmail.com>
2025-11-13 15:11:49 +0530

    Added C++ solution to 'Shuffle the Array'

---
+++ easy/Shuffle the Array/C++/main.cpp
@@ -0,0 +1,21 @@
+// Problem: Shuffle the Array
+// Link to the problem: https://leetcode.com/problems/shuffle-the-array/
+class Solution
+{
+public:
+    vector<int> shuffle(vector<int> &nums, int n)
+    {
+        for (int i = n; i < 2 * n; i++)
+        {
+            nums[i] = nums[i] << 10;
+            nums[i] = nums[i] | nums[i - n];
+        }
+        int m = n;
+        for (int i = 0; i < 2 * n; i = i + 2)
+        {
+            nums[i] = nums[m] & 1023;
+            nums[i + 1] = nums[m++] >> 10;
+        }
+        return nums;
+    }
+};
