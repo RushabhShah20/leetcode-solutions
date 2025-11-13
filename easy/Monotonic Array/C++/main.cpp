679764a68761e7e584c6f1b16fbd2a987d61cb1d
Rushabh <20rush09abh2001@gmail.com>
2025-11-13 16:41:03 +0530

    Added C++ solution to 'Monotonic Array'

---
+++ easy/Monotonic Array/C++/main.cpp
@@ -0,0 +1,27 @@
+// Problem: Monotonic Array
+// Link to the problem: https://leetcode.com/problems/monotonic-array/
+class Solution
+{
+public:
+    bool isMonotonic(vector<int> &nums)
+    {
+        int len = nums.size();
+        if (nums[len - 1] >= nums[0])
+        {
+            for (int i = 1; i < len; i++)
+            {
+                if (nums[i] < nums[i - 1])
+                    return false;
+            }
+        }
+        else
+        {
+            for (int i = 0; i < len - 1; i++)
+            {
+                if (nums[i] < nums[i + 1])
+                    return false;
+            }
+        }
+        return true;
+    }
+};
