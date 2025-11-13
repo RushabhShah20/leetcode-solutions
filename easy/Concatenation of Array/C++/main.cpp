634461d78b8e5f401c6a369dbf6dd0f68fcc0833
Rushabh <20rush09abh2001@gmail.com>
2025-11-13 15:08:33 +0530

    Added C++ solution to 'Concatenation of Array'

---
+++ easy/Concatenation of Array/C++/main.cpp
@@ -0,0 +1,11 @@
+// Problem: Concatenation of Array
+// Link to the problem: https://leetcode.com/problems/concatenation-of-array/
+class Solution
+{
+public:
+    vector<int> getConcatenation(vector<int> &nums)
+    {
+        nums.insert(nums.end(), nums.begin(), nums.end());
+        return nums;
+    }
+};
