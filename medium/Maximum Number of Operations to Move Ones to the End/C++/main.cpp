7cc2e4f83790f127ace72009d0ad29bce02706dd
Rushabh <20rush09abh2001@gmail.com>
2025-11-13 12:19:03 +0530

    Added C++ solution to 'Maximum Number of Operations to Move Ones to the End'

---
+++ medium/Maximum Number of Operations to Move Ones to the End/C++/main.cpp
@@ -0,0 +1,22 @@
+// Problem: Maximum Number of Operations to Move Ones to the End
+// Link to the problem: https://leetcode.com/problems/maximum-number-of-operations-to-move-ones-to-the-end/
+class Solution
+{
+public:
+    int maxOperations(string s)
+    {
+        int ans = 0, n = s.size(), ones = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (s[i] == '1')
+            {
+                ones++;
+            }
+            else if (i > 0 && s[i - 1] == '1')
+            {
+                ans += ones;
+            }
+        }
+        return ans;
+    }
+};
