b552d6b4de8d247c76ca23864cd3276527dca3dd
Rushabh <20rush09abh2001@gmail.com>
2025-11-13 16:42:31 +0530

    Added Dart solution to 'Monotonic Array'

---
+++ easy/Monotonic Array/Dart/main.dart
@@ -0,0 +1,17 @@
+// Problem: Monotonic Array
+// Link to the problem: https://leetcode.com/problems/monotonic-array/
+class Solution {
+  bool isMonotonic(List<int> nums) {
+    int len = nums.length;
+    if (nums[len - 1] >= nums[0]) {
+      for (int i = 1; i < len; i++) {
+        if (nums[i] < nums[i - 1]) return false;
+      }
+    } else {
+      for (int i = 0; i < len - 1; i++) {
+        if (nums[i] < nums[i + 1]) return false;
+      }
+    }
+    return true;
+  }
+}
