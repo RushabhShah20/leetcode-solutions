abb55291f71241a4c628887fc0611a9a59a6ec1b
Rushabh <20rush09abh2001@gmail.com>
2025-11-15 18:49:13 +0530

    Added Go solution to 'Lowest Common Ancestor of a Binary Search Tree'

---
+++ medium/Lowest Common Ancestor of a Binary Search Tree/Go/main.go
@@ -0,0 +1,23 @@
+// Problem: Lowest Common Ancestor of a Binary Search Tree
+// Link to the problem: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val   int
+ *     Left  *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil {
+		return root
+	}
+	if root.Val > p.Val && root.Val > q.Val {
+		return lowestCommonAncestor(root.Left, p, q)
+	} else if root.Val < p.Val && root.Val < q.Val {
+		return lowestCommonAncestor(root.Right, p, q)
+	} else {
+		return root
+	}
+}
