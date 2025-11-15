e8d2b1d1a0d51bfa0e22f37d91426a99fda357d2
Rushabh <20rush09abh2001@gmail.com>
2025-11-15 15:41:12 +0530

    Added Dart solution to 'Count Integers With Even Digit Sum'

---
+++ easy/Count Integers With Even Digit Sum/Dart/main.dart
@@ -0,0 +1,19 @@
+// Problem: Count Integers With Even Digit Sum
+// Link to the problem: https://leetcode.com/problems/count-integers-with-even-digit-sum/
+class Solution {
+  int countEven(int num) {
+    int count = 0;
+    for (int i = 1; i <= num; i++) {
+      int sum = 0;
+      int n = i;
+      while (n != 0) {
+        sum += n % 10;
+        n ~/= 10;
+      }
+      if (sum % 2 == 0) {
+        count++;
+      }
+    }
+    return count;
+  }
+}
