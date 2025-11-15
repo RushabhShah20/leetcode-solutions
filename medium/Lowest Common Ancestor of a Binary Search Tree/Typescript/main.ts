0cd50a15b663f5c9d2cd934a3159ce269b25176c
Rushabh <20rush09abh2001@gmail.com>
2025-11-15 18:49:01 +0530

    Added Typescript solution to 'Lowest Common Ancestor of a Binary Search Tree'

---
+++ medium/Lowest Common Ancestor of a Binary Search Tree/Typescript/main.ts
@@ -0,0 +1,28 @@
+// Problem: Lowest Common Ancestor of a Binary Search Tree
+// Link to the problem: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function lowestCommonAncestor(root: TreeNode | null, p: TreeNode | null, q: TreeNode | null): TreeNode | null {
+    if (root === null) {
+        return root;
+    }
+    if (root.val > p.val && root.val > q.val) {
+        return lowestCommonAncestor(root.left, p, q);
+    } else if (root.val < p.val && root.val < q.val) {
+        return lowestCommonAncestor(root.right, p, q);
+    } else {
+        return root;
+    }
+};
