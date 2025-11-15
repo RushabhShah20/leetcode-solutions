0202bc1968cc4a8c159660ce1dd4cc37368e7836
Rushabh <20rush09abh2001@gmail.com>
2025-11-15 18:29:27 +0530

    Added C solution to 'Lowest Common Ancestor of a Binary Tree'

---
+++ medium/Lowest Common Ancestor of a Binary Tree/C/main.c
@@ -0,0 +1,31 @@
+// Problem: Lowest Common Ancestor of a Binary Tree
+// Link to the problem: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+struct TreeNode *lowestCommonAncestor(struct TreeNode *root, struct TreeNode *p, struct TreeNode *q)
+{
+    if (root == NULL || root == p || root == q)
+    {
+        return root;
+    }
+    struct TreeNode *left = lowestCommonAncestor(root->left, p, q);
+    struct TreeNode *right = lowestCommonAncestor(root->right, p, q);
+    if (left == NULL)
+    {
+        return right;
+    }
+    else if (right == NULL)
+    {
+        return left;
+    }
+    else
+    {
+        return root;
+    }
+}
