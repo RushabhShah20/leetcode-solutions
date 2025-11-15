954d16b3ed36d2b141735003531b2bbbe86390bd
Rushabh <20rush09abh2001@gmail.com>
2025-11-15 15:08:26 +0530

    Added Java solution to 'Find if Digit Game Can Be Won'

---
+++ easy/Find if Digit Game Can Be Won/Java/main.java
@@ -0,0 +1,16 @@
+// Problem: Find if Digit Game Can Be Won
+// Link to the problem: https://leetcode.com/problems/find-if-digit-game-can-be-won/
+class Solution {
+    public boolean canAliceWin(int[] nums) {
+        int x = 0;
+        int y = 0;
+        for (int num : nums) {
+            if (num / 10 == 0) {
+                x += num;
+            } else {
+                y += num;
+            }
+        }
+        return x != y;
+    }
+}
