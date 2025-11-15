4d28ad278b491c178670713ba380967a23caf4e6
Rushabh <20rush09abh2001@gmail.com>
2025-11-15 16:02:19 +0530

    Added C# solution to 'Find Numbers with Even Number of Digits'

---
+++ easy/Find Numbers with Even Number of Digits/C#/main.cs
@@ -0,0 +1,24 @@
+// Problem: Find Numbers with Even Number of Digits
+// Link to the problem: https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
+public class Solution
+{
+    public int FindNumbers(int[] nums)
+    {
+        int ans = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int n = nums[i];
+            int count = 0;
+            while (n != 0)
+            {
+                count++;
+                n /= 10;
+            }
+            if (count % 2 == 0)
+            {
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
