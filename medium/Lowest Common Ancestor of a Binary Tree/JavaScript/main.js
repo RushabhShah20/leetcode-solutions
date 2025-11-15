3f9a50ce1ae90afa8468f431c6d19e85a660af47
Rushabh <20rush09abh2001@gmail.com>
2025-11-15 18:30:16 +0530

    Added JavaScript solution to 'Lowest Common Ancestor of a Binary Tree'

---
+++ medium/Lowest Common Ancestor of a Binary Tree/JavaScript/main.js
@@ -0,0 +1,29 @@
+// Problem: Lowest Common Ancestor of a Binary Tree
+// Link to the problem: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function (root, p, q) {
+    if (root === null || root === p || root === q) {
+        return root;
+    }
+    let left = lowestCommonAncestor(root.left, p, q);
+    let right = lowestCommonAncestor(root.right, p, q);
+    if (left === null) {
+        return right;
+    } else if (right === null) {
+        return left;
+    } else {
+        return root;
+    }
+};
