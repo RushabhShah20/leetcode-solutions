fba637312c2bf565425f04290e514ee89a1b9947
Rushabh <20rush09abh2001@gmail.com>
2025-11-17 11:41:36 +0530

    Added JavaScript solution to 'Check If All 1's Are at Least Length K Places Away'

---
+++ easy/Check If All 1's Are at Least Length K Places Away/JavaScript/main.js
@@ -0,0 +1,30 @@
+// Problem: Check If All 1's Are at Least Length K Places Away
+// Link to the problem: https://leetcode.com/problems/check-if-all-1s-are-at-least-length-k-places-away/
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var kLengthApart = function (nums, k) {
+    let pos = new Array();
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === 1) {
+            pos.push(i);
+        }
+    }
+    let minDiff = 1000000000;
+    if (pos.length === 0) {
+        return true;
+    }
+    else {
+        for (let i = 1; i < pos.length; i++) {
+            minDiff = Math.min(minDiff, Math.abs(pos[i] - pos[i - 1] - 1));
+        }
+        if (minDiff < k) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+};
