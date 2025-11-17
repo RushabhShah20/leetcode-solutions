9d4ef79ed3dc4fe175d7bc72b8a453bb95fd4a8b
Rushabh <20rush09abh2001@gmail.com>
2025-11-17 14:53:26 +0530

    Added Dart solution to 'Rotate Image'

---
+++ medium/Rotate Image/Dart/main.dart
@@ -0,0 +1,21 @@
+// Problem: Rotate Image
+// Link to the problem: https://leetcode.com/problems/rotate-image/
+class Solution {
+  void rotate(List<List<int>> matrix) {
+    int n = matrix.length;
+    for (int i = 0; i < n; i++) {
+      for (int j = i + 1; j < n; j++) {
+        int temp = matrix[i][j];
+        matrix[i][j] = matrix[j][i];
+        matrix[j][i] = temp;
+      }
+    }
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n ~/ 2; j++) {
+        int temp = matrix[i][j];
+        matrix[i][j] = matrix[i][n - 1 - j];
+        matrix[i][n - 1 - j] = temp;
+      }
+    }
+  }
+}
