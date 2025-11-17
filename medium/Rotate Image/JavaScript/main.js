6e1c0d4dc8b197038c0357bf2c38332cd8c48ffa
Rushabh <20rush09abh2001@gmail.com>
2025-11-17 14:52:32 +0530

    Added JavaScript solution to 'Rotate Image'

---
+++ medium/Rotate Image/JavaScript/main.js
@@ -0,0 +1,23 @@
+// Problem: Rotate Image
+// Link to the problem: https://leetcode.com/problems/rotate-image/
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function (matrix) {
+    let n = matrix.length;
+    for (let i = 0; i < n; i++) {
+        for (let j = i + 1; j < n; j++) {
+            let temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
+        }
+    }
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < Math.floor(n / 2); j++) {
+            let temp = matrix[i][j];
+            matrix[i][j] = matrix[i][n - 1 - j];
+            matrix[i][n - 1 - j] = temp;
+        }
+    }
+};
