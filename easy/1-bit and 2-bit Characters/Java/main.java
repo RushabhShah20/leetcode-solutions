681a6b12495d6b921db308bd624a563ecc0f654b
Rushabh <20rush09abh2001@gmail.com>
2025-11-18 09:42:39 +0530

    Added Java solution to '1-bit and 2-bit Characters'

---
+++ easy/1-bit and 2-bit Characters/Java/main.java
@@ -0,0 +1,12 @@
+// Problem: 1-bit and 2-bit Characters
+// Link to the problem: https://leetcode.com/problems/1-bit-and-2-bit-characters/
+class Solution {
+    public boolean isOneBitCharacter(int[] bits) {
+        int n = bits.length;
+        int i = 0;
+        while (i < n - 1) {
+            i += bits[i] + 1;
+        }
+        return i == n - 1;
+    }
+}
