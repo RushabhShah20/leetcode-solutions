bbfd217c903df5f4efb881e77aa1f85cfac2622c
Rushabh <20rush09abh2001@gmail.com>
2025-11-18 09:43:41 +0530

    Added JavaScript solution to '1-bit and 2-bit Characters'

---
+++ easy/1-bit and 2-bit Characters/JavaScript/main.js
@@ -0,0 +1,14 @@
+// Problem: 1-bit and 2-bit Characters
+// Link to the problem: https://leetcode.com/problems/1-bit-and-2-bit-characters/
+/**
+ * @param {number[]} bits
+ * @return {boolean}
+ */
+var isOneBitCharacter = function (bits) {
+    let n = bits.length;
+    let i = 0;
+    while (i < n - 1) {
+        i += bits[i] + 1;
+    }
+    return i === n - 1;
+};
