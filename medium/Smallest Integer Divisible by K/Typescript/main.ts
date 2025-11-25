7a0005f96abf8c7b6d7314c454dd29034aa2c641
Rushabh <20rush09abh2001@gmail.com>
2025-11-25 11:36:39 +0530

    Added Typescript solution to 'Smallest Integer Divisible by K'

---
+++ medium/Smallest Integer Divisible by K/Typescript/main.ts
@@ -0,0 +1,19 @@
+// Problem: Smallest Integer Divisible by K
+// Link to the problem: https://leetcode.com/problems/smallest-integer-divisible-by-k/
+function smallestRepunitDivByK(k: number): number {
+    if (k % 2 === 0 || k % 5 === 0) {
+        return -1;
+    }
+    if (k === 1) {
+        return 1;
+    }
+    let rem: number = 1, ans: number = 1;
+    while (rem > 0) {
+        rem = ((rem * 10) + 1) % k;
+        ans++;
+        if (ans > k) {
+            return -1;
+        }
+    }
+    return ans;
+};
