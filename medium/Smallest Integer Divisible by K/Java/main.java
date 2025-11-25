c912b4e10d924382c4482d1915b5139c7955dc21
Rushabh <20rush09abh2001@gmail.com>
2025-11-25 11:34:33 +0530

    Added Java solution to 'Smallest Integer Divisible by K'

---
+++ medium/Smallest Integer Divisible by K/Java/main.java
@@ -0,0 +1,21 @@
+// Problem: Smallest Integer Divisible by K
+// Link to the problem: https://leetcode.com/problems/smallest-integer-divisible-by-k/
+class Solution {
+    public int smallestRepunitDivByK(int k) {
+        if (k % 2 == 0 || k % 5 == 0) {
+            return -1;
+        }
+        if (k == 1) {
+            return 1;
+        }
+        int rem = 1, ans = 1;
+        while (rem > 0) {
+            rem = ((rem * 10) + 1) % k;
+            ans++;
+            if (ans > k) {
+                return -1;
+            }
+        }
+        return ans;
+    }
+}
