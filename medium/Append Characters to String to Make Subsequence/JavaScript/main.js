e90ad8f2b47a2d4e345c6321005b1ffa04ee2d3c
Rushabh <20rush09abh2001@gmail.com>
2025-11-26 16:46:23 +0530

    Added JavaScript solution to 'Append Characters to String to Make Subsequence'

---
+++ medium/Append Characters to String to Make Subsequence/JavaScript/main.js
@@ -0,0 +1,18 @@
+// Problem: Append Characters to String to Make Subsequence
+// Link to the problem: https://leetcode.com/problems/append-characters-to-string-to-make-subsequence/
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {number}
+ */
+var appendCharacters = function (s, t) {
+    let ans = 0, i = 0, j = 0, n = s.length, m = t.length;
+    while (i < n && j < m) {
+        if (s[i] === t[j]) {
+            j++;
+        }
+        i++;
+    }
+    ans = m - j;
+    return ans;
+};
