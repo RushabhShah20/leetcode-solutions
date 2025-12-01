449dbe370d90f3e43c27abee4f0302a4d5c93609
Rushabh <20rush09abh2001@gmail.com>
2025-12-01 16:05:59 +0530

    Added Dart solution to 'Maximum Height of a Triangle'

---
+++ easy/Maximum Height of a Triangle/Dart/main.dart
@@ -0,0 +1,34 @@
+// Problem: Maximum Height of a Triangle
+// Link to the problem: https://leetcode.com/problems/maximum-height-of-a-triangle/
+class Solution {
+  int maxHeightOfTriangle(int red, int blue) {
+    int x = 0, y = 0;
+    int a = red, b = blue;
+    int i = 1;
+    while (a >= 0 && b >= 0) {
+      if (i % 2 == 0) {
+        a -= i;
+        x++;
+      } else {
+        b -= i;
+        x++;
+      }
+      i++;
+    }
+    a = red;
+    b = blue;
+    i = 1;
+    while (a >= 0 && b >= 0) {
+      if (i % 2 == 0) {
+        b -= i;
+        y++;
+      } else {
+        a -= i;
+        y++;
+      }
+      i++;
+    }
+    final int ans = max(x - 1, y - 1);
+    return ans;
+  }
+}
