bc04a8039472ece72dcee17258de7cbf48d5ca3b
Rushabh <20rush09abh2001@gmail.com>
2025-12-01 17:22:07 +0530

    Added Rust solution to 'Range Addition II'

---
+++ easy/Range Addition II/Rust/main.rs
@@ -0,0 +1,12 @@
+// Problem: Range Addition II
+// Link to the problem: https://leetcode.com/problems/range-addition-ii/
+impl Solution {
+    pub fn max_count(mut m: i32, mut n: i32, ops: Vec<Vec<i32>>) -> i32 {
+        for op in ops {
+            m = m.min(op[0]);
+            n = n.min(op[1]);
+        }
+        let ans: i32 = m * n;
+        return ans;
+    }
+}
