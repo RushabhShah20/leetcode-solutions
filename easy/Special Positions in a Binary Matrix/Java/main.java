b30ac2dd4110d634b26e5ce97b12a5221190eb3e
Rushabh <20rush09abh2001@gmail.com>
2025-12-01 16:43:45 +0530

    Added Java solution to 'Special Positions in a Binary Matrix'

---
+++ easy/Special Positions in a Binary Matrix/Java/main.java
@@ -0,0 +1,24 @@
+// Problem: Special Positions in a Binary Matrix
+// Link to the problem: https://leetcode.com/problems/special-positions-in-a-binary-matrix/
+class Solution {
+    public int numSpecial(int[][] mat) {
+        int m = mat.length, n = mat[0].length, ans = 0;
+        int[] rows = new int[m], cols = new int[n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 1) {
+                    rows[i]++;
+                    cols[j]++;
+                }
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (rows[i] == 1 && cols[j] == 1 && mat[i][j] == 1) {
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+}
