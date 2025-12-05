5af7655dc4662eea7cb4e7fa6377d84c00409888
Rushabh <20rush09abh2001@gmail.com>
2025-12-05 12:59:12 +0530

    Added Java solution to 'Count Partitions with Even Sum Difference'

--- easy/Count Partitions with Even Sum Difference/Java/main.java
+++ easy/Count Partitions with Even Sum Difference/Java/main.java
@@ -1,22 +1,15 @@
 // Problem: Count Partitions with Even Sum Difference
 // Link to the problem: https://leetcode.com/problems/count-partitions-with-even-sum-difference/
-class Solution
-{
-public:
-    int countPartitions(vector<int> &nums)
-    {
-        int sum = 0, ans = nums.size() - 1;
-        for (const int num : nums)
-        {
+class Solution {
+    public int countPartitions(int[] nums) {
+        int sum = 0, ans = nums.length - 1;
+        for (final int num : nums) {
             sum += num;
         }
-        if (sum % 2 == 0)
-        {
+        if (sum % 2 == 0) {
             return ans;
-        }
-        else
-        {
+        } else {
             return 0;
         }
     }
-};
+}
