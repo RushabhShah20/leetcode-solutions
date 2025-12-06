9f361db4e629ce5530a14c8499da17e6696ad9d2
Rushabh <20rush09abh2001@gmail.com>
2025-12-06 16:34:25 +0530

    Added JavaScript solution to 'Sort By'

---
+++ easy/Sort By/JavaScript/main.js
@@ -0,0 +1,10 @@
+// Problem: Sort By
+// Link to the problem: https://leetcode.com/problems/sort-by/
+/**
+ * @param {Array} arr
+ * @param {Function} fn
+ * @return {Array}
+ */
+var sortBy = function (arr, fn) {
+    return arr.sort((a, b) => fn(a) - fn(b));
+};
