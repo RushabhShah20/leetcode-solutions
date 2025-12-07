7cc88e64c53a4d5feb57536de2a0d1a8cc72b298
Rushabh <20rush09abh2001@gmail.com>
2025-12-07 14:55:54 +0530

    Added C solution to 'Count Odd Numbers in an Interval Range'

---
+++ easy/Count Odd Numbers in an Interval Range/C/main.c
@@ -0,0 +1,15 @@
+// Problem: Count Odd Numbers in an Interval Range
+// Link to the problem: https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/
+int countOdds(int low, int high)
+{
+    int ans;
+    if (low % 2 == 0)
+    {
+        ans = (high - low + 1) / 2;
+    }
+    else
+    {
+        ans = (high - low + 2) / 2;
+    }
+    return ans;
+}
