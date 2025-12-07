61c7188ab132167a117cc020ae5d338d7efe0ce3
Rushabh <20rush09abh2001@gmail.com>
2025-12-07 14:55:15 +0530

    Added Java solution to 'Count Odd Numbers in an Interval Range'

---
+++ easy/Count Odd Numbers in an Interval Range/Java/main.java
@@ -0,0 +1,13 @@
+// Problem: Count Odd Numbers in an Interval Range
+// Link to the problem: https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/
+class Solution {
+    public int countOdds(int low, int high) {
+        int ans;
+        if (low % 2 == 0) {
+            ans = (high - low + 1) / 2;
+        } else {
+            ans = (high - low + 2) / 2;
+        }
+        return ans;
+    }
+}
