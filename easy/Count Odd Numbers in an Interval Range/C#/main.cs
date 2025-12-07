daf8b9e14e14a7f2365aa7e7ab69093082f08413
Rushabh <20rush09abh2001@gmail.com>
2025-12-07 14:55:32 +0530

    Added C# solution to 'Count Odd Numbers in an Interval Range'

---
+++ easy/Count Odd Numbers in an Interval Range/C#/main.cs
@@ -0,0 +1,18 @@
+// Problem: Count Odd Numbers in an Interval Range
+// Link to the problem: https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/
+public class Solution
+{
+    public int CountOdds(int low, int high)
+    {
+        int ans;
+        if (low % 2 == 0)
+        {
+            ans = (high - low + 1) / 2;
+        }
+        else
+        {
+            ans = (high - low + 2) / 2;
+        }
+        return ans;
+    }
+}
