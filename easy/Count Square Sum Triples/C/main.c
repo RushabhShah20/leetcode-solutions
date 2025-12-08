fc930c0a00f1b3be28d755dc11156f6865b46dfb
Rushabh <20rush09abh2001@gmail.com>
2025-12-08 12:49:48 +0530

    Added C solution to 'Count Square Sum Triples'

---
+++ easy/Count Square Sum Triples/C/main.c
@@ -0,0 +1,31 @@
+// Problem: Count Square Sum Triples
+// Link to the problem: https://leetcode.com/problems/count-square-sum-triples/
+int countTriples(int n)
+{
+    int ans = 0;
+    for (int i = 1; i <= n; ++i)
+    {
+        int x = i * i;
+        int l = 1;
+        int r = i - 1;
+        while (l < r)
+        {
+            int sum = l * l + r * r;
+            if (sum == x)
+            {
+                ans += 2;
+                l++;
+                r--;
+            }
+            else if (sum < x)
+            {
+                l++;
+            }
+            else
+            {
+                r--;
+            }
+        }
+    }
+    return ans;
+}
