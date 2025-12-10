05be21585cc770924ba2fca4f73e6131bcd4a2a0
Rushabh <20rush09abh2001@gmail.com>
2025-12-10 15:50:57 +0530

    Added C++ solution to 'Count the Number of Computer Unlocking Permutations'

---
+++ medium/Count the Number of Computer Unlocking Permutations/C++/main.cpp
@@ -0,0 +1,23 @@
+// Problem: Count the Number of Computer Unlocking Permutations
+// Link to the problem: https://leetcode.com/problems/count-the-number-of-computer-unlocking-permutations/
+class Solution
+{
+public:
+    int countPermutations(vector<int> &complexity)
+    {
+        int n = complexity.size();
+        long long ans = 1, mod = 1000000007;
+        for (int i = 1; i < n; i++)
+        {
+            if (complexity[i] <= complexity[0])
+            {
+                return 0;
+            }
+            else
+            {
+                ans = ((ans * i) % mod);
+            }
+        }
+        return ans;
+    }
+};
