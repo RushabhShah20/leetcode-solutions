1f7603c8709738b42f2106a12d294f84455e311d
Rushabh <20rush09abh2001@gmail.com>
2025-12-10 15:51:13 +0530

    Added Java solution to 'Count the Number of Computer Unlocking Permutations'

---
+++ medium/Count the Number of Computer Unlocking Permutations/Java/main.java
@@ -0,0 +1,16 @@
+// Problem: Count the Number of Computer Unlocking Permutations
+// Link to the problem: https://leetcode.com/problems/count-the-number-of-computer-unlocking-permutations/
+class Solution {
+    public int countPermutations(int[] complexity) {
+        int n = complexity.length;
+        long ans = 1, mod = 1000000007;
+        for (int i = 1; i < n; i++) {
+            if (complexity[i] <= complexity[0]) {
+                return 0;
+            } else {
+                ans = ((ans * i) % mod);
+            }
+        }
+        return (int) ans;
+    }
+}
