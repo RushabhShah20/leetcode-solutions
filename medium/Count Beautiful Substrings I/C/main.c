78e2f8575168fdcbe841d9a1c41acc6e7921c265
Rushabh <20rush09abh2001@gmail.com>
2025-12-13 17:26:18 +0530

    Added C solution to 'Count Beautiful Substrings I'

---
+++ medium/Count Beautiful Substrings I/C/main.c
@@ -0,0 +1,26 @@
+// Problem: Count Beautiful Substrings I
+// Link to the problem: https://leetcode.com/problems/count-beautiful-substrings-i/
+int beautifulSubstrings(char *s, int k)
+{
+    int ans = 0, n = strlen(s);
+    for (int i = 0; i < n; i++)
+    {
+        int vowels = 0, consonents = 0;
+        for (int j = i; j < n; j++)
+        {
+            if (s[j] == 'a' || s[j] == 'e' || s[j] == 'i' || s[j] == 'o' || s[j] == 'u')
+            {
+                vowels++;
+            }
+            else
+            {
+                consonents++;
+            }
+            if ((vowels == consonents) && (vowels * consonents) % k == 0)
+            {
+                ans++;
+            }
+        }
+    }
+    return ans;
+}
