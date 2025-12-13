af3f7b6bac981ee3aeaaaf163f1000d90d72426c
Rushabh <20rush09abh2001@gmail.com>
2025-12-13 17:26:06 +0530

    Added C# solution to 'Count Beautiful Substrings I'

---
+++ medium/Count Beautiful Substrings I/C#/main.cs
@@ -0,0 +1,29 @@
+// Problem: Count Beautiful Substrings I
+// Link to the problem: https://leetcode.com/problems/count-beautiful-substrings-i/
+public class Solution
+{
+    public int BeautifulSubstrings(string s, int k)
+    {
+        int ans = 0, n = s.Length;
+        for (int i = 0; i < n; i++)
+        {
+            int vowels = 0, consonents = 0;
+            for (int j = i; j < n; j++)
+            {
+                if (s[j] == 'a' || s[j] == 'e' || s[j] == 'i' || s[j] == 'o' || s[j] == 'u')
+                {
+                    vowels++;
+                }
+                else
+                {
+                    consonents++;
+                }
+                if ((vowels == consonents) && (vowels * consonents) % k == 0)
+                {
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+}
