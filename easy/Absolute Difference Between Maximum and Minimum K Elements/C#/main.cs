4708605c993224c54bd6304bcab9d06d2d1a95cc
Rushabh <20rush09abh2001@gmail.com>
2025-12-14 14:20:52 +0530

    Added C# solution to 'Absolute Difference Between Maximum and Minimum K Elements'

---
+++ easy/Absolute Difference Between Maximum and Minimum K Elements/C#/main.cs
@@ -0,0 +1,15 @@
+// Problem: Absolute Difference Between Maximum and Minimum K Elements
+// Link to the problem: https://leetcode.com/problems/absolute-difference-between-maximum-and-minimum-k-elements/
+public class Solution
+{
+    public int AbsDifference(int[] nums, int k)
+    {
+        Array.Sort(nums);
+        int ans = 0, n = nums.Length;
+        for (int i = 0; i < k; i++)
+        {
+            ans += (-nums[i] + nums[n - i - 1]);
+        }
+        return ans;
+    }
+}
