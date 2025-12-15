86e536cee29858746c86734f57753cf831750f63
Rushabh <20rush09abh2001@gmail.com>
2025-12-15 18:27:23 +0530

    Added JavaScript solution to 'Trapping Rain Water'

---
+++ hard/Trapping Rain Water/JavaScript/main.js
@@ -0,0 +1,23 @@
+// Problem: Trapping Rain Water
+// Link to the problem: https://leetcode.com/problems/trapping-rain-water/
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function (height) {
+    let i = 0, left_max = height[0], ans = 0;
+    let j = height.length - 1, right_max = height[j];
+    while (i < j) {
+        if (left_max <= right_max) {
+            ans += (left_max - height[i]);
+            i++;
+            left_max = Math.max(left_max, height[i]);
+        }
+        else {
+            ans += (right_max - height[j]);
+            j--;
+            right_max = Math.max(right_max, height[j]);
+        }
+    }
+    return ans;
+};
