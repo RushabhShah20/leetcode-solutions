596ec77c9db3e749acfeaa314dc6a00a14624aaf
Rushabh <20rush09abh2001@gmail.com>
2025-12-18 12:25:00 +0530

    Added C++ solution to 'Online Stock Span'

---
+++ medium/Online Stock Span/C++/main.cpp
@@ -0,0 +1,28 @@
+// Problem: Online Stock Span
+// Link to the problem: https://leetcode.com/problems/online-stock-span/
+class StockSpanner
+{
+public:
+    stack<pair<int, int>> st;
+    StockSpanner()
+    {
+    }
+
+    int next(int price)
+    {
+        int ans = 1;
+        while (!st.empty() && st.top().first <= price)
+        {
+            ans += st.top().second;
+            st.pop();
+        }
+        st.push({price, ans});
+        return ans;
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
