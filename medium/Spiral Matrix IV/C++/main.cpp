954650a9dbd0382642a1aa43b024545b9643b8b1
Rushabh <20rush09abh2001@gmail.com>
2025-12-18 18:39:06 +0530

    Added C++ solution to 'Spiral Matrix IV'

---
+++ medium/Spiral Matrix IV/C++/main.cpp
@@ -0,0 +1,55 @@
+// Problem: Spiral Matrix IV
+// Link to the problem: https://leetcode.com/problems/spiral-matrix-iv/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    vector<vector<int>> spiralMatrix(int m, int n, ListNode *head)
+    {
+        vector<vector<int>> ans(m, vector<int>(n, -1));
+        int top = 0, bottom = m - 1, left = 0, right = n - 1;
+        while (head != NULL)
+        {
+            for (int i = left; i <= right && head != NULL; i++)
+            {
+                ans[top][i] = head->val;
+                head = head->next;
+            }
+            top++;
+            for (int i = top; i <= bottom && head != NULL; i++)
+            {
+                ans[i][right] = head->val;
+                head = head->next;
+            }
+            right--;
+            if (top <= bottom)
+            {
+                for (int i = right; i >= left && head != NULL; i--)
+                {
+                    ans[bottom][i] = head->val;
+                    head = head->next;
+                }
+                bottom--;
+            }
+            if (left <= right)
+            {
+                for (int i = bottom; i >= top && head != NULL; i--)
+                {
+                    ans[i][left] = head->val;
+                    head = head->next;
+                }
+                left++;
+            }
+        }
+        return ans;
+    }
+};
