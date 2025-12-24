f6390c0d0d9187d24f390129993a1f1a41ce5353
Rushabh <20rush09abh2001@gmail.com>
2025-12-24 17:48:41 +0530

    Added Java solution to 'Max Chunks To Make Sorted'

---
+++ medium/Max Chunks To Make Sorted/Java/main.java
@@ -0,0 +1,16 @@
+// Problem: Max Chunks To Make Sorted
+// Link to the problem: https://leetcode.com/problems/max-chunks-to-make-sorted/
+class Solution {
+    public int maxChunksToSorted(int[] arr) {
+        int ans = 0, n = arr.length, max = 0;
+        for (int i = 0; i < n; i++) {
+            if (max < arr[i]) {
+                max = arr[i];
+            }
+            if (max == i) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
