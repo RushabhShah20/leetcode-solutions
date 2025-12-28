db286ea0ca6e79e5969b719b7104db62639730c2
Rushabh <20rush09abh2001@gmail.com>
2025-12-28 15:37:47 +0530

    Added Go solution to 'Smallest All-Ones Multiple'

---
+++ medium/Smallest All-Ones Multiple/Go/main.go
@@ -0,0 +1,20 @@
+// Problem: Smallest All-Ones Multiple
+// Link to the problem: https://leetcode.com/problems/smallest-all-ones-multiple/
+func minAllOneMultiple(k int) int {
+	if k%2 == 0 || k%5 == 0 {
+		return -1
+	}
+	if k == 1 {
+		return 1
+	}
+	var rem int = 1
+	var ans int = 1
+	for rem > 0 {
+		rem = ((rem * 10) + 1) % k
+		ans++
+		if ans > k {
+			return -1
+		}
+	}
+	return ans
+}
