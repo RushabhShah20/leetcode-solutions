0a2aa8dfd65bf79c56cfadf6f6bfdc43a5f230c9
Rushabh <20rush09abh2001@gmail.com>
2025-12-28 15:35:07 +0530

    Added C# solution to 'Smallest All-Ones Multiple'

---
+++ medium/Smallest All-Ones Multiple/C#/main.cs
@@ -0,0 +1,27 @@
+// Problem: Smallest All-Ones Multiple
+// Link to the problem: https://leetcode.com/problems/smallest-all-ones-multiple/
+public class Solution
+{
+    public int MinAllOneMultiple(int k)
+    {
+        if (k % 2 == 0 || k % 5 == 0)
+        {
+            return -1;
+        }
+        if (k == 1)
+        {
+            return 1;
+        }
+        int rem = 1, ans = 1;
+        while (rem > 0)
+        {
+            rem = ((rem * 10) + 1) % k;
+            ans++;
+            if (ans > k)
+            {
+                return -1;
+            }
+        }
+        return ans;
+    }
+}
