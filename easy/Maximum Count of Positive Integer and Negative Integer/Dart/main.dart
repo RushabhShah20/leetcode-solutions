ba8c76e29cf738bf2610bcdf24a29ff3c4473628
Rushabh <20rush09abh2001@gmail.com>
2025-12-28 21:54:04 +0530

    Added Dart solution to 'Maximum Count of Positive Integer and Negative Integer'

---
+++ easy/Maximum Count of Positive Integer and Negative Integer/Dart/main.dart
@@ -0,0 +1,31 @@
+// Problem: Maximum Count of Positive Integer and Negative Integer
+// Link to the problem: https://leetcode.com/problems/maximum-count-of-positive-integer-and-negative-integer/
+class Solution {
+  int maximumCount(List<int> nums) {
+    int n = nums.length, start = 0, end = n - 1, maxNeg = -1, minPos = n;
+    while (start <= end) {
+      int mid = start + (end - start) ~/ 2;
+      if (nums[mid] > 0) {
+        minPos = mid;
+        end = mid - 1;
+      } else {
+        start = mid + 1;
+      }
+    }
+    start = 0;
+    end = n - 1;
+    while (start <= end) {
+      int mid = start + (end - start) ~/ 2;
+      if (nums[mid] < 0) {
+        maxNeg = mid;
+        start = mid + 1;
+      } else {
+        end = mid - 1;
+      }
+    }
+    maxNeg += 1;
+    minPos = n - minPos;
+    final int ans = max(maxNeg, minPos);
+    return ans;
+  }
+}
