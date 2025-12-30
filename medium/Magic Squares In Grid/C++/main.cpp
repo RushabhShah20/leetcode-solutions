b415c3e10459814ff6ce98de52dfffd5d6b8750a
Rushabh <20rush09abh2001@gmail.com>
2025-12-30 11:34:27 +0530

    Added Java solution to 'Magic Squares In Grid'

--- medium/Magic Squares In Grid/C++/main.cpp
+++ medium/Magic Squares In Grid/C++/main.cpp
@@ -1,50 +1,68 @@
 // Problem: Magic Squares In Grid
 // Link to the problem: https://leetcode.com/problems/magic-squares-in-grid/
+#include <bits/stdc++.h>
+using namespace std;
 class Solution
 {
 public:
-    bool isMagicSquare(vector<vector<int>> &mat)
+    bool isMagicSquare(vector<vector<int>> &mat, int r, int c)
     {
-        int row1sum = mat[0][0] + mat[0][1] + mat[0][2];
-        int row2sum = mat[1][0] + mat[1][1] + mat[1][2];
-        int row3sum = mat[2][0] + mat[2][1] + mat[2][2];
-        int col1sum = mat[0][0] + mat[1][0] + mat[2][0];
-        int col2sum = mat[0][1] + mat[1][1] + mat[2][1];
-        int col3sum = mat[0][2] + mat[1][2] + mat[2][2];
-        int diag1sum = mat[0][0] + mat[1][1] + mat[2][2];
-        int diag2sum = mat[2][0] + mat[1][1] + mat[0][2];
-        vector<int> elements = {mat[0][0], mat[0][1], mat[0][2], mat[1][0], mat[1][1], mat[1][2], mat[2][0], mat[2][1], mat[2][2]};
-        set<int> s(elements.begin(), elements.end());
-        if (s.size() != elements.size())
+        vector<int> seen(10, 0);
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                const int v = mat[r + i][c + j];
+                if (v < 1 || v > 9 || seen[v])
+                {
+                    return false;
+                }
+                seen[v] = 1;
+            }
+        }
+        if (mat[r][c] + mat[r][c + 1] + mat[r][c + 2] != 15)
         {
             return false;
         }
-        else if (s.find(10) != s.end() || s.find(11) != s.end() || s.find(12) != s.end() || s.find(13) != s.end() || s.find(14) != s.end() || s.find(15) != s.end() | s.find(0) != s.end())
+        if (mat[r + 1][c] + mat[r + 1][c + 1] + mat[r + 1][c + 2] != 15)
         {
             return false;
         }
-        else if (s.find(1) == s.end() || s.find(2) == s.end() || s.find(3) == s.end() || s.find(4) == s.end() || s.find(5) == s.end() || s.find(6) == s.end() || s.find(7) == s.end() || s.find(8) == s.end() || s.find(9) == s.end())
+        if (mat[r + 2][c] + mat[r + 2][c + 1] + mat[r + 2][c + 2] != 15)
         {
             return false;
         }
-        else if (row2sum == row1sum && row3sum == row1sum && col1sum == row1sum && col2sum == row1sum && col3sum == row1sum && diag1sum == row1sum && diag2sum == row1sum)
-        {
-            return true;
-        }
-        else
+        if (mat[r][c] + mat[r + 1][c] + mat[r + 2][c] != 15)
         {
             return false;
         }
+        if (mat[r][c + 1] + mat[r + 1][c + 1] + mat[r + 2][c + 1] != 15)
+        {
+            return false;
+        }
+        if (mat[r][c + 2] + mat[r + 1][c + 2] + mat[r + 2][c + 2] != 15)
+        {
+            return false;
+        }
+        if (mat[r][c] + mat[r + 1][c + 1] + mat[r + 2][c + 2] != 15)
+        {
+            return false;
+        }
+        if (mat[r][c + 2] + mat[r + 1][c + 1] + mat[r + 2][c] != 15)
+        {
+            return false;
+        }
+        return true;
     }
     int numMagicSquaresInside(vector<vector<int>> &grid)
     {
-        int ans = 0, m = grid.size(), n = grid[0].size();
-        for (int i = 1; i < m - 1; i++)
+        const int m = grid.size(), n = grid[0].size();
+        int ans = 0;
+        for (int i = 0; i < m - 2; i++)
         {
-            for (int j = 1; j < n - 1; j++)
+            for (int j = 0; j < n - 2; j++)
             {
-                vector<vector<int>> mat = {{grid[i - 1][j - 1], grid[i - 1][j], grid[i - 1][j + 1]}, {grid[i][j - 1], grid[i][j], grid[i][j + 1]}, {grid[i + 1][j - 1], grid[i + 1][j], grid[i + 1][j + 1]}};
-                if (isMagicSquare(mat))
+                if (isMagicSquare(grid, i, j))
                 {
                     ans++;
                 }
