61f7d4f5766e6301f90115921d87fcfdbcb5e4bd
Rushabh <20rush09abh2001@gmail.com>
2025-12-30 13:38:23 +0530

    Added Java solution to 'How Many Numbers Are Smaller Than the Current Number'

---
+++ easy/How Many Numbers Are Smaller Than the Current Number/Java/main.java
@@ -0,0 +1,23 @@
+// Problem: How Many Numbers Are Smaller Than the Current Number
+// Link to the problem: https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        final int n = nums.length;
+        int[] freq = new int[101];
+        for (final int num : nums) {
+            freq[num]++;
+        }
+        for (int i = 1; i < 101; i++) {
+            freq[i] += freq[i - 1];
+        }
+        int[] ans = new int[n];
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) {
+                ans[i] = 0;
+            } else {
+                ans[i] = freq[nums[i] - 1];
+            }
+        }
+        return ans;
+    }
+}
