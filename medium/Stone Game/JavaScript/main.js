8b1826bf5a47086438d9c0531d5a733eeb1e8f4e
Rushabh <20rush09abh2001@gmail.com>
2025-12-31 14:10:42 +0530

    Added JavaScript solution to 'Stone Game'

---
+++ medium/Stone Game/JavaScript/main.js
@@ -0,0 +1,9 @@
+// Problem: Stone Game
+// Link to the problem: https://leetcode.com/problems/stone-game/
+/**
+ * @param {number[]} piles
+ * @return {boolean}
+ */
+var stoneGame = function (piles) {
+    return true;
+};
