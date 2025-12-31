d7cfa912e87d46807606afd9a43e6428fea8400c
Rushabh <20rush09abh2001@gmail.com>
2025-12-31 17:14:01 +0530

    Added C++ solution to 'Rotate Array'

---
+++ medium/Rotate Array/C++/main.cpp
@@ -0,0 +1,40 @@
+// Problem: Rotate Array
+// Link to the problem: https://leetcode.com/problems/rotate-array/
+class Solution
+{
+public:
+    void rotate(vector<int> &nums, int k)
+    {
+        const int n = nums.size();
+        k %= n;
+        int i = 0, j = n - 1;
+        while (i < j)
+        {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--;
+        }
+        i = 0;
+        j = k - 1;
+        while (i < j)
+        {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--;
+        }
+        i = k;
+        j = n - 1;
+        while (i < j)
+        {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--;
+        }
+    }
+};
