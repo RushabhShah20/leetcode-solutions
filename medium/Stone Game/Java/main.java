f03e1cf1b075054d8272a00f4226ef167ec30a8d
Rushabh <20rush09abh2001@gmail.com>
2025-12-31 14:09:09 +0530

    Added Java solution to 'Stone Game'

---
+++ medium/Stone Game/Java/main.java
@@ -0,0 +1,7 @@
+// Problem: Stone Game
+// Link to the problem: https://leetcode.com/problems/stone-game/
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        return true;
+    }
+}
