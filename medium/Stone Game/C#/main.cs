37f1e0fea7f42e4a4f74a2c1ffd054b5c488dc1c
Rushabh <20rush09abh2001@gmail.com>
2025-12-31 14:09:31 +0530

    Added C# solution to 'Stone Game'

---
+++ medium/Stone Game/C#/main.cs
@@ -0,0 +1,9 @@
+// Problem: Stone Game
+// Link to the problem: https://leetcode.com/problems/stone-game/
+public class Solution
+{
+    public bool StoneGame(int[] piles)
+    {
+        return true;
+    }
+}
