91746e86735b23668d7c5fe5c46e4eac3358e434
Rushabh <20rush09abh2001@gmail.com>
2025-12-31 14:12:23 +0530

    Added Rust solution to 'Stone Game'

---
+++ medium/Stone Game/Rust/main.rs
@@ -0,0 +1,7 @@
+// Problem: Stone Game
+// Link to the problem: https://leetcode.com/problems/stone-game/
+impl Solution {
+    pub fn stone_game(piles: Vec<i32>) -> bool {
+        return true;
+    }
+}
