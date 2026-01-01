84a2984732028ea085a0067fe8244f3a38962559
Rushabh <20rush09abh2001@gmail.com>
2026-01-01 10:01:48 +0530

    Added Java solution to 'Plus One'

--- easy/Plus One/Java/main.java
+++ easy/Plus One/Java/main.java
@@ -1,30 +1,24 @@
 // Problem: Plus One
 // Link to the problem: https://leetcode.com/problems/plus-one/
-class Solution
-{
-public:
-    vector<int> plusOne(vector<int> &digits)
-    {
-        int carry = 0, n = digits.size();
-        for (int i = n - 1; i >= 0; i--)
-        {
-            if (i == n - 1)
-            {
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int carry = 0, n = digits.length;
+        for (int i = n - 1; i >= 0; i--) {
+            if (i == n - 1) {
                 int x = digits[i];
                 digits[i] = (digits[i] + 1) % 10;
                 carry = (x + 1) / 10;
-            }
-            else
-            {
+            } else {
                 int x = digits[i];
                 digits[i] = (digits[i] + carry) % 10;
                 carry = (x + carry) / 10;
             }
         }
-        if (carry == 1)
-        {
-            digits.insert(digits.begin(), 1);
+        if (carry == 1) {
+            List<Integer> l = Arrays.stream(digits).boxed().collect(Collectors.toList());
+            l.add(0, 1);
+            digits = l.stream().mapToInt(i -> i).toArray();
         }
         return digits;
     }
-};
+}
