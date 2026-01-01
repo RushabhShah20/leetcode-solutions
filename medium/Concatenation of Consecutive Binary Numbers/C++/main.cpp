510426cc174c7c1674c3878bb2e52b1bec38f74b
Rushabh <20rush09abh2001@gmail.com>
2026-01-01 16:54:40 +0530

    Added C++ solution to 'Concatenation of Consecutive Binary Numbers'

---
+++ medium/Concatenation of Consecutive Binary Numbers/C++/main.cpp
@@ -0,0 +1,21 @@
+// Problem: Concatenation of Consecutive Binary Numbers
+// Link to the problem: https://leetcode.com/problems/concatenation-of-consecutive-binary-numbers/
+class Solution
+{
+public:
+    int concatenatedBinary(int n)
+    {
+        long long ans = 0, mod = 1000000007;
+        for (int i = 1; i <= n; i++)
+        {
+            int x = i;
+            while (x > 0)
+            {
+                x /= 2;
+                ans *= 2;
+            }
+            ans = (ans + i) % mod;
+        }
+        return ans;
+    }
+};
