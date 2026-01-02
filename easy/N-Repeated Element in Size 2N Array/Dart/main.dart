ba9f3c69ad5527d030a394a5c1c9b7dbb8d267bd
Rushabh <20rush09abh2001@gmail.com>
2026-01-02 11:57:40 +0530

    Added Dart solution to 'N-Repeated Element in Size 2N Array'

---
+++ easy/N-Repeated Element in Size 2N Array/Dart/main.dart
@@ -0,0 +1,13 @@
+// Problem: N-Repeated Element in Size 2N Array
+// Link to the problem: https://leetcode.com/problems/n-repeated-element-in-size-2n-array/
+class Solution {
+  int repeatedNTimes(List<int> nums) {
+    final int n = nums.length;
+    for (int i = 0; i < n - 2; i++) {
+      if (nums[i] == nums[i + 1] || nums[i] == nums[i + 2]) {
+        return nums[i];
+      }
+    }
+    return nums[n - 1];
+  }
+}
