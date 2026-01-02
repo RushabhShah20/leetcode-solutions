d39356776725029682aa57efb5574f92e7e3ad34
Rushabh <20rush09abh2001@gmail.com>
2026-01-02 14:18:38 +0530

    Added C++ solution to 'Check If Array Pairs Are Divisible by k'

---
+++ medium/Check If Array Pairs Are Divisible by k/C++/main.cpp
@@ -0,0 +1,32 @@
+// Problem: Check If Array Pairs Are Divisible by k
+// Link to the problem: https://leetcode.com/problems/check-if-array-pairs-are-divisible-by-k/
+class Solution
+{
+public:
+    bool canArrange(vector<int> &arr, int k)
+    {
+        const int n = arr.size();
+        vector<int> freq(k, 0);
+        for (const int &num : arr)
+        {
+            int rem = num % k;
+            if (rem < 0)
+            {
+                rem += k;
+            }
+            freq[rem]++;
+        }
+        if (freq[0] % 2 != 0)
+        {
+            return false;
+        }
+        for (int i = 1; i <= k / 2; i++)
+        {
+            if (freq[i] != freq[k - i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
