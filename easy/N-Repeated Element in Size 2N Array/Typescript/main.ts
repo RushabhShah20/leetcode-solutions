12cc8292106f9e268aad73e05ba024e6d692a719
Rushabh <20rush09abh2001@gmail.com>
2026-01-02 11:57:24 +0530

    Added Typescript solution to 'N-Repeated Element in Size 2N Array'

---
+++ easy/N-Repeated Element in Size 2N Array/Typescript/main.ts
@@ -0,0 +1,11 @@
+// Problem: N-Repeated Element in Size 2N Array
+// Link to the problem: https://leetcode.com/problems/n-repeated-element-in-size-2n-array/
+function repeatedNTimes(nums: number[]): number {
+    const n: number = nums.length;
+    for (let i = 0; i < n - 2; i++) {
+        if (nums[i] === nums[i + 1] || nums[i] === nums[i + 2]) {
+            return nums[i];
+        }
+    }
+    return nums[n - 1];
+};
