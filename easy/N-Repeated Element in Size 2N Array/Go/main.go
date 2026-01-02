817f264baa83aca33f7a16536207169afbf340ee
Rushabh <20rush09abh2001@gmail.com>
2026-01-02 11:57:53 +0530

    Added Go solution to 'N-Repeated Element in Size 2N Array'

---
+++ easy/N-Repeated Element in Size 2N Array/Go/main.go
@@ -0,0 +1,11 @@
+// Problem: N-Repeated Element in Size 2N Array
+// Link to the problem: https://leetcode.com/problems/n-repeated-element-in-size-2n-array/
+func repeatedNTimes(nums []int) int {
+	var n int = len(nums)
+	for i := 0; i < n-2; i++ {
+		if nums[i] == nums[i+1] || nums[i] == nums[i+2] {
+			return nums[i]
+		}
+	}
+	return nums[n-1]
+}
