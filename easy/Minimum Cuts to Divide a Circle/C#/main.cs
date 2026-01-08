ed20c1b72ac106eda7d6909fbb332fee3c858889
Rushabh <20rush09abh2001@gmail.com>
2026-01-08 14:38:18 +0530

    Added C# solution to 'Minimum Cuts to Divide a Circle'

---
+++ easy/Minimum Cuts to Divide a Circle/C#/main.cs
@@ -0,0 +1,20 @@
+// Problem: Minimum Cuts to Divide a Circle
+// Link to the problem: https://leetcode.com/problems/minimum-cuts-to-divide-a-circle/
+public class Solution
+{
+    public int NumberOfCuts(int n)
+    {
+        if (n == 1)
+        {
+            return 0;
+        }
+        else if (n % 2 == 0)
+        {
+            return n / 2;
+        }
+        else
+        {
+            return n;
+        }
+    }
+}
