d3636b64e4c54587030324e714f5c9f0482d2ac3
Rushabh <20rush09abh2001@gmail.com>
2026-01-08 14:38:28 +0530

    Added C solution to 'Minimum Cuts to Divide a Circle'

---
+++ easy/Minimum Cuts to Divide a Circle/C/main.c
@@ -0,0 +1,17 @@
+// Problem: Minimum Cuts to Divide a Circle
+// Link to the problem: https://leetcode.com/problems/minimum-cuts-to-divide-a-circle/
+int numberOfCuts(int n)
+{
+    if (n == 1)
+    {
+        return 0;
+    }
+    else if (n % 2 == 0)
+    {
+        return n / 2;
+    }
+    else
+    {
+        return n;
+    }
+}
