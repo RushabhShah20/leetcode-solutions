d2e96d924b9f46c93d8527da4b980c2c3080ec80
Rushabh <20rush09abh2001@gmail.com>
2026-01-08 14:20:50 +0530

    Added JavaScript solution to 'Three Divisors'

---
+++ easy/Three Divisors/JavaScript/main.js
@@ -0,0 +1,20 @@
+// Problem: Three Divisors
+// Link to the problem: https://leetcode.com/problems/three-divisors/
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isThree = function (n) {
+    let count = 0;
+    for (let i = 1; i <= n; i++) {
+        if (n % i === 0) {
+            count++;
+        }
+    }
+    if (count === 3) {
+        return true;
+    }
+    else {
+        return false;
+    }
+};
