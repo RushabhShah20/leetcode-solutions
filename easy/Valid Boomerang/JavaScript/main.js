6e7a59f45df8e036ea70d47a958ddf3bd7b54ecf
Rushabh <20rush09abh2001@gmail.com>
2026-01-08 16:02:22 +0530

    Added JavaScript solution to 'Valid Boomerang'

---
+++ easy/Valid Boomerang/JavaScript/main.js
@@ -0,0 +1,20 @@
+// Problem: Valid Boomerang
+// Link to the problem: https://leetcode.com/problems/valid-boomerang/
+/**
+ * @param {number[][]} points
+ * @return {boolean}
+ */
+var isBoomerang = function (points) {
+    const x1 = points[0][0], x2 = points[1][0], x3 = points[2][0], y1 = points[0][1], y2 = points[1][1], y3 = points[2][1];
+    if ((x1 === x2 && y1 === y2) || (x2 === x3 && y2 === y3) || (x3 === x1 && y3 === y1)) {
+        return false;
+    }
+    else {
+        if ((y2 - y1) * (x3 - x2) !== (y3 - y2) * (x2 - x1)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+};
