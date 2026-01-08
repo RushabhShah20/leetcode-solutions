4a313e043bfc013e512a1e9ef628188f27cc332d
Rushabh <20rush09abh2001@gmail.com>
2026-01-08 14:06:24 +0530

    Added Java solution to 'Smallest Even Multiple'

---
+++ easy/Smallest Even Multiple/Java/main.java
@@ -0,0 +1,11 @@
+// Problem: Smallest Even Multiple
+// Link to the problem: https://leetcode.com/problems/smallest-even-multiple/
+class Solution {
+    public int smallestEvenMultiple(int n) {
+        if (n % 2 == 0) {
+            return n;
+        } else {
+            return 2 * n;
+        }
+    }
+}
