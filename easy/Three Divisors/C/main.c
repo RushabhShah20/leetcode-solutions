9d6e1b6fe1f0a1aa3b46d15abf3b52521a1203db
Rushabh <20rush09abh2001@gmail.com>
2026-01-08 14:20:19 +0530

    Added C solution to 'Three Divisors'

---
+++ easy/Three Divisors/C/main.c
@@ -0,0 +1,21 @@
+// Problem: Three Divisors
+// Link to the problem: https://leetcode.com/problems/three-divisors/
+bool isThree(int n)
+{
+    int count = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            count++;
+        }
+    }
+    if (count == 3)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
