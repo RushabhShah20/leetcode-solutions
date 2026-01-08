df83ff96129dba8df7900a2da7cc80b01a5167f4
Rushabh <20rush09abh2001@gmail.com>
2026-01-08 14:07:16 +0530

    Added JavaScript solution to 'Smallest Even Multiple'

---
+++ easy/Smallest Even Multiple/JavaScript/main.js
@@ -0,0 +1,14 @@
+// Problem: Smallest Even Multiple
+// Link to the problem: https://leetcode.com/problems/smallest-even-multiple/
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var smallestEvenMultiple = function (n) {
+    if (n % 2 === 0) {
+        return n;
+    }
+    else {
+        return 2 * n;
+    }
+};
