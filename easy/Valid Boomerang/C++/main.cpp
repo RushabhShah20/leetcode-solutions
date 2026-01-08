78ebf9cd0644f4724291f9dd7c65467277aa6468
Rushabh <20rush09abh2001@gmail.com>
2026-01-08 16:01:30 +0530

    Added Java solution to 'Valid Boomerang'

--- easy/Valid Boomerang/C++/main.cpp
+++ easy/Valid Boomerang/C++/main.cpp
@@ -5,7 +5,7 @@ class Solution
 public:
     bool isBoomerang(vector<vector<int>> &points)
     {
-        int x1 = points[0][0], x2 = points[1][0], x3 = points[2][0], y1 = points[0][1], y2 = points[1][1], y3 = points[2][1];
+        const int x1 = points[0][0], x2 = points[1][0], x3 = points[2][0], y1 = points[0][1], y2 = points[1][1], y3 = points[2][1];
         if ((x1 == x2 && y1 == y2) || (x2 == x3 && y2 == y3) || (x3 == x1 && y3 == y1))
         {
             return false;
