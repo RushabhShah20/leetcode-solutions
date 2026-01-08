20d9ec83de31e545adac8abc137ed1db8d9095fb
Rushabh <20rush09abh2001@gmail.com>
2026-01-08 16:01:42 +0530

    Added C# solution to 'Valid Boomerang'

---
+++ easy/Valid Boomerang/C#/main.cs
@@ -0,0 +1,24 @@
+// Problem: Valid Boomerang
+// Link to the problem: https://leetcode.com/problems/valid-boomerang/
+public class Solution
+{
+    public bool IsBoomerang(int[][] points)
+    {
+        int x1 = points[0][0], x2 = points[1][0], x3 = points[2][0], y1 = points[0][1], y2 = points[1][1], y3 = points[2][1];
+        if ((x1 == x2 && y1 == y2) || (x2 == x3 && y2 == y3) || (x3 == x1 && y3 == y1))
+        {
+            return false;
+        }
+        else
+        {
+            if ((y2 - y1) * (x3 - x2) != (y3 - y2) * (x2 - x1))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
