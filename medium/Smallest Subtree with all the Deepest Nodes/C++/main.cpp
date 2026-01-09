f04d494b1b4259832e8ac51e7222f2d5b8df1196
Rushabh <20rush09abh2001@gmail.com>
2026-01-09 14:23:23 +0530

    Added C++ solution to 'Smallest Subtree with all the Deepest Nodes'

---
+++ medium/Smallest Subtree with all the Deepest Nodes/C++/main.cpp
@@ -0,0 +1,38 @@
+// Problem: Smallest Subtree with all the Deepest Nodes
+// Link to the problem: https://leetcode.com/problems/smallest-subtree-with-all-the-deepest-nodes/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    pair<TreeNode *, int> dfs(TreeNode *root)
+    {
+        if (root == NULL)
+        {
+            return {NULL, 0};
+        }
+        pair<TreeNode *, int> left = dfs(root->left), right = dfs(root->right);
+        if (left.second > right.second)
+        {
+            return {left.first, left.second + 1};
+        }
+        if (right.second > left.second)
+        {
+            return {right.first, right.second + 1};
+        }
+        return {root, left.second + 1};
+    }
+    TreeNode *subtreeWithAllDeepest(TreeNode *root)
+    {
+        return dfs(root).first;
+    }
+};
