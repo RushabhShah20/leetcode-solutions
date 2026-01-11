da6ca326665b1161f2467c8af2520b0f1148c1e0
Rushabh <20rush09abh2001@gmail.com>
2026-01-11 16:36:53 +0530

    Added C++ solution to 'Reverse Bits'

---
+++ easy/Reverse Bits/C++/main.cpp
@@ -0,0 +1,17 @@
+// Problem: Reverse Bits
+// Link to the problem: https://leetcode.com/problems/reverse-bits/
+class Solution
+{
+public:
+    int reverseBits(int n)
+    {
+        int ans = 0;
+        for (int i = 0; i < 32; i++)
+        {
+            ans <<= 1;
+            ans |= (n & 1);
+            n >>= 1;
+        }
+        return ans;
+    }
+};
