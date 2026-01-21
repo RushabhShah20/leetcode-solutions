fe32114077ba2ab2dcaa1c044af9e994fb33782e
Rushabh <20rush09abh2001@gmail.com>
2026-01-21 13:15:53 +0530

    Added Rust solution to 'Construct the Minimum Bitwise Array II'

---
+++ medium/Construct the Minimum Bitwise Array II/Rust/main.rs
@@ -0,0 +1,16 @@
+// Problem: Construct the Minimum Bitwise Array II
+// Link to the problem: https://leetcode.com/problems/construct-the-minimum-bitwise-array-ii/
+impl Solution {
+    pub fn min_bitwise_array(nums: Vec<i32>) -> Vec<i32> {
+        let n: usize = nums.len();
+        let mut ans: Vec<i32> = vec![0; n];
+        for i in 0..n {
+            if (nums[i] % 2 == 0) {
+                ans[i] = -1;
+            } else {
+                ans[i] = nums[i] - (((nums[i] + 1) & (-nums[i] - 1)) / 2);
+            }
+        }
+        return ans;
+    }
+}
