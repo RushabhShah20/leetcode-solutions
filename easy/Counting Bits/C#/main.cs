13f1443c84645d1e70954fcf9313eadb79392834
Rushabh <20rush09abh2001@gmail.com>
2026-01-22 15:46:59 +0530

    Added C# solution to 'Counting Bits'

---
+++ easy/Counting Bits/C#/main.cs
@@ -0,0 +1,14 @@
+// Problem: Counting Bits
+// Link to the problem: https://leetcode.com/problems/counting-bits/
+public class Solution
+{
+    public int[] CountBits(int n)
+    {
+        int[] ans = new int[n + 1];
+        for (int i = 1; i <= n; i++)
+        {
+            ans[i] = ans[i >> 1] + (i & 1);
+        }
+        return ans;
+    }
+}
