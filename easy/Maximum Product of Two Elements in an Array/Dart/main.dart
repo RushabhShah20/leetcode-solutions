f6dea0e7c9ece93b0cfd2f7da2d824509e3e15e0
Rushabh <20rush09abh2001@gmail.com>
2026-01-22 15:27:52 +0530

    Added Dart solution to 'Maximum Product of Two Elements in an Array'

---
+++ easy/Maximum Product of Two Elements in an Array/Dart/main.dart
@@ -0,0 +1,17 @@
+// Problem: Maximum Product of Two Elements in an Array
+// Link to the problem: https://leetcode.com/problems/maximum-product-of-two-elements-in-an-array/
+class Solution {
+  int maxProduct(List<int> nums) {
+    int x = 0, y = 0;
+    for (final int num in nums) {
+      if (num > x) {
+        y = x;
+        x = num;
+      } else if (num > y) {
+        y = num;
+      }
+    }
+    final int ans = (x - 1) * (y - 1);
+    return ans;
+  }
+}
