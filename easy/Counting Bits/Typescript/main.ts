fc4026756ec8c294e13914d3d067101d2a051ec9
Rushabh <20rush09abh2001@gmail.com>
2026-01-22 15:48:05 +0530

    Added Typescript solution to 'Counting Bits'

---
+++ easy/Counting Bits/Typescript/main.ts
@@ -0,0 +1,9 @@
+// Problem: Counting Bits
+// Link to the problem: https://leetcode.com/problems/counting-bits/
+function countBits(n: number): number[] {
+    let ans: number[] = new Array(n + 1).fill(0);
+    for (let i = 1; i <= n; i++) {
+        ans[i] = ans[i >> 1] + (i & 1);
+    }
+    return ans;
+};
