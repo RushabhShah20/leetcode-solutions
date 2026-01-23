80cc5108bd9c1f66f52051b23320a8087f411ac4
Rushabh <20rush09abh2001@gmail.com>
2026-01-23 15:18:08 +0530

    Added Typescript solution to 'A Number After a Double Reversal'

---
+++ easy/A Number After a Double Reversal/Typescript/main.ts
@@ -0,0 +1,13 @@
+// Problem: A Number After a Double Reversal
+// Link to the problem: https://leetcode.com/problems/a-number-after-a-double-reversal/
+function isSameAfterReversals(num: number): boolean {
+    if (num === 0) {
+        return true;
+    }
+    if (num % 10 === 0) {
+        return false;
+    }
+    else {
+        return true;
+    }
+};
