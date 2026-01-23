644d4e98ce844e27fe9b3af1a8f529f426a30916
Rushabh <20rush09abh2001@gmail.com>
2026-01-23 15:17:46 +0530

    Added JavaScript solution to 'A Number After a Double Reversal'

---
+++ easy/A Number After a Double Reversal/JavaScript/main.js
@@ -0,0 +1,17 @@
+// Problem: A Number After a Double Reversal
+// Link to the problem: https://leetcode.com/problems/a-number-after-a-double-reversal/
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isSameAfterReversals = function (num) {
+    if (num === 0) {
+        return true;
+    }
+    if (num % 10 === 0) {
+        return false;
+    }
+    else {
+        return true;
+    }
+};
