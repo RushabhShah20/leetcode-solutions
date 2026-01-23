4d51394fa5058eb2f53d50fb376b55f86b985856
Rushabh <20rush09abh2001@gmail.com>
2026-01-23 15:32:43 +0530

    Added C++ solution to 'Intervals Between Identical Elements'

---
+++ medium/Intervals Between Identical Elements/C++/main.cpp
@@ -0,0 +1,37 @@
+// Problem: Intervals Between Identical Elements
+// Link to the problem: https://leetcode.com/problems/intervals-between-identical-elements/
+class Solution
+{
+public:
+    vector<long long> getDistances(vector<int> &arr)
+    {
+        const int n = arr.size();
+        unordered_map<int, vector<long long>> m;
+        for (int i = 0; i < n; i++)
+        {
+            m[arr[i]].push_back(i);
+        }
+        vector<long long> ans(n, 0), prefix(n, 0), suffix(n, 0);
+        for (const pair<int, vector<long long>> i : m)
+        {
+            const vector<long long> v = i.second;
+            for (int i = 1; i < v.size(); i++)
+            {
+                prefix[v[i]] = prefix[v[i - 1]] + (i * (long long)(v[i] - v[i - 1]));
+            }
+        }
+        for (const pair<int, vector<long long>> i : m)
+        {
+            const vector<long long> v = i.second;
+            for (int i = v.size() - 2; i >= 0; i--)
+            {
+                suffix[v[i]] = suffix[v[i + 1]] + ((long long)(v.size() - i - 1) * (long long)(v[i + 1] - v[i]));
+            }
+        }
+        for (int i = 0; i < n; i++)
+        {
+            ans[i] += (prefix[i] + suffix[i]);
+        }
+        return ans;
+    }
+};
