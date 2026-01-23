4db2e13033d3a9156d0f862defab283b5eda7c95
Rushabh <20rush09abh2001@gmail.com>
2026-01-23 15:17:02 +0530

    Added C# solution to 'A Number After a Double Reversal'

---
+++ easy/A Number After a Double Reversal/C#/main.cs
@@ -0,0 +1,20 @@
+// Problem: A Number After a Double Reversal
+// Link to the problem: https://leetcode.com/problems/a-number-after-a-double-reversal/
+public class Solution
+{
+    public bool IsSameAfterReversals(int num)
+    {
+        if (num == 0)
+        {
+            return true;
+        }
+        if (num % 10 == 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+}
