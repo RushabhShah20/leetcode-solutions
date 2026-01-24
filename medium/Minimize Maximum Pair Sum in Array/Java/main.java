df7a2d188e31c8e6f7db1dff721040dd92bdc41f
Rushabh <20rush09abh2001@gmail.com>
2026-01-24 11:41:38 +0530

    Added Java solution to 'Minimize Maximum Pair Sum in Array'

--- medium/Minimize Maximum Pair Sum in Array/Java/main.java
+++ medium/Minimize Maximum Pair Sum in Array/Java/main.java
@@ -1,19 +1,15 @@
 // Problem: Minimize Maximum Pair Sum in Array
 // Link to the problem: https://leetcode.com/problems/minimize-maximum-pair-sum-in-array/
-class Solution
-{
-public:
-    int minPairSum(vector<int> &nums)
-    {
-        const int n = nums.size();
-        sort(nums.begin(), nums.end());
+class Solution {
+    public int minPairSum(int[] nums) {
+        final int n = nums.length;
+        Arrays.sort(nums);
         int l = 0, r = n - 1, ans = 0;
-        while (l < r)
-        {
-            ans = max(ans, nums[l] + nums[r]);
+        while (l < r) {
+            ans = Math.max(ans, nums[l] + nums[r]);
             l++;
             r--;
         }
         return ans;
     }
-};
+}
