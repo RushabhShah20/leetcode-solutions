53529bc8ef86dbebf3ea790822727cb9ff09d528
Rushabh <20rush09abh2001@gmail.com>
2026-01-25 08:55:58 +0530

    Added Java solution to 'Minimum Difference Between Highest and Lowest of K Scores'

---
+++ easy/Minimum Difference Between Highest and Lowest of K Scores/Java/main.java
@@ -0,0 +1,13 @@
+// Problem: Minimum Difference Between Highest and Lowest of K Scores
+// Link to the problem: https://leetcode.com/problems/minimum-difference-between-highest-and-lowest-of-k-scores/
+class Solution {
+    public int minimumDifference(int[] nums, int k) {
+        final int n = nums.length;
+        Arrays.sort(nums);
+        int ans = 100001;
+        for (int i = 0; i < n - k + 1; i++) {
+            ans = Math.min(ans, nums[i + k - 1] - nums[i]);
+        }
+        return ans;
+    }
+}
