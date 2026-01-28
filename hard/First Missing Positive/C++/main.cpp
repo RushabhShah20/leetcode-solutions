f5abd1b163fb29a0087316e20df82f66a818a76c
Rushabh <20rush09abh2001@gmail.com>
2026-01-28 14:13:38 +0530

    Added C++ solution to 'First Missing Positive'

---
+++ hard/First Missing Positive/C++/main.cpp
@@ -0,0 +1,30 @@
+// Problem: First Missing Positive
+// Link to the problem: https://leetcode.com/problems/first-missing-positive/
+class Solution
+{
+public:
+    int firstMissingPositive(vector<int> &nums)
+    {
+        const int n = nums.size();
+        int i = 0;
+        while (i < n)
+        {
+            if (nums[i] > 0 && nums[i] < n && nums[i] != nums[nums[i] - 1])
+            {
+                swap(nums[i], nums[nums[i] - 1]);
+            }
+            else
+            {
+                i++;
+            }
+        }
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] != i + 1)
+            {
+                return i + 1;
+            }
+        }
+        return n + 1;
+    }
+};
