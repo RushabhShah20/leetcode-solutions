1e4e603183bae887b3155c254df24aa3728d18fc
Rushabh <20rush09abh2001@gmail.com>
2026-01-31 12:27:42 +0530

    Added Java solution to 'Find Smallest Letter Greater Than Target'

--- easy/Find Smallest Letter Greater Than Target/Java/main.java
+++ easy/Find Smallest Letter Greater Than Target/Java/main.java
@@ -1,26 +1,19 @@
 // Problem: Find Smallest Letter Greater Than Target
 // Link to the problem: https://leetcode.com/problems/find-smallest-letter-greater-than-target/
-class Solution
-{
-public:
-    char nextGreatestLetter(vector<char> &letters, char target)
-    {
-        const int n = letters.size();
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        final int n = letters.length;
         int l = 0, r = n - 1;
         char ans = letters[0];
-        while (l <= r)
-        {
-            const int m = l + (r - l) / 2;
-            if (letters[m] > target)
-            {
+        while (l <= r) {
+            final int m = l + (r - l) / 2;
+            if (letters[m] > target) {
                 ans = letters[m];
                 r = m - 1;
-            }
-            else
-            {
+            } else {
                 l = m + 1;
             }
         }
         return ans;
     }
-};
+}
