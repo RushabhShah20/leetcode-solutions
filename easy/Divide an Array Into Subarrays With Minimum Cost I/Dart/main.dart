b507fd3f8f06ed0344e6f4a93c7e90b5dfab29ed
Rushabh <20rush09abh2001@gmail.com>
2026-02-01 07:56:25 +0530

    Added Dart solution to 'Divide an Array Into Subarrays With Minimum Cost I'

---
+++ easy/Divide an Array Into Subarrays With Minimum Cost I/Dart/main.dart
@@ -0,0 +1,18 @@
+// Problem: Divide an Array Into Subarrays With Minimum Cost I
+// Link to the problem: https://leetcode.com/problems/divide-an-array-into-subarrays-with-minimum-cost-i/
+class Solution {
+  int minimumCost(List<int> nums) {
+    final int n = nums.length;
+    int m1 = 51, m2 = 51;
+    for (int i = 1; i < n; i++) {
+      if (nums[i] < m1) {
+        m2 = m1;
+        m1 = nums[i];
+      } else if (nums[i] < m2) {
+        m2 = nums[i];
+      }
+    }
+    final int ans = nums[0] + m1 + m2;
+    return ans;
+  }
+}
