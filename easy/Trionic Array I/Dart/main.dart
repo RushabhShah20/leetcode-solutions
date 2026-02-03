195e1c9eb351eda2c9ef916f6aa57c5a5b8dd8a2
Rushabh <20rush09abh2001@gmail.com>
2026-02-03 13:27:09 +0530

    Added Dart solution to 'Trionic Array I'

---
+++ easy/Trionic Array I/Dart/main.dart
@@ -0,0 +1,27 @@
+// Problem: Trionic Array I
+// Link to the problem: https://leetcode.com/problems/trionic-array-i/
+class Solution {
+  bool isTrionic(List<int> nums) {
+    final int n = nums.length;
+    int i = 0, p = 0, q = 0;
+    while (i < n - 1 && nums[i + 1] > nums[i]) {
+      i++;
+    }
+    p = i;
+    while (i < n - 1 && nums[i + 1] < nums[i]) {
+      i++;
+    }
+    q = i;
+    while (i < n - 1 && nums[i + 1] > nums[i]) {
+      i++;
+    }
+    if (i == n - 1) {
+      if (p != 0 && q != 0 && p != n - 1 && q != n - 1) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+    return false;
+  }
+}
