fc6351223e6c62d4956a7de84ee6b48c76676089
Rushabh <20rush09abh2001@gmail.com>
2026-02-03 13:26:08 +0530

    Added C solution to 'Trionic Array I'

---
+++ easy/Trionic Array I/C/main.c
@@ -0,0 +1,33 @@
+// Problem: Trionic Array I
+// Link to the problem: https://leetcode.com/problems/trionic-array-i/
+bool isTrionic(int *nums, int numsSize)
+{
+    const int n = numsSize;
+    int i = 0, p = 0, q = 0;
+    while (i < n - 1 && nums[i + 1] > nums[i])
+    {
+        i++;
+    }
+    p = i;
+    while (i < n - 1 && nums[i + 1] < nums[i])
+    {
+        i++;
+    }
+    q = i;
+    while (i < n - 1 && nums[i + 1] > nums[i])
+    {
+        i++;
+    }
+    if (i == n - 1)
+    {
+        if (p != 0 && q != 0 && p != n - 1 && q != n - 1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    return false;
+}
