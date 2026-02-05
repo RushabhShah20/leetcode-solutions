012a27f0833a8260b5dab29b7aa7b4cb7e09e5c9
Rushabh <20rush09abh2001@gmail.com>
2026-02-05 17:06:54 +0530

    Added Go solution to 'Stone Removal Game'

---
+++ easy/Stone Removal Game/Go/main.go
@@ -0,0 +1,17 @@
+// Problem: Stone Removal Game
+// Link to the problem: https://leetcode.com/problems/stone-removal-game/
+func canAliceWin(n int) bool {
+	if n >= 10 && n < 19 {
+		return true
+	}
+	if n >= 27 && n < 34 {
+		return true
+	}
+	if n >= 40 && n < 45 {
+		return true
+	}
+	if n >= 49 && n < 52 {
+		return true
+	}
+	return false
+}
