4f05ce0887f07cc0cda18225d2cb089d5fafaff2
Rushabh <20rush09abh2001@gmail.com>
2026-02-05 17:05:36 +0530

    Added C solution to 'Stone Removal Game'

---
+++ easy/Stone Removal Game/C/main.c
@@ -0,0 +1,22 @@
+// Problem: Stone Removal Game
+// Link to the problem: https://leetcode.com/problems/stone-removal-game/
+bool canAliceWin(int n)
+{
+    if (n >= 10 && n < 19)
+    {
+        return true;
+    }
+    if (n >= 27 && n < 34)
+    {
+        return true;
+    }
+    if (n >= 40 && n < 45)
+    {
+        return true;
+    }
+    if (n >= 49 && n < 52)
+    {
+        return true;
+    }
+    return false;
+}
