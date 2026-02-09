0a2c9e643874a43a7b16bb48d9140223ea171270
Rushabh <20rush09abh2001@gmail.com>
2026-02-09 15:04:13 +0530

    Added Java solution to 'Lucky Numbers in a Matrix'

---
+++ easy/Lucky Numbers in a Matrix/Java/main.java
@@ -0,0 +1,28 @@
+// Problem: Lucky Numbers in a Matrix
+// Link to the problem: https://leetcode.com/problems/lucky-numbers-in-a-matrix/
+class Solution {
+    public List<Integer> luckyNumbers(int[][] matrix) {
+        final int m = matrix.length, n = matrix[0].length;
+        List<Integer> ans = new ArrayList<>();
+        int col = -1;
+        for (int i = 0; i < m; i++) {
+            int minVal = 100001;
+            int maxVal = 0;
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] < minVal) {
+                    minVal = matrix[i][j];
+                    col = j;
+                }
+            }
+            for (int k = 0; k < m; k++) {
+                if (matrix[k][col] > maxVal) {
+                    maxVal = matrix[k][col];
+                }
+            }
+            if (minVal == maxVal) {
+                ans.add(minVal);
+            }
+        }
+        return ans;
+    }
+}
