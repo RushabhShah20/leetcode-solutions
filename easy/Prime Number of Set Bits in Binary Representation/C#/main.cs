d11d0248a5e4afe71e6352e30be5f135bd236e0c
Rushabh <20rush09abh2001@gmail.com>
2026-02-21 12:07:24 +0530

    Added C# solution to 'Prime Number of Set Bits in Binary Representation'

---
+++ easy/Prime Number of Set Bits in Binary Representation/C#/main.cs
@@ -0,0 +1,42 @@
+// Problem: Prime Number of Set Bits in Binary Representation
+// Link to the problem: https://leetcode.com/problems/prime-number-of-set-bits-in-binary-representation/
+public class Solution
+{
+    public bool isPrime(int n)
+    {
+        if (n == 1)
+        {
+            return false;
+        }
+        for (int i = 2; i * i <= n; i++)
+        {
+            if (n % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    public int setBits(int n)
+    {
+        int ans = 0;
+        while (n > 0)
+        {
+            n &= (n - 1);
+            ans++;
+        }
+        return ans;
+    }
+    public int CountPrimeSetBits(int left, int right)
+    {
+        int ans = 0;
+        for (int i = left; i <= right; i++)
+        {
+            if (isPrime(setBits(i)))
+            {
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
