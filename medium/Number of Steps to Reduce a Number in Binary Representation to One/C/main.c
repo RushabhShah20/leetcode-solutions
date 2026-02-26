eeb287aa7932e4ba2d9cb39f1ad88c83bb2a8eac
Rushabh <20rush09abh2001@gmail.com>
2026-02-26 16:00:05 +0530

    Added C solution to 'Number of Steps to Reduce a Number in Binary Representation to One'

---
+++ medium/Number of Steps to Reduce a Number in Binary Representation to One/C/main.c
@@ -0,0 +1,22 @@
+// Problem: Number of Steps to Reduce a Number in Binary Representation to One
+// Link to the problem: https://leetcode.com/problems/number-of-steps-to-reduce-a-number-in-binary-representation-to-one/
+int numSteps(char *s)
+{
+    const int n = strlen(s);
+    int ans = 0, x = 0;
+    for (int i = n - 1; i > 0; i--)
+    {
+        const int y = s[i] - '0' + x;
+        if (y % 2 == 0)
+        {
+            ans++;
+        }
+        else
+        {
+            ans += 2;
+            x = 1;
+        }
+    }
+    ans += x;
+    return ans;
+}
