4f081ca11a880eaaf44881189811c94111357a7d
Rushabh <20rush09abh2001@gmail.com>
2026-02-26 16:01:54 +0530

    Added Dart solution to 'Number of Steps to Reduce a Number in Binary Representation to One'

---
+++ medium/Number of Steps to Reduce a Number in Binary Representation to One/Dart/main.dart
@@ -0,0 +1,19 @@
+// Problem: Number of Steps to Reduce a Number in Binary Representation to One
+// Link to the problem: https://leetcode.com/problems/number-of-steps-to-reduce-a-number-in-binary-representation-to-one/
+class Solution {
+  int numSteps(String s) {
+    final int n = s.length;
+    int ans = 0, x = 0;
+    for (int i = n - 1; i > 0; i--) {
+      final int y = (s[i]).codeUnitAt(0) - ('0').codeUnitAt(0) + x;
+      if (y % 2 == 0) {
+        ans++;
+      } else {
+        ans += 2;
+        x = 1;
+      }
+    }
+    ans += x;
+    return ans;
+  }
+}
