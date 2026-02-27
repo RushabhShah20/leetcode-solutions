9a56627be847dbf27a506746f712ae476329b918
Rushabh <20rush09abh2001@gmail.com>
2026-02-27 18:17:13 +0530

    Added C++ solution to 'Matrix Diagonal Sum'

---
+++ easy/Matrix Diagonal Sum/C++/main.cpp
@@ -0,0 +1,20 @@
+// Problem: Matrix Diagonal Sum
+// Link to the problem: https://leetcode.com/problems/matrix-diagonal-sum/
+class Solution
+{
+public:
+    int diagonalSum(vector<vector<int>> &mat)
+    {
+        const int n = mat.size();
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            ans += mat[i][i];
+            if (i != n - 1 - i)
+            {
+                ans += mat[i][n - i - 1];
+            }
+        }
+        return ans;
+    }
+};
