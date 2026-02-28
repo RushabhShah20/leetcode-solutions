67e4810505a811a34b087aa6c42a562f4723fd69
Rushabh <20rush09abh2001@gmail.com>
2026-02-28 15:32:50 +0530

    Added C solution to 'Concatenation of Consecutive Binary Numbers'

---
+++ medium/Concatenation of Consecutive Binary Numbers/C/main.c
@@ -0,0 +1,19 @@
+// Problem: Concatenation of Consecutive Binary Numbers
+// Link to the problem: https://leetcode.com/problems/concatenation-of-consecutive-binary-numbers/
+
+int concatenatedBinary(int n)
+{
+    const long long mod = 1000000007;
+    long long ans = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        int x = i;
+        while (x > 0)
+        {
+            x /= 2;
+            ans *= 2;
+        }
+        ans = (ans + i) % mod;
+    }
+    return ans;
+}
