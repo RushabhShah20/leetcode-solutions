b322724d4c6c086e1355ca6f7bc6892c55d1fc18
Rushabh <20rush09abh2001@gmail.com>
2026-02-28 15:32:22 +0530

    Added Java solution to 'Concatenation of Consecutive Binary Numbers'

--- medium/Concatenation of Consecutive Binary Numbers/Java/main.java
+++ medium/Concatenation of Consecutive Binary Numbers/Java/main.java
@@ -1,22 +1,17 @@
 // Problem: Concatenation of Consecutive Binary Numbers
 // Link to the problem: https://leetcode.com/problems/concatenation-of-consecutive-binary-numbers/
-class Solution
-{
-public:
-    int concatenatedBinary(int n)
-    {
-        const long long mod = 1000000007;
-        long long ans = 0;
-        for (int i = 1; i <= n; i++)
-        {
+class Solution {
+    public int concatenatedBinary(int n) {
+        final long mod = 1000000007;
+        long ans = 0;
+        for (int i = 1; i <= n; i++) {
             int x = i;
-            while (x > 0)
-            {
+            while (x > 0) {
                 x /= 2;
                 ans *= 2;
             }
             ans = (ans + i) % mod;
         }
-        return ans;
+        return (int) ans;
     }
-};
+}
