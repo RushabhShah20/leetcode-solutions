416583785c4135fe92746884f16b22f2c5653589
Rushabh <20rush09abh2001@gmail.com>
2026-03-01 10:23:02 +0530

    Added C# solution to 'Count of Matches in Tournament'

---
+++ easy/Count of Matches in Tournament/C#/main.cs
@@ -0,0 +1,23 @@
+// Problem: Count of Matches in Tournament
+// Link to the problem: https://leetcode.com/problems/count-of-matches-in-tournament/
+public class Solution
+{
+    public int NumberOfMatches(int n)
+    {
+        int ans = 0, m = n;
+        while (m > 1)
+        {
+            if (m % 2 == 0)
+            {
+                ans += (m / 2);
+                m /= 2;
+            }
+            else
+            {
+                ans += ((m - 1) / 2);
+                m = ((m - 1) / 2) + 1;
+            }
+        }
+        return ans;
+    }
+}
