57f07b843ce90d48e7ebe22238b2736770e21e9e
Rushabh <20rush09abh2001@gmail.com>
2026-03-01 10:23:46 +0530

    Added JavaScript solution to 'Count of Matches in Tournament'

---
+++ easy/Count of Matches in Tournament/JavaScript/main.js
@@ -0,0 +1,20 @@
+// Problem: Count of Matches in Tournament
+// Link to the problem: https://leetcode.com/problems/count-of-matches-in-tournament/
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numberOfMatches = function (n) {
+    let ans = 0, m = n;
+    while (m > 1) {
+        if (m % 2 === 0) {
+            ans += (m / 2);
+            m /= 2;
+        }
+        else {
+            ans += ((m - 1) / 2);
+            m = ((m - 1) / 2) + 1;
+        }
+    }
+    return ans;
+};
