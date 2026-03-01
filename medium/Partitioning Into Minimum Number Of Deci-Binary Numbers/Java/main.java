d3e318370bb59b68617dcb99aef25a37e3b9768e
Rushabh <20rush09abh2001@gmail.com>
2026-03-01 09:13:23 +0530

    Added Java solution to 'Partitioning Into Minimum Number Of Deci-Binary Numbers'

--- medium/Partitioning Into Minimum Number Of Deci-Binary Numbers/Java/main.java
+++ medium/Partitioning Into Minimum Number Of Deci-Binary Numbers/Java/main.java
@@ -1,15 +1,11 @@
 // Problem: Partitioning Into Minimum Number Of Deci-Binary Numbers
 // Link to the problem: https://leetcode.com/problems/partitioning-into-minimum-number-of-deci-binary-numbers/
-class Solution
-{
-public:
-    int minPartitions(string n)
-    {
+class Solution {
+    public int minPartitions(String n) {
         int ans = 0;
-        for (const char c : n)
-        {
-            ans = max(ans, c - '0');
+        for (final char c : n.toCharArray()) {
+            ans = Math.max(ans, c - '0');
         }
         return ans;
     }
-};
+}
