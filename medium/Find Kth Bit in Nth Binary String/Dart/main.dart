af28d650fe6f28497078baf7f867043af92e9413
Rushabh <20rush09abh2001@gmail.com>
2026-03-03 13:25:11 +0530

    Added Dart solution to 'Find Kth Bit in Nth Binary String'

---
+++ medium/Find Kth Bit in Nth Binary String/Dart/main.dart
@@ -0,0 +1,20 @@
+// Problem: Find Kth Bit in Nth Binary String
+// Link to the problem: https://leetcode.com/problems/find-kth-bit-in-nth-binary-string/
+class Solution {
+  String findKthBit(int n, int k) {
+    if (n == 1) {
+      return '0';
+    }
+    final int x = (1 << n) - 1, m = x ~/ 2 + 1;
+    if (k == m) {
+      return '1';
+    }
+    if (k < m) {
+      return findKthBit(n - 1, k);
+    }
+    if (findKthBit(n - 1, x - k + 1) == '0') {
+      return '1';
+    }
+    return '0';
+  }
+}
