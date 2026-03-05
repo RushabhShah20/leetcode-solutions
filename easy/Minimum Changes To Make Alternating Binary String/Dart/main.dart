3ffea45f4047b015ba12152e8d703fe636c8d735
Rushabh <20rush09abh2001@gmail.com>
2026-03-05 11:52:42 +0530

    Added Dart solution to 'Minimum Changes To Make Alternating Binary String'

---
+++ easy/Minimum Changes To Make Alternating Binary String/Dart/main.dart
@@ -0,0 +1,25 @@
+// Problem: Minimum Changes To Make Alternating Binary String
+// Link to the problem: https://leetcode.com/problems/minimum-changes-to-make-alternating-binary-string/
+class Solution {
+  int minOperations(String s) {
+    final int n = s.length;
+    int ansEven = 0, ansOdd = 0;
+    for (int i = 0; i < n; i++) {
+      if (i % 2 == 0) {
+        if (s[i] == '0') {
+          ansEven++;
+        } else {
+          ansOdd++;
+        }
+      } else {
+        if (s[i] == '1') {
+          ansEven++;
+        } else {
+          ansOdd++;
+        }
+      }
+    }
+    final int ans = min(ansEven, ansOdd);
+    return ans;
+  }
+}
