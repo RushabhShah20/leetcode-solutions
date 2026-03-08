ac9a8492adf81ff0a092eb594b421308e7bae7ad
Rushabh <20rush09abh2001@gmail.com>
2026-03-08 07:39:22 +0530

    Added C# solution to 'Find Unique Binary String'

---
+++ medium/Find Unique Binary String/C#/main.cs
@@ -0,0 +1,15 @@
+// Problem: Find Unique Binary String
+// Link to the problem: https://leetcode.com/problems/find-unique-binary-string/
+public class Solution
+{
+    public string FindDifferentBinaryString(string[] nums)
+    {
+        int n = nums.Length;
+        char[] ans = new char[n];
+        for (int i = 0; i < n; i++)
+        {
+            ans[i] = nums[i][i] == '0' ? '1' : '0';
+        }
+        return new string(ans);
+    }
+}
