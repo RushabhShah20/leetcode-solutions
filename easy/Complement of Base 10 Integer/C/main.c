3966a1a805ec5ff75ba0e0b3b4384b7f6c7e9320
Rushabh <20rush09abh2001@gmail.com>
2026-03-11 12:31:30 +0530

    Added C solution to 'Complement of Base 10 Integer'

--- easy/Complement of Base 10 Integer/C/main.c
+++ easy/Complement of Base 10 Integer/C/main.c
@@ -1,13 +1,16 @@
 // Problem: Complement of Base 10 Integer
 // Link to the problem: https://leetcode.com/problems/complement-of-base-10-integer/
-function bitwiseComplement(n: number): number {
-    if (n === 0) {
+int bitwiseComplement(int n)
+{
+    if (n == 0)
+    {
         return 1;
     }
-    let x: number = 1;
-    while (x < n) {
+    int x = 1;
+    while (x < n)
+    {
         x = (x << 1) | 1;
     }
-    const ans: number = n ^ x;
+    const int ans = n ^ x;
     return ans;
-};
+}
--- easy/Complement of Base 10 Integer/JavaScript/main.js
+++ easy/Complement of Base 10 Integer/JavaScript/main.js
@@ -4,35 +4,14 @@
  * @param {number} n
  * @return {number}
  */
-var toBinary = function (n) {
-    let ans = new Array();
-    while (n > 0) {
-        let rem = n % 2;
-        ans.push(rem);
-        n = (n / 2) >> 0;
-    }
-    return ans;
-};
-var complement = function (res) {
-    for (let i = 0; i < res.length; i++) {
-        if (res[i] === 1) {
-            res[i] = 0;
-        } else if (res[i] === 0) {
-            res[i] = 1;
-        }
-    }
-    return res;
-};
 var bitwiseComplement = function (n) {
     if (n === 0) {
         return 1;
-    } else {
-        let ans = 0;
-        let binary = toBinary(n);
-        let com = complement(binary);
-        for (let i = 0; i < com.length; i++) {
-            ans += Math.pow(2, i) * com[i];
-        }
-        return ans;
     }
+    let x = 1;
+    while (x < n) {
+        x = (x << 1) | 1;
+    }
+    const ans = n ^ x;
+    return ans;
 };
