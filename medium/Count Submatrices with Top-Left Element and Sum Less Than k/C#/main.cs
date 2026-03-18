d765e232056552973d1f061b864f9125f9c871c1
Rushabh <20rush09abh2001@gmail.com>
2026-03-18 15:48:08 +0530

    Added C# solution to 'Count Submatrices with Top-Left Element and Sum Less Than k'

---
+++ medium/Count Submatrices with Top-Left Element and Sum Less Than k/C#/main.cs
@@ -0,0 +1,29 @@
+// Problem: Count Submatrices with Top-Left Element and Sum Less Than k
+// Link to the problem: https://leetcode.com/problems/count-submatrices-with-top-left-element-and-sum-less-than-k/
+public class Solution
+{
+    public int CountSubmatrices(int[][] grid, int k)
+    {
+        int m = grid.Length, n = grid[0].Length;
+        int[,] prefix = new int[m + 1, n + 1];
+        for (int i = 1; i <= m; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                prefix[i, j] = prefix[i - 1, j] + prefix[i, j - 1] - prefix[i - 1, j - 1] + grid[i - 1][j - 1];
+            }
+        }
+        int ans = 0;
+        for (int i = 1; i <= m; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                if (prefix[i, j] <= k)
+                {
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+}
