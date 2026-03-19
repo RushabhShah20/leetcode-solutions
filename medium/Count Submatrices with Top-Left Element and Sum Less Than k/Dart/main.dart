965885695eb8179ba9df4659f612dac2e1c66d33
Rushabh <20rush09abh2001@gmail.com>
2026-03-19 21:17:43 +0530

    Added Dart solution to 'Count Submatrices with Top-Left Element and Sum Less Than k'

---
+++ medium/Count Submatrices with Top-Left Element and Sum Less Than k/Dart/main.dart
@@ -0,0 +1,26 @@
+// Problem: Count Submatrices with Top-Left Element and Sum Less Than k
+// Link to the problem: https://leetcode.com/problems/count-submatrices-with-top-left-element-and-sum-less-than-k/
+class Solution {
+  int countSubmatrices(List<List<int>> grid, int k) {
+    final int m = grid.length, n = grid[0].length;
+    List<List<int>> prefix = List.generate(m + 1, (_) => List.filled(n + 1, 0));
+    for (int i = 1; i <= m; i++) {
+      for (int j = 1; j <= n; j++) {
+        prefix[i][j] =
+            prefix[i - 1][j] +
+            prefix[i][j - 1] -
+            prefix[i - 1][j - 1] +
+            grid[i - 1][j - 1];
+      }
+    }
+    int ans = 0;
+    for (int i = 1; i <= m; i++) {
+      for (int j = 1; j <= n; j++) {
+        if (prefix[i][j] <= k) {
+          ans++;
+        }
+      }
+    }
+    return ans;
+  }
+}
