8f8d461ff36b798448d7c7a7c8836bd9f8493078
Rushabh <20rush09abh2001@gmail.com>
2026-03-22 14:12:57 +0530

    Added C++ solution to 'Construct Uniform Parity Array I'

---
+++ easy/Construct Uniform Parity Array I/C++/main.cpp
@@ -0,0 +1,10 @@
+// Problem: Construct Uniform Parity Array I
+// Link to the problem: https://leetcode.com/problems/construct-uniform-parity-array-i/
+class Solution
+{
+public:
+    bool uniformArray(vector<int> &nums1)
+    {
+        return true;
+    }
+};
