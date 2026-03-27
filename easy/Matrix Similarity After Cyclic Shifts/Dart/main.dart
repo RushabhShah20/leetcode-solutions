a46a9dae29d5bf2cd836d059474ccf5a92aff548
Rushabh <20rush09abh2001@gmail.com>
2026-03-27 13:31:01 +0530

    Added Dart solution to 'Matrix Similarity After Cyclic Shifts'

---
+++ easy/Matrix Similarity After Cyclic Shifts/Dart/main.dart
@@ -0,0 +1,15 @@
+// Problem: Matrix Similarity After Cyclic Shifts
+// Link to the problem: https://leetcode.com/problems/matrix-similarity-after-cyclic-shifts/
+class Solution {
+  bool areSimilar(List<List<int>> mat, int k) {
+    final int m = mat.length, n = mat[0].length;
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        if (mat[i][j] != mat[i][(j + k) % n]) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+}
