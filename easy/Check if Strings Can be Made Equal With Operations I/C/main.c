beb5c1a11aa1f5a01683c87f42ed8e3712d0b78b
Rushabh <20rush09abh2001@gmail.com>
2026-03-29 13:49:11 +0530

    Added C solution to 'Check if Strings Can be Made Equal With Operations I'

---
+++ easy/Check if Strings Can be Made Equal With Operations I/C/main.c
@@ -0,0 +1,9 @@
+// Problem: Check if Strings Can be Made Equal With Operations I
+// Link to the problem: https://leetcode.com/problems/check-if-strings-can-be-made-equal-with-operations-i/
+bool canBeEqual(char *s1, char *s2)
+{
+    const bool x = (s1[0] == s2[0] && s1[2] == s2[2]) || (s1[0] == s2[2] && s1[2] == s2[0]);
+    const bool y = (s1[1] == s2[1] && s1[3] == s2[3]) || (s1[1] == s2[3] && s1[3] == s2[1]);
+    const bool ans = x && y;
+    return ans;
+}
