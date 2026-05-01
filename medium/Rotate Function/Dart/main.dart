4722137028fe3f570f9e3dc7ac682857379a18fb
Rushabh <20rush09abh2001@gmail.com>
2026-05-01 20:49:12 +0530

    Added Dart solution to 'Rotate Function'

---
+++ medium/Rotate Function/Dart/main.dart
@@ -0,0 +1,18 @@
+// Problem: Rotate Function
+// Link to the problem: https://leetcode.com/problems/rotate-function/
+class Solution {
+  int maxRotateFunction(List<int> nums) {
+    final int n = nums.length;
+    int x = 0, y = 0;
+    for (int i = 0; i < n; i++) {
+      x += nums[i];
+      y += i * nums[i];
+    }
+    int ans = y;
+    for (int i = n - 1; i >= 0; i--) {
+      y = y + x - n * nums[i];
+      ans = max(ans, y);
+    }
+    return ans;
+  }
+}
