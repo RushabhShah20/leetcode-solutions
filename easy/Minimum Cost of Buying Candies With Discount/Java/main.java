204c1ba44d8608cacf749e54319860b574dd39a5
Rushabh <20rush09abh2001@gmail.com>
2026-06-01 14:12:34 +0530

    Added Java solution to 'Minimum Cost of Buying Candies With Discount'

--- easy/Minimum Cost of Buying Candies With Discount/Java/main.java
+++ easy/Minimum Cost of Buying Candies With Discount/Java/main.java
@@ -1,24 +1,17 @@
 // Problem: Minimum Cost of Buying Candies With Discount
 // Link to the problem: https://leetcode.com/problems/minimum-cost-of-buying-candies-with-discount/
-class Solution
-{
-public:
-    int minimumCost(vector<int> &cost)
-    {
-        const int n = cost.size();
+class Solution {
+    public int minimumCost(int[] cost) {
+        final int n = cost.length;
         int ans = 0;
-        sort(cost.begin(), cost.end());
-        for (int i = n - 1; i >= 0; i -= 3)
-        {
-            if (i - 1 >= 0)
-            {
+        Arrays.sort(cost);
+        for (int i = n - 1; i >= 0; i -= 3) {
+            if (i - 1 >= 0) {
                 ans += cost[i] + cost[i - 1];
-            }
-            else
-            {
+            } else {
                 ans += cost[i];
             }
         }
         return ans;
     }
-};
+}
