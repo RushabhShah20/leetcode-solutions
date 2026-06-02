b3b79323647f0a122360634431e32a05b561afca
Rushabh <20rush09abh2001@gmail.com>
2026-06-02 14:36:34 +0530

    Added C++ solution to 'Earliest Finish Time for Land and Water Rides I'

---
+++ easy/Earliest Finish Time for Land and Water Rides I/C++/main.cpp
@@ -0,0 +1,28 @@
+// Problem: Earliest Finish Time for Land and Water Rides I
+// Link to the problem: https://leetcode.com/problems/earliest-finish-time-for-land-and-water-rides-i/
+class Solution
+{
+public:
+    int solve(const int n, const int m, const vector<int> &a1, const vector<int> &d1, const vector<int> &a2, const vector<int> &d2)
+    {
+        int x = INT_MAX;
+        for (int i = 0; i < n; i++)
+        {
+            x = min(x, a1[i] + d1[i]);
+        }
+        int y = INT_MAX;
+        for (int i = 0; i < m; i++)
+        {
+            y = min(y, max(a2[i], x) + d2[i]);
+        }
+        return y;
+    }
+    int earliestFinishTime(vector<int> &landStartTime, vector<int> &landDuration, vector<int> &waterStartTime, vector<int> &waterDuration)
+    {
+        const int n = landDuration.size(), m = waterDuration.size();
+        const int x = solve(n, m, landStartTime, landDuration, waterStartTime, waterDuration);
+        const int y = solve(m, n, waterStartTime, waterDuration, landStartTime, landDuration);
+        const int ans = min(x, y);
+        return ans;
+    }
+};
