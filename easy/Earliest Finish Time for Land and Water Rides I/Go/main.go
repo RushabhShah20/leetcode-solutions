a7d3802c892f1a972bc30e99aac09e526ed0da6e
Rushabh <20rush09abh2001@gmail.com>
2026-06-03 14:53:18 +0530

    Added Go solution to 'Earliest Finish Time for Land and Water Rides I'

---
+++ easy/Earliest Finish Time for Land and Water Rides I/Go/main.go
@@ -0,0 +1,21 @@
+// Problem: Earliest Finish Time for Land and Water Rides I
+// Link to the problem: https://leetcode.com/problems/earliest-finish-time-for-land-and-water-rides-i/
+func solve(n int, m int, a1 []int, d1 []int, a2 []int, d2 []int) int {
+	var x int = math.MaxInt
+	for i := 0; i < n; i++ {
+		x = min(x, a1[i]+d1[i])
+	}
+	var y int = math.MaxInt
+	for i := 0; i < m; i++ {
+		y = min(y, max(a2[i], x)+d2[i])
+	}
+	return y
+}
+func earliestFinishTime(landStartTime []int, landDuration []int, waterStartTime []int, waterDuration []int) int {
+	var n int = len(landDuration)
+	var m int = len(waterDuration)
+	var x int = solve(n, m, landStartTime, landDuration, waterStartTime, waterDuration)
+	var y int = solve(m, n, waterStartTime, waterDuration, landStartTime, landDuration)
+	var ans int = min(x, y)
+	return ans
+}
