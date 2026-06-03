9dcf12099a69db25606dcb191e7d1b0adefca0d2
Rushabh <20rush09abh2001@gmail.com>
2026-06-03 14:52:35 +0530

    Added Typescript solution to 'Earliest Finish Time for Land and Water Rides I'

---
+++ easy/Earliest Finish Time for Land and Water Rides I/Typescript/main.ts
@@ -0,0 +1,20 @@
+// Problem: Earliest Finish Time for Land and Water Rides I
+// Link to the problem: https://leetcode.com/problems/earliest-finish-time-for-land-and-water-rides-i/
+function solve(n: number, m: number, a1: number[], d1: number[], a2: number[], d2: number[]): number {
+    let x: number = Number.MAX_SAFE_INTEGER;
+    for (let i = 0; i < n; i++) {
+        x = Math.min(x, a1[i] + d1[i]);
+    }
+    let y: number = Number.MAX_SAFE_INTEGER;
+    for (let i = 0; i < m; i++) {
+        y = Math.min(y, Math.max(a2[i], x) + d2[i]);
+    }
+    return y;
+}
+function earliestFinishTime(landStartTime: number[], landDuration: number[], waterStartTime: number[], waterDuration: number[]): number {
+    const n: number = landDuration.length, m: number = waterDuration.length;
+    const x: number = solve(n, m, landStartTime, landDuration, waterStartTime, waterDuration);
+    const y: number = solve(m, n, waterStartTime, waterDuration, landStartTime, landDuration);
+    const ans: number = Math.min(x, y);
+    return ans;
+};
