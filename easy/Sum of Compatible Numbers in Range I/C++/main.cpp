4de5482b66bbda9acbc452d1e8e161ee2d38ce7e
Rushabh <20rush09abh2001@gmail.com>
2026-06-07 10:58:47 +0530

    Added C++ solution to 'Sum of Compatible Numbers in Range I'

---
+++ easy/Sum of Compatible Numbers in Range I/C++/main.cpp
@@ -0,0 +1,18 @@
+// Problem: Sum of Compatible Numbers in Range I
+// Link to the problem: https://leetcode.com/problems/sum-of-compatible-numbers-in-range-i/
+class Solution
+{
+public:
+    int sumOfGoodIntegers(int n, int k)
+    {
+        int ans = 0;
+        for (int i = max(1, n - k); i <= n + k; i++)
+        {
+            if ((n & i) == 0)
+            {
+                ans += i;
+            }
+        }
+        return ans;
+    }
+};
