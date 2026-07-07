14a99bbef8c7a19b7d0aa9fb986b4e49676a339f
Rushabh <20rush09abh2001@gmail.com>
2026-07-07 15:11:43 +0530

    Added Java solution to 'Concatenate Non-Zero Digits and Multiply by Sum I'

--- easy/Concatenate Non-Zero Digits and Multiply by Sum I/Java/main.java
+++ easy/Concatenate Non-Zero Digits and Multiply by Sum I/Java/main.java
@@ -1,24 +1,19 @@
 // Problem: Concatenate Non-Zero Digits and Multiply by Sum I
 // Link to the problem: https://leetcode.com/problems/concatenate-non-zero-digits-and-multiply-by-sum-i/
-class Solution
-{
-public:
-    long long sumAndMultiply(int n)
-    {
-        long long x = 0, y = 0, a = 1;
-        while (n > 0)
-        {
-            const long long z = n % 10;
+class Solution {
+    public long sumAndMultiply(int n) {
+        long x = 0, y = 0, a = 1;
+        while (n > 0) {
+            final long z = n % 10;
             n /= 10;
-            if (z == 0)
-            {
+            if (z == 0) {
                 continue;
             }
             x = z * a + x;
             y += z;
             a *= 10;
         }
-        const long long ans = x * y;
+        final long ans = x * y;
         return ans;
     }
-};
+}
