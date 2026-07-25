3ddcb8d1aa923e7307866b2966f86c6d1f75b40b
Rushabh <20rush09abh2001@gmail.com>
2026-07-25 16:05:02 +0530

    Added Dart solution to 'Maximum Produdct of Two Digits'

---
+++ easy/Maximum Product of Two Digits/Dart/main.dart
@@ -0,0 +1,19 @@
+// Problem: Maximum Produdct of Two Digits
+// Link to the problem: https://leetcode.com/problems/maximum-product-of-two-digits/
+class Solution {
+  int maxProduct(int n) {
+    int x = 0, y = 0;
+    while (n > 0) {
+      final int k = n % 10;
+      if (k > x) {
+        y = x;
+        x = k;
+      } else if (k > y) {
+        y = k;
+      }
+      n ~/= 10;
+    }
+    final int ans = x * y;
+    return ans;
+  }
+}
