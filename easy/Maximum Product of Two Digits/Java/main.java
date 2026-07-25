1c13567ab0c3b282eeca72e6eb83fc8f75d6a2b2
Rushabh <20rush09abh2001@gmail.com>
2026-07-25 16:03:45 +0530

    Added Java solution to 'Maximum Produdct of Two Digits'

--- easy/Maximum Product of Two Digits/Java/main.java
+++ easy/Maximum Product of Two Digits/Java/main.java
@@ -1,26 +1,19 @@
 // Problem: Maximum Produdct of Two Digits
 // Link to the problem: https://leetcode.com/problems/maximum-product-of-two-digits/
-class Solution
-{
-public:
-    int maxProduct(int n)
-    {
+class Solution {
+    public int maxProduct(int n) {
         int x = 0, y = 0;
-        while (n > 0)
-        {
-            const int k = n % 10;
-            if (k > x)
-            {
+        while (n > 0) {
+            final int k = n % 10;
+            if (k > x) {
                 y = x;
                 x = k;
-            }
-            else if (k > y)
-            {
+            } else if (k > y) {
                 y = k;
             }
             n /= 10;
         }
-        const int ans = x * y;
+        final int ans = x * y;
         return ans;
     }
-};
+}
