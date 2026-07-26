60e8dfdde49e7725dee79933c7bbf763bdda4899
Rushabh <20rush09abh2001@gmail.com>
2026-07-26 08:14:46 +0530

    Added Java solution to 'Maximum Product of Three Numbers'

--- easy/Maximum Product of Three Numbers/Java/main.java
+++ easy/Maximum Product of Three Numbers/Java/main.java
@@ -1,40 +1,28 @@
 // Problem: Maximum Product of Three Numbers
 // Link to the problem: https://leetcode.com/problems/maximum-product-of-three-numbers/
-class Solution
-{
-public:
-    int maximumProduct(vector<int> &nums)
-    {
-        const int n = nums.size();
+class Solution {
+    public int maximumProduct(int[] nums) {
+        final int n = nums.length;
         int a = -1001, b = -1001, c = -1001, x = 1001, y = 1001;
-        for (int i = 0; i < n; i++)
-        {
-            if (nums[i] < x)
-            {
+        for (int i = 0; i < n; i++) {
+            if (nums[i] < x) {
                 y = x;
                 x = nums[i];
-            }
-            else if (nums[i] < y)
-            {
+            } else if (nums[i] < y) {
                 y = nums[i];
             }
-            if (nums[i] > a)
-            {
+            if (nums[i] > a) {
                 c = b;
                 b = a;
                 a = nums[i];
-            }
-            else if (nums[i] > b)
-            {
+            } else if (nums[i] > b) {
                 c = b;
                 b = nums[i];
-            }
-            else if (nums[i] > c)
-            {
+            } else if (nums[i] > c) {
                 c = nums[i];
             }
         }
-        const int ans = max(a * b * c, a * x * y);
+        final int ans = Math.max(a * b * c, a * x * y);
         return ans;
     }
-};
+}
