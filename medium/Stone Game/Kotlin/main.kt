3e1a7791d1f48ad15ef51fd2290c1b66c782202e
Rushabh <20rush09abh2001@gmail.com>
2026-08-02 11:49:33 +0530

    Added Kotlin solution to 'Stone Game'

---
+++ medium/Stone Game/Kotlin/main.kt
@@ -0,0 +1,7 @@
+// Problem: Stone Game
+// Link to the problem: https://leetcode.com/problems/stone-game/
+class Solution {
+    fun stoneGame(piles: IntArray): Boolean {
+        return true;
+    }
+}
