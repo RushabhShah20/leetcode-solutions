9528e34c6e183c65b16cd44a4a0599703238c242
Rushabh <20rush09abh2001@gmail.com>
2026-08-11 15:12:47 +0530

    Added Java solution to 'Smallest Missing Integer Greater Than Sequential Prefix Sum'

--- easy/Smallest Missing Integer Greater Than Sequential Prefix Sum/Java/main.java
+++ easy/Smallest Missing Integer Greater Than Sequential Prefix Sum/Java/main.java
@@ -1,29 +1,24 @@
 // Problem: Smallest Missing Integer Greater Than Sequential Prefix Sum
 // Link to the problem: https://leetcode.com/problems/smallest-missing-integer-greater-than-sequential-prefix-sum/
-class Solution
-{
-public:
-    int missingInteger(vector<int> &nums)
-    {
-        const int n = nums.size();
-        unordered_set<int> s(nums.begin(), nums.end());
+class Solution {
+    public int missingInteger(int[] nums) {
+        final int n = nums.length;
+        Set<Integer> s = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            s.add(nums[i]);
+        }
         int m = 1;
-        for (int i = 1; i < n; i++)
-        {
-            if (nums[i] == nums[i - 1] + 1)
-            {
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i - 1] + 1) {
                 m++;
-            }
-            else
-            {
+            } else {
                 break;
             }
         }
         int ans = (nums[m - 1] + nums[0]) * m / 2;
-        while (s.count(ans))
-        {
+        while (s.contains(ans)) {
             ans++;
         }
         return ans;
     }
-};
+}
