5c738e1fd47758e699b71d27a3f29bda751eaab9
Rushabh <20rush09abh2001@gmail.com>
2026-08-16 08:30:57 +0530

    Added JavaScript solution to 'Stone Game IX'

---
+++ medium/Stone Game IX/JavaScript/main.js
@@ -0,0 +1,24 @@
+// Problem: Stone Game IX
+// Link to the problem: https://leetcode.com/problems/stone-game-ix/
+/**
+ * @param {number[]} stones
+ * @return {boolean}
+ */
+var stoneGameIX = function (stones) {
+    const n = stones.length;
+    let a = 0, b = 0, c = 0;
+    for (let i = 0; i < n; i++) {
+        const x = stones[i] % 3;
+        if (x === 1) {
+            b++;
+        }
+        else if (x === 2) {
+            c++;
+        }
+        else {
+            a++;
+        }
+    }
+    const ans = a & 1 ? Math.abs(b - c) > 2 : b >= 1 && c >= 1;
+    return ans;
+};
