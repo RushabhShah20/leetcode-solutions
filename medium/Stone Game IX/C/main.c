45e051aefa671d5bec8ead372d8a6f34c0ebed11
Rushabh <20rush09abh2001@gmail.com>
2026-08-16 08:29:52 +0530

    Added C solution to 'Stone Game IX'

---
+++ medium/Stone Game IX/C/main.c
@@ -0,0 +1,25 @@
+// Problem: Stone Game IX
+// Link to the problem: https://leetcode.com/problems/stone-game-ix/
+bool stoneGameIX(int *stones, int stonesSize)
+{
+    const int n = stonesSize;
+    int a = 0, b = 0, c = 0;
+    for (int i = 0; i < n; i++)
+    {
+        const int x = stones[i] % 3;
+        if (x == 1)
+        {
+            b++;
+        }
+        else if (x == 2)
+        {
+            c++;
+        }
+        else
+        {
+            a++;
+        }
+    }
+    const bool ans = a & 1 ? abs(b - c) > 2 : b >= 1 && c >= 1;
+    return ans;
+}
