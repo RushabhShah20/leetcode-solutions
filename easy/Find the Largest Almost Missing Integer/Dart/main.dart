a0d1b3785e02e2aeb4d5705f35ad44b196f59a46
Rushabh <20rush09abh2001@gmail.com>
2026-08-18 13:41:17 +0530

    Added Dart solution to 'Find the Largest Almost Missing Integer'

---
+++ easy/Find the Largest Almost Missing Integer/Dart/main.dart
@@ -0,0 +1,33 @@
+// Problem: Find the Largest Almost Missing Integer
+// Link to the problem: https://leetcode.com/problems/find-the-largest-almost-missing-integer/
+class Solution {
+  int largestInteger(List<int> nums, int k) {
+    final int n = nums.length;
+    int ans = -1;
+    if (n == k) {
+      for (int i = 0; i < n; i++) {
+        ans = max(ans, nums[i]);
+      }
+      return ans;
+    }
+    List<int> a = new List.filled(51, 0);
+    for (int i = 0; i < n; i++) {
+      a[nums[i]]++;
+    }
+    if (k == 1) {
+      for (int i = 50; i >= 0; i--) {
+        if (a[i] == 1) {
+          return i;
+        }
+      }
+      return -1;
+    }
+    if (a[nums[0]] == 1) {
+      ans = max(ans, nums[0]);
+    }
+    if (a[nums[n - 1]] == 1) {
+      ans = max(ans, nums[n - 1]);
+    }
+    return ans;
+  }
+}
