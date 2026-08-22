a68906ed097fb8d5dc4a0b2ddad0a25fb8eef463
Rushabh <20rush09abh2001@gmail.com>
2026-08-22 09:59:57 +0530

    Added Java solution to 'Check Divisibility by Digit Sum and Product'

--- easy/Check Divisibility by Digit Sum and Product/Java/main.java
+++ easy/Check Divisibility by Digit Sum and Product/Java/main.java
@@ -1,19 +1,15 @@
 // Problem: Check Divisibility by Digit Sum and Product
 // Link to the problem: https://leetcode.com/problems/check-divisibility-by-digit-sum-and-product/
-class Solution
-{
-public:
-    bool checkDivisibility(int n)
-    {
+class Solution {
+    public boolean checkDivisibility(int n) {
         int x = 0, y = 1, m = n;
-        while (m > 0)
-        {
-            const int z = m % 10;
+        while (m > 0) {
+            final int z = m % 10;
             x += z;
             y *= z;
             m /= 10;
         }
-        const bool ans = n % (x + y) == 0;
+        final boolean ans = (n % (x + y)) == 0;
         return ans;
     }
-};
+}
