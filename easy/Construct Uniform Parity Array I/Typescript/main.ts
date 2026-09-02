ee2c6331677f5b94f849a571bc09221c39f97bd9
Rushabh <20rush09abh2001@gmail.com>
2026-09-02 09:10:58 +0530

    Added Typescript solution to 'Construct Uniform Parity Array I'

---
+++ easy/Construct Uniform Parity Array I/Typescript/main.ts
@@ -0,0 +1,5 @@
+// Problem: Construct Uniform Parity Array I
+// Link to the problem: https://leetcode.com/problems/construct-uniform-parity-array-i/
+function uniformArray(nums1: number[]): boolean {
+    return true;
+};
