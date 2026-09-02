037dc5c03cc28903e4ba16d4c545c97a4853ab18
Rushabh <20rush09abh2001@gmail.com>
2026-09-02 09:11:42 +0530

    Added Rust solution to 'Construct Uniform Parity Array I'

---
+++ easy/Construct Uniform Parity Array I/Rust/main.rs
@@ -0,0 +1,7 @@
+// Problem: Construct Uniform Parity Array I
+// Link to the problem: https://leetcode.com/problems/construct-uniform-parity-array-i/
+impl Solution {
+    pub fn uniform_array(nums1: Vec<i32>) -> bool {
+        return true;
+    }
+}
