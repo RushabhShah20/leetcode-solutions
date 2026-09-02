21ccedb0781924fe19ec0e93eafc6f40eba642e9
Rushabh <20rush09abh2001@gmail.com>
2026-09-02 09:09:53 +0530

    Added C# solution to 'Construct Uniform Parity Array I'

---
+++ easy/Construct Uniform Parity Array I/C#/main.cs
@@ -0,0 +1,9 @@
+// Problem: Construct Uniform Parity Array I
+// Link to the problem: https://leetcode.com/problems/construct-uniform-parity-array-i/
+public class Solution
+{
+    public bool UniformArray(int[] nums1)
+    {
+        return true;
+    }
+}
