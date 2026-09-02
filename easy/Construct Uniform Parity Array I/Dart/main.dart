86016cef59b9bd6ad63a5c4866d04fec587be489
Rushabh <20rush09abh2001@gmail.com>
2026-09-02 09:11:11 +0530

    Added Dart solution to 'Construct Uniform Parity Array I'

---
+++ easy/Construct Uniform Parity Array I/Dart/main.dart
@@ -0,0 +1,7 @@
+// Problem: Construct Uniform Parity Array I
+// Link to the problem: https://leetcode.com/problems/construct-uniform-parity-array-i/
+class Solution {
+  bool uniformArray(List<int> nums1) {
+    return true;
+  }
+}
