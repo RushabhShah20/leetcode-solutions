10715a5fb433fd1088b5986175d42122806da76d
Rushabh <20rush09abh2001@gmail.com>
2026-09-02 09:10:19 +0530

    Added Python solution to 'Construct Uniform Parity Array I'

---
+++ easy/Construct Uniform Parity Array I/Python/main.py
@@ -0,0 +1,5 @@
+# Problem: Construct Uniform Parity Array I
+# Link to the problem: https://leetcode.com/problems/construct-uniform-parity-array-i/
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+        return True
