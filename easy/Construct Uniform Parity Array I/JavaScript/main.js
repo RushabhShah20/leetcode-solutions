ada27277069478da57fb30b24a893e6035b094db
Rushabh <20rush09abh2001@gmail.com>
2026-09-02 09:10:43 +0530

    Added JavaScript solution to 'Construct Uniform Parity Array I'

---
+++ easy/Construct Uniform Parity Array I/JavaScript/main.js
@@ -0,0 +1,9 @@
+// Problem: Construct Uniform Parity Array I
+// Link to the problem: https://leetcode.com/problems/construct-uniform-parity-array-i/
+/**
+ * @param {number[]} nums1
+ * @return {boolean}
+ */
+var uniformArray = function (nums1) {
+    return true;
+};
