aa34741e189fffb5ffe546cb586905683cf8795e
Rushabh <20rush09abh2001@gmail.com>
2026-09-02 09:10:06 +0530

    Added C solution to 'Construct Uniform Parity Array I'

---
+++ easy/Construct Uniform Parity Array I/C/main.c
@@ -0,0 +1,6 @@
+// Problem: Construct Uniform Parity Array I
+// Link to the problem: https://leetcode.com/problems/construct-uniform-parity-array-i/
+bool uniformArray(int *nums1, int nums1Size)
+{
+    return true;
+}
