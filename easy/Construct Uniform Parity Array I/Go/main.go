f2e85b383a8f1aea2b23e16d60321ce89ddf4bac
Rushabh <20rush09abh2001@gmail.com>
2026-09-02 09:11:28 +0530

    Added Go solution to 'Construct Uniform Parity Array I'

---
+++ easy/Construct Uniform Parity Array I/Go/main.go
@@ -0,0 +1,5 @@
+// Problem: Construct Uniform Parity Array I
+// Link to the problem: https://leetcode.com/problems/construct-uniform-parity-array-i/
+func uniformArray(nums1 []int) bool {
+	return true
+}
