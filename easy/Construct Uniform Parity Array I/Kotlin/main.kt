5012f7a9f8aebfc0e0ebbb8c5c2de340512a84ee
Rushabh <20rush09abh2001@gmail.com>
2026-09-02 09:11:56 +0530

    Added Kotlin solution to 'Construct Uniform Parity Array I'

---
+++ easy/Construct Uniform Parity Array I/Kotlin/main.kt
@@ -0,0 +1,7 @@
+// Problem: Construct Uniform Parity Array I
+// Link to the problem: https://leetcode.com/problems/construct-uniform-parity-array-i/
+class Solution {
+    fun uniformArray(nums1: IntArray): Boolean {
+        return true;
+    }
+}
