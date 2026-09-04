c33609a672d50d528481ffb538b8cedfbf613b3f
Rushabh <20rush09abh2001@gmail.com>
2026-09-04 10:45:50 +0530

    Added Java solution to 'Smallest Stable Index I'

---
+++ easy/Smallest Stable Index I/Java/main.java
@@ -0,0 +1,22 @@
+// Problem: Smallest Stable Index I
+// Link to the problem: https://leetcode.com/problems/smallest-stable-index-i/
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+        final int n = nums.length;
+        int[] mn = new int[n], mx = new int[n];
+        mx[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            mx[i] = Math.max(mx[i - 1], nums[i]);
+        }
+        mn[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            mn[i] = Math.min(mn[i + 1], nums[i]);
+        }
+        for (int i = 0; i < n; i++) {
+            if (mx[i] - mn[i] <= k) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
