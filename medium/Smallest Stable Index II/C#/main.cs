5287d6c786b531e3d0f2a17d6e8485f31447e196
Rushabh <20rush09abh2001@gmail.com>
2026-09-05 10:51:36 +0530

    Added C# solution to 'Smallest Stable Index II'

---
+++ medium/Smallest Stable Index II/C#/main.cs
@@ -0,0 +1,28 @@
+// Problem: Smallest Stable Index II
+// Link to the problem: https://leetcode.com/problems/smallest-stable-index-ii/
+public class Solution
+{
+    public int FirstStableIndex(int[] nums, int k)
+    {
+        int n = nums.Length;
+        int[] mn = new int[n], mx = new int[n];
+        mx[0] = nums[0];
+        for (int i = 1; i < n; i++)
+        {
+            mx[i] = Math.Max(mx[i - 1], nums[i]);
+        }
+        mn[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--)
+        {
+            mn[i] = Math.Min(mn[i + 1], nums[i]);
+        }
+        for (int i = 0; i < n; i++)
+        {
+            if (mx[i] - mn[i] <= k)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
