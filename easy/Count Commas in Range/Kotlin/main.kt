ea35e1fe6fdd8d59a82b62c0bbb4bfd6b1d983ae
Rushabh <20rush09abh2001@gmail.com>
2026-09-08 10:32:58 +0530

    Added Kotlin solution to 'Count Commas in Range'

---
+++ easy/Count Commas in Range/Kotlin/main.kt
@@ -0,0 +1,8 @@
+// Problem: Count Commas in Range
+// Link to the problem: https://leetcode.com/problems/count-commas-in-range/
+class Solution {
+    fun countCommas(n: Int): Int {
+        val ans: Int = maxOf(n - 999, 0);
+        return ans;
+    }
+}
