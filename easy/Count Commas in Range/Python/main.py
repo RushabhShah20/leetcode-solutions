88ddf102c2d3c702b1620bee90de5f060d91a996
Rushabh <20rush09abh2001@gmail.com>
2026-09-08 10:11:26 +0530

    Added Python solution to 'Count Commas in Range'

---
+++ easy/Count Commas in Range/Python/main.py
@@ -0,0 +1,6 @@
+# Problem: Count Commas in Range
+# Link to the problem: https://leetcode.com/problems/count-commas-in-range/
+class Solution:
+    def countCommas(self, n: int) -> int:
+        ans: int = max(n - 999, 0)
+        return ans
