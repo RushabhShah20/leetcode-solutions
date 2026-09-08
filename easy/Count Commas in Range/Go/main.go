617440d88bedc3922b7677eeefd1abd6670d9d20
Rushabh <20rush09abh2001@gmail.com>
2026-09-08 10:12:56 +0530

    Added Go solution to 'Count Commas in Range'

---
+++ easy/Count Commas in Range/Go/main.go
@@ -0,0 +1,6 @@
+// Problem: Count Commas in Range
+// Link to the problem: https://leetcode.com/problems/count-commas-in-range/
+func countCommas(n int) int {
+	var ans int = max(n-999, 0)
+	return ans
+}
