c89d1f24a8b8cc655f52c1a2ff03c16e194552c7
Rushabh <20rush09abh2001@gmail.com>
2026-09-08 10:10:40 +0530

    Added C# solution to 'Count Commas in Range'

---
+++ easy/Count Commas in Range/C#/main.cs
@@ -0,0 +1,10 @@
+// Problem: Count Commas in Range
+// Link to the problem: https://leetcode.com/problems/count-commas-in-range/
+public class Solution
+{
+    public int CountCommas(int n)
+    {
+        int ans = Math.Max(n - 999, 0);
+        return ans;
+    }
+}
