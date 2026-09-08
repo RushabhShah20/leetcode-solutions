79020fb45d87f1e17f1261bb9b7e71485c33658e
Rushabh <20rush09abh2001@gmail.com>
2026-09-08 10:12:31 +0530

    Added Dart solution to 'Count Commas in Range'

---
+++ easy/Count Commas in Range/Dart/main.dart
@@ -0,0 +1,8 @@
+// Problem: Count Commas in Range
+// Link to the problem: https://leetcode.com/problems/count-commas-in-range/
+class Solution {
+  int countCommas(int n) {
+    final int ans = max(n - 999, 0);
+    return ans;
+  }
+}
