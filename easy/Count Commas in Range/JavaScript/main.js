c7bec8f962d2f041165a1efa6e7eb200cf543efb
Rushabh <20rush09abh2001@gmail.com>
2026-09-08 10:11:44 +0530

    Added JavaScript solution to 'Count Commas in Range'

---
+++ easy/Count Commas in Range/JavaScript/main.js
@@ -0,0 +1,10 @@
+// Problem: Count Commas in Range
+// Link to the problem: https://leetcode.com/problems/count-commas-in-range/
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countCommas = function (n) {
+    const ans = Math.max(n - 999, 0);
+    return ans;
+};
