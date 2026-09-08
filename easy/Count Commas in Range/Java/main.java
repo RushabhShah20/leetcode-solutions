fd5bf1ef2baffac46da1e7ee334d447929842e46
Rushabh <20rush09abh2001@gmail.com>
2026-09-08 10:10:23 +0530

    Added Java solution to 'Count Commas in Range'

--- easy/Count Commas in Range/Java/main.java
+++ easy/Count Commas in Range/Java/main.java
@@ -1,11 +1,8 @@
 // Problem: Count Commas in Range
 // Link to the problem: https://leetcode.com/problems/count-commas-in-range/
-class Solution
-{
-public:
-    int countCommas(int n)
-    {
-        const int ans = max(n - 999, 0);
+class Solution {
+    public int countCommas(int n) {
+        final int ans = Math.max(n - 999, 0);
         return ans;
     }
-};
+}
