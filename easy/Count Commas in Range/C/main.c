8e4c68bb0275f4c00fa60e01c77bd21160a1b4f5
Rushabh <20rush09abh2001@gmail.com>
2026-09-08 10:11:02 +0530

    Added C solution to 'Count Commas in Range'

---
+++ easy/Count Commas in Range/C/main.c
@@ -0,0 +1,7 @@
+// Problem: Count Commas in Range
+// Link to the problem: https://leetcode.com/problems/count-commas-in-range/
+int countCommas(int n)
+{
+    const int ans = fmax(n - 999, 0);
+    return ans;
+}
