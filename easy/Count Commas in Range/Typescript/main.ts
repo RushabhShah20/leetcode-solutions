58b7ca522490a79afa653a40188e4260804adc60
Rushabh <20rush09abh2001@gmail.com>
2026-09-08 10:11:58 +0530

    Added Typescript solution to 'Count Commas in Range'

---
+++ easy/Count Commas in Range/Typescript/main.ts
@@ -0,0 +1,6 @@
+// Problem: Count Commas in Range
+// Link to the problem: https://leetcode.com/problems/count-commas-in-range/
+function countCommas(n: number): number {
+    const ans: number = Math.max(n - 999, 0);
+    return ans;
+};
