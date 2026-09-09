bb6834a944420d3be036e6649bcbb6642d321bd0
Rushabh <20rush09abh2001@gmail.com>
2026-09-09 09:45:49 +0530

    Added Java solution to 'Count Commas in Range II'

--- medium/Count Commas in Range II/Java/main.java
+++ medium/Count Commas in Range II/Java/main.java
@@ -1,16 +1,12 @@
 // Problem: Count Commas in Range II
 // Link to the problem: https://leetcode.com/problems/count-commas-in-range-ii/
-class Solution
-{
-public:
-    long long countCommas(long long n)
-    {
-        long long ans = 0, x = 1000;
-        while (x <= n)
-        {
+class Solution {
+    public long countCommas(long n) {
+        long ans = 0, x = 1000;
+        while (x <= n) {
             ans += (n - x + 1);
             x *= 1000;
         }
         return ans;
     }
-};
+}
