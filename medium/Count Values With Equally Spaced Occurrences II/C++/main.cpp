0bfbf347d4a8d8d58b1db4ea8081549c6009c35e
Rushabh <20rush09abh2001@gmail.com>
2026-09-13 08:31:40 +0530

    Added C++ solution to 'Count Values With Equally Spaced Occurrences II'

---
+++ medium/Count Values With Equally Spaced Occurrences II/C++/main.cpp
@@ -0,0 +1,37 @@
+// Problem: Count Values With Equally Spaced Occurrences II
+// Link to the problem: https://leetcode.com/problems/count-values-with-equally-spaced-occurrences-ii/
+class Solution
+{
+public:
+    int countSpecialIntegers(vector<int> &nums)
+    {
+        const int n = nums.size();
+        unordered_map<int, vector<int>> m;
+        for (int i = 0; i < n; i++)
+        {
+            m[nums[i]].push_back(i);
+        }
+        int ans = 0;
+        for (const pair<int, vector<int>> i : m)
+        {
+            if (i.second.size() >= 3)
+            {
+                const int m = i.second.size();
+                bool x = true;
+                for (int j = 1; j < m - 1; j++)
+                {
+                    if (i.second[j] - i.second[j - 1] != i.second[j + 1] - i.second[j])
+                    {
+                        x = false;
+                        break;
+                    }
+                }
+                if (x)
+                {
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
