7bb45d8bd9452c7b2bc2bdb2b93b5eb57ce6b546
Rushabh <20rush09abh2001@gmail.com>
2026-09-14 10:03:27 +0530

    Added Typescript solution to 'Rectangle Overlap'

---
+++ easy/Rectangle Overlap/Typescript/main.ts
@@ -0,0 +1,6 @@
+// Problem: Rectangle Overlap
+// Link to the problem: https://leetcode.com/problems/rectangle-overlap/
+function isRectangleOverlap(rec1: number[], rec2: number[]): boolean {
+    const ans: boolean = (Math.min(rec1[2], rec2[2]) > Math.max(rec1[0], rec2[0])) && (Math.min(rec1[3], rec2[3]) > Math.max(rec1[1], rec2[1]));
+    return ans;
+};
